2a58fa377209eeae7d5ec9b9a08c4a21fb155f4e
Felix Faerber <mail@ffaerber.com>
2014-02-27 16:50:34 +0100

    add user

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,13 @@ module Buzzn
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.generators do |g|
+        g.test_framework :rspec, :spec => true, :fixture => false
+        g.stylesheets = false
+        g.javascripts = false
+        g.helper      = false
+    end
+
   end
 end
