a6e45222ea9e5ee7aa9f1d33f5f40de95e978631
Felix Faerber <mail@ffaerber.com>
2014-03-10 21:25:43 +0100

    more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
+# encoding: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'rubygems' #so it can load gems
+
+puts '  seed development database'
+
+meters = []
+
+puts 'create admin'
+admin = Fabricate(:admin)
+admin.add_role 'admin'
+
+puts 'create stefan'
+meters << meter_stefan = Fabricate(:meter_stefan)
+stefan = Fabricate(:stefan)
+stefan.add_role :manager, meter_stefan
+
+puts 'create jan'
+meters << meter_jan = Fabricate(:meter_jan)
+jan = Fabricate(:jan)
+jan.add_role :manager, meter_jan
+
+puts 'chreate meter data'
+meters.each do |meter|
+  File.foreach("#{Rails.root}/db/seeds/meter#{meter.id}.txt").with_index { |line, line_num|
+    Measurement.create(
+      meter_id:  meter.id,
+      timestamp: DateTime.now.in_time_zone(Time.zone).beginning_of_day + line_num.minute,
+      wh:        line.to_i
+    )
+  }
+end
