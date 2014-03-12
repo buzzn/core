9f3a063af31701f97dbda48bc415d707d94ecbe7
Felix Faerber <mail@ffaerber.com>
2014-03-12 19:45:38 +0100

    simple working aggregation

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -7,4 +7,46 @@ class Measurement
 
   index({ meter_id: 1 })
   index({ timestamp: 1 })
+
+
+
+
+  def self.this_day_to_hours_by_meter_id(meter_id)
+    pipe = [
+      { "$match" => {
+          timestamp: {
+            "$gte" => Time.at(DateTime.now.utc.beginning_of_day),
+            "$lt"  => Time.at(DateTime.now.utc.end_of_day)
+          },
+          meter_id: {
+            "$in" => [meter_id]
+          }
+        }
+      },
+      { "$project" => {
+          wh: 1,
+          hourly: { "$hour" => "$timestamp" }
+        }
+      },
+      { "$group" => {
+          _id: "$hourly",
+          firstReading: { "$last"  => "$wh" },
+          lastReading: { "$first" => "$wh" }
+        }
+      },
+      { "$project" => {
+          hourReading: { "$subtract" => [ "$firstReading", "$lastReading" ] }
+        }
+      },
+      { "$sort" => {
+          _id: 1
+        }
+      }
+    ]
+    return Measurement.collection.aggregate(pipe)
+  end
+
+
+
+
 end
--- app/models/meter.rb
+++ app/models/meter.rb
@@ -4,4 +4,16 @@ class Meter < ActiveRecord::Base
 
   belongs_to :user
 
+
+
+  def day_to_hours
+    hours = []
+    Measurement.this_day_to_hours_by_meter_id(self.id).each do |hour|
+      hours << hour['hourReading']
+    end
+
+    return hours.join(', ')
+  end
+
+
 end
