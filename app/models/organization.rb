891e481c7c5e5d77a2206b2ea2ead14d57f316c8
Felix Faerber <mail@ffaerber.com>
2014-05-28 14:13:46 +0200

    add metering_point_operator_contract to organisation

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,6 +2,7 @@ class Organization < ActiveRecord::Base
   has_many :distribution_system_operator_contracts
   has_many :electricity_supplier_contracts
   has_many :metering_service_provider_contracts
+  has_many :metering_point_operator_contract
   has_one  :contracting_party
 
   has_one :address, as: :addressable, dependent: :destroy
