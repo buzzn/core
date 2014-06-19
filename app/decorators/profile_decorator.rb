182e34ff6076a12b5e033135d24d6af9b5e9de77
Felix Faerber <mail@ffaerber.com>
2014-06-19 10:11:05 +0200

    update activity stream

--- app/decorators/profile_decorator.rb
+++ app/decorators/profile_decorator.rb
@@ -9,18 +9,18 @@ class ProfileDecorator < Draper::Decorator
 
   def image_tag_small
     if model.image?
-      image_tag model.image.small, class: 'img-circle'
+      image_tag model.image.small, class: 'img-circle', size: '30x30'
     else
-      image_tag 'male.png'
+      image_tag 'male.png', size: '30x30'
     end
   end
 
 
   def image_tag_medium
     if model.image?
-      image_tag model.image.medium, class: 'img-circle'
+      image_tag model.image.medium, class: 'img-circle', size: '150x150'
     else
-      image_tag 'male.png'
+      image_tag 'male.png', size: '150x150'
     end
   end
 
--- app/decorators/public_activity/activity_decorator.rb
+++ app/decorators/public_activity/activity_decorator.rb
@@ -3,30 +3,32 @@ class PublicActivity::ActivityDecorator < Draper::Decorator
   decorates PublicActivity::Activity
   delegate_all
 
+  decorates_association :owner
+
   def key_icon
     case model.key
     when 'location.create'
-      t('update_metering_point')
+      h.content_tag(:i, '', :class => "fa fa-plus-square fa-2x")
 
     when 'location.update'
-      t('update_metering_point')
+      h.content_tag(:i, '', :class => "fa fa-refresh fa-2x")
 
-    when 'location.delete'
-      t('update_metering_point')
+    when 'location.destroy'
+      h.content_tag(:i, '', :class => "fa fa-minus-square fa-2x")
+
 
     when 'metering_point.create'
-      t('create_metering_point')
+      h.content_tag(:i, '', :class => "fa fa-plus-square fa-2x")
 
     when 'metering_point.update'
-      h.content_tag :span, :class => "fa-stack fa-lg" do
-        content_tag(:i, '', :class => "fa fa-square-o fa-stack-2x") +
-        content_tag(:i, '', :class => "fa fa-home fa-stack-1x")
-      end
+      h.content_tag(:i, '', :class => "fa fa-refresh fa-2x")
 
-    when 'metering_point.delete'
-      t('delete_metering_point')
+    when 'metering_point.destroy'
+      h.content_tag(:i, '', :class => "fa fa-minus-square fa-2x")
 
     end
   end
 
 end
+
+
