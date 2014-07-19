89d8dc92dfc925754e20be039bdd73bbe5a0ccad
Felix Faerber <mail@ffaerber.com>
2014-07-19 18:48:36 +0200

    bla

--- app/assets/javascripts/groups.js.coffee
+++ app/assets/javascripts/groups.js.coffee
@@ -3,3 +3,7 @@ $(".groups.show").ready ->
   $("#mytab a").click (e) ->
     e.preventDefault()
     $(this).tab "show"
+
+  for metering_point in gon.metering_points
+
+    console.log metering_point
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,22 +1,13 @@
 class GroupsController < InheritedResources::Base
   respond_to :html
 
-
   def show
     @group                = Group.find(params[:id]).decorate
     @metering_points      = @group.metering_points
-
     @users                = @group.users
     @out_users            = @metering_points.collect(&:users).first
-
-    # @out_metering_points  = MeteringPoint.by_group_id_and_mode_eq(@group.id, :out).decorate
-    # @in_metering_points   = MeteringPoint.by_group_id_and_mode_eq(@group.id, :in).decorate
-
-    # @out_users            = @out_metering_points.collect(&:users).first
-    # @in_users             = @in_metering_points.collect(&:users).first
-
-    #@out_devices          = Device.all.decorate
     @groups               = Group.all.decorate
+    gon.push({ metering_points: @group.metering_points.collect(&:day_to_hours) })
   end
 
   def permitted_params
