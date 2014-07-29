f5549d782879f61a0212d25009f362f85ab051a3
Felix Faerber <mail@ffaerber.com>
2014-07-29 20:38:08 +0200

    mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,9 +65,9 @@ Buzzn::Application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = false
+  #config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { :host => "www.buzzn.net" }
-  config.action_mailer.delivery_method = :sendmail #:amazon_ses
+  config.action_mailer.delivery_method = :amazon_ses
 
 
 
