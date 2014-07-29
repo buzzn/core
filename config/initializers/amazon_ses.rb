68af57c0df43700d4b3d5fabd284e4d22f18d472
Felix Faerber <mail@ffaerber.com>
2014-07-29 23:10:13 +0200

    mail

--- config/initializers/amazon_ses.rb
+++ config/initializers/amazon_ses.rb
@@ -1,3 +1,3 @@
 ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
-  :access_key_id     => Rails.application.secrets.aws_access_key,
-  :secret_access_key => Rails.application.secrets.aws_secret_access_key
+  :access_key_id     => 'AKIAJ4F2IFTNLLEZTWPQ',
+  :secret_access_key => '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'
