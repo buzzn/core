a2f739e1793046857eecfcd9022f2dbd221c6d68
Christian <christian@buzzn.net>
2014-08-11 12:47:27 +0200

    asset
    Signed-off-by: Christian <christian@buzzn.net>

--- app/decorators/asset_decorator.rb
+++ app/decorators/asset_decorator.rb
@@ -9,7 +9,7 @@ class AssetDecorator < Draper::Decorator
 
   def image_tag_small
     if model.image?
-      image_tag model.image.small, class: 'img-circle', size: '45x45'
+      image_tag model.image.small, class: 'img-thumbnail', size: '140x140'
     else
       content_tag(:i, '', class: 'fa fa-bolt')
     end
