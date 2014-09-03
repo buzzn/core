765ec2734eac025ca405526a29313958611731e7
Felix Faerber <mail@ffaerber.com>
2014-09-03 16:24:24 +0200

    update workers

--- app/workers/meter_reading_update_worker.rb
+++ app/workers/meter_reading_update_worker.rb
@@ -1,6 +1,6 @@
 class MeterReadingUpdateWorker
   include Sidekiq::Worker
-  sidekiq_options queue: "high"
+  # sidekiq_options queue: "high"
   # sidekiq_options retry: false
 
   def perform(register_id, manufacturer_device_number, msp_name, msp_login_username, msp_login_password, start_time, end_time)
@@ -8,15 +8,27 @@ class MeterReadingUpdateWorker
     case msp_name
     when 'discovergy'
       discovergy = Discovergy.new(msp_login_username, msp_login_password, "EASYMETER_#{manufacturer_device_number}")
-      result     = discovergy.call(start_time, end_time)
-      time       = result['result'].first['time']
-      energy     = result['result'].first['energy']
+      request     = discovergy.call(start_time, end_time)
 
-      Reading.create(
-        register_id:  register_id,
-        timestamp:    DateTime.strptime(time.to_s,'%Q'),
-        watt_hour:    energy
-      )
+      if request['status'] == "ok"
+        if request['result'].any?
+          time       = request['result'].first['time']
+          energy     = request['result'].first['energy']
+          Reading.create(
+            register_id:  register_id,
+            timestamp:    DateTime.strptime(time.to_s,'%Q'),
+            watt_hour:    energy
+          )
+        else
+          logger.warn "MeterReadingUpdateWorker: No result form request. starttime: #{start_time}, endtime: #{end_time}"
+        end
+      elsif request['status'] == "error"
+        logger.warn request
+        register = Register.find(register_id)
+        register.metering_point
+      else
+        logger.error request
+      end
 
     when 'fluxo'
       puts "It's fluxo"
