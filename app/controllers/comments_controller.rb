b8ea3dda7ee376a5f7c2ec992859f446e093f8d2
Felix Faerber <mail@ffaerber.com>
2014-10-01 16:18:34 +0200

    fix

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,13 +7,16 @@ class CommentsController < InheritedResources::Base
     @obj = @comment_hash[:commentable_type].constantize.find(@comment_hash[:commentable_id])
     # Not implemented: check to see whether the user has permission to create a comment on this object
     @comment = Comment.build_from(@obj, current_user.id, @comment_hash[:body])
-    if @comment.save
-      render :partial => "comments/comment", :locals => { :comment => @comment }, :layout => false, :status => :created
-    else
-      render :js => "alert('error saving comment');"
+
+    respond_to do |format|
+      if @comment.save
+        format.js { @user }
+      else
+        format.js { "alert('error saving comment');" }
+      end
     end
-    create!
   end
+
 
   def destroy
     @comment = Comment.find(params[:id])
