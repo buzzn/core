169db66834f79eb4d0a3f8ab057d4a5ad7a5a956
Felix Faerber <mail@ffaerber.com>
2014-10-17 14:16:03 +0200

    make test break

--- spec/features/location_spec.rb
+++ spec/features/location_spec.rb
@@ -12,28 +12,28 @@ feature 'Location' do
       click_button 'Sign in'
     end
 
-    # it 'will be signed in' do
-    #   page.should have_content('Signed in successfully.')
-    # end
+    it 'will be signed in' do
+      page.should have_content('Signed in successfully.')
+    end
 
-    # it 'will fail to create location' do
-    #   visit "/profiles/#{@user.profile.slug}"
-    #   click_link 'Create New Location'
+    it 'will fail to create location' do
+      visit "/profiles/#{@user.profile.slug}"
+      click_link 'Create New Location'
 
-    #   fill_in :location_address_attributes_street_name,   with: 'Urbanstraße'
-    #   fill_in :location_address_attributes_street_number, with: 88
-    #   fill_in :location_address_attributes_zip,           with: 10967
-    #   fill_in :location_address_attributes_city,          with: 'Berlin'
+      fill_in :location_address_attributes_street_name,   with: 'Urbanstraße'
+      fill_in :location_address_attributes_street_number, with: 88
+      fill_in :location_address_attributes_zip,           with: 10967
+      fill_in :location_address_attributes_city,          with: 'Berlin'
 
-    #   click_button 'Create Location'
-    #   page.should have_content('Your ')
+      click_button 'Create Location'
+      page.should have_content('Your ')
 
-    #   #fill_in :location_address_attributes_state, with: 'Berlin'
+      #fill_in :location_address_attributes_state, with: 'Berlin'
 
 
-    #   # find("Add New Device", match: :first).click
+      # find("Add New Device", match: :first).click
 
-    # end
+    end
 
 
   end
