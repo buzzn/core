7f3ac61782ba5501aef2603160335aac6ae3c43a
Felix Faerber <mail@ffaerber.com>
2014-10-29 22:58:08 +0100

    add retray

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -10,7 +10,7 @@ Buzzn::Application.configure do
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
   # preloads Rails for running tests, you may have to set it to true.
-  config.eager_load = false
+  config.eager_load = true
 
   # Configure static asset server for tests with Cache-Control for performance.
   config.serve_static_assets  = true
@@ -35,5 +35,5 @@ Buzzn::Application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
-  #config.allow_concurrency = false # TODO https://github.com/rails/rails/issues/15089
+  config.allow_concurrency = false # TODO https://github.com/rails/rails/issues/15089
 end
