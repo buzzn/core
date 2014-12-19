4ae5b45ac049cdea027f72757bcf9792555df06e
Felix Faerber <mail@ffaerber.com>
2014-12-19 15:40:05 +0100

    no dead for pusher queue

--- app/workers/push_reading_worker.rb
+++ app/workers/push_reading_worker.rb
@@ -1,6 +1,6 @@
 class PushReadingWorker
   include Sidekiq::Worker
-  sidekiq_options :retry => false
+  sidekiq_options :retry => false, :dead => false
 
   def perform(register_id, watt_hour)
     Pusher.trigger("register_#{register_id}", 'new_reading', {:watt_hour => "Aktueller Zählerstand: #{watt_hour / 1000.0} kWh", :register_id => "#{register_id}"})
