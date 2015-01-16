dc306d4b317d949fbdd61fe75211d0ec661bb792
Felix Faerber <mail@ffaerber.com>
2015-01-16 16:16:29 +0100

    test

--- app/controllers/api/v1/Registers.rb
+++ app/controllers/api/v1/Registers.rb
@@ -3,17 +3,17 @@ module API
     class Registers < Grape::API
       include API::V1::Defaults
 
-      resource :registers do
+      # resource :registers do
 
-        desc "Get Register by ID"
-        params do
-          requires :id, type: String, desc: "ID of the Register"
-        end
-        get ":id", root: "register" do
-          Register.where(id: permitted_params[:id]).first!
-        end
+      #   desc "Get Register by ID"
+      #   params do
+      #     requires :id, type: String, desc: "ID of the Register"
+      #   end
+      #   get ":id", root: "register" do
+      #     Register.where(id: permitted_params[:id]).first!
+      #   end
 
-      end
+      # end
 
     end
   end
