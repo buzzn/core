1267bcfdb1e15a3c94eae24734f836d311bccb03
Felix Faerber <ffaerber@gmail.com>
2015-02-26 17:58:40 +0100

    cover

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -30,7 +30,7 @@ class PictureUploader < CarrierWave::Uploader::Base
 
 
   version :cover do
-    process :resize_to_fill  => [1000, 400]
+    process :resize_to_fill  => [1000, 550]
   end
 
   version :sm do
