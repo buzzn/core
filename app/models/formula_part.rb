ee31beaa78112a7e5670323b5c513066e98e467e
Felix Faerber <ffaerber@gmail.com>
2015-03-25 19:30:42 +0100

    gtoup adable

--- app/models/formula_part.rb
+++ app/models/formula_part.rb
@@ -2,7 +2,7 @@ class FormulaPart < ActiveRecord::Base
   belongs_to :metering_point
   belongs_to :operand, class_name: 'MeteringPoint'
 
-  validates :operand_id, presence: true
-  validates :metering_point_id, presence: true
+  # validates :operand_id, presence: true
+  # validates :metering_point_id, presence: true
 
 end
