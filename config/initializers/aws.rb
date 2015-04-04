b0ae642a0b88bc9ecbbee950ac8d3ca95d04a1d2
Felix Faerber <ffaerber@gmail.com>
2015-04-04 14:32:25 +0200

    aws

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,9 +1,4 @@
-require 'logger'
-if Rails.env.production?
-  Aws.config(
-    :access_key_id      => Rails.application.secrets.aws_access_key,
-    :secret_access_key  => Rails.application.secrets.aws_secret_access_key,
-    :region             => Rails.application.secrets.aws_region,
-    :logger             => Logger.new($stdout)
-  )
-end
+Aws.config.update({
+  region: Rails.application.secrets.aws_region,
+  credentials: Aws::Credentials.new( 'Rails.application.secrets.aws_access_key', 'Rails.application.secrets.aws_secret_access_key' )
+})
