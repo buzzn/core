7f926a3e4f980bb9efbca6eeb14a58e98b369183
Felix Faerber <ffaerber@gmail.com>
2015-04-04 14:36:32 +0200

    aws

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,4 +1,4 @@
 Aws.config.update({
   region: Rails.application.secrets.aws_region,
-  credentials: Aws::Credentials.new( 'Rails.application.secrets.aws_access_key', 'Rails.application.secrets.aws_secret_access_key' )
+  credentials: Aws::Credentials.new( Rails.application.secrets.aws_access_key, Rails.application.secrets.aws_secret_access_key )
 })
