0149ff27f6a048b2421aa13be9de763bb0c48c85
Felix Faerber <ffaerber@gmail.com>
2015-04-13 23:34:28 +0200

    fix

--- app/workers/push_reading_worker.rb
+++ app/workers/push_reading_worker.rb
@@ -2,7 +2,7 @@ class PushReadingWorker
   include Sidekiq::Worker
   sidekiq_options :retry => false, :dead => false
 
-  def perform(metering_point_id, watt_hour, timestamp)
-    Pusher.trigger("metering_point_#{metering_point_id}", 'new_reading', :watt_hour => watt_hour, :timestamp => timestamp, :metering_point_id => "#{metering_point_id}")
+  def perform(metering_point_id, watt_hour, power, timestamp)
+    Pusher.trigger("metering_point_#{metering_point_id}", 'new_reading', :watt_hour => watt_hour, :power => power, :timestamp => timestamp, :metering_point_id => "#{metering_point_id}")
   end
 end
