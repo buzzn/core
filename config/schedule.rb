d4dd8dc450c86d1605176b897583b77a2a0ff389
Felix Faerber <ffaerber@gmail.com>
2015-05-08 11:05:27 +0200

    time

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,7 +3,7 @@ env :PATH, '/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/aw
 set :output, 'log/cron.log'
 
 
-every 1.minute do
+every 2.minute do
   runner "Meter.pull_readings"
 end
 
