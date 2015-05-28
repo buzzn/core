99464c85331cfd6eaf0cccd2a56d8243247ea567
Felix Faerber <ffaerber@gmail.com>
2015-05-28 17:07:23 +0200

    fix

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -63,7 +63,7 @@ Buzzn::Application.configure do
                                       }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = "//#{Rails.application.secrets.hostname}"
+  config.action_controller.asset_host = Rails.application.secrets.hostname
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
