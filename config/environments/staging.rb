e4f752d3f281e6b68bad6f1c8f02ae4be43dda51
Felix Faerber <ffaerber@gmail.com>
2015-05-28 17:01:46 +0200

    fix

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -25,8 +25,8 @@ Buzzn::Application.configure do
   config.serve_static_files = false
 
   # Compress JavaScripts and CSS.
-  config.assets.compress = false
-  # config.assets.js_compressor  = :uglifier # Uglifier.new(mangle: false)
+  config.assets.compress = true
+  config.assets.js_compressor  = :uglifier # Uglifier.new(mangle: false)
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
@@ -40,7 +40,7 @@ Buzzn::Application.configure do
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
@@ -63,7 +63,7 @@ Buzzn::Application.configure do
                                       }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = "//#{Rails.application.secrets.hostname}"
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
