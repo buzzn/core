0af6f88adcb34d9c34cc1f5e6d89043dd6aafd6a
Felix Faerber <ffaerber@gmail.com>
2015-06-16 16:23:07 +0200

    root path stream_index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,14 +18,15 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
 
-    sign_in_url = url_for(:action => 'new', :controller => 'sessions', :only_path => false, :protocol => 'http')
-    if request.referer == sign_in_url
-      super
-    else
-      stored_location_for(resource) || request.referer || root_path
-    end
+    # sign_in_url = url_for(:action => 'new', :controller => 'sessions', :only_path => false)
 
-    current_user.profile
+    # if request.referer == sign_in_url
+    #   super
+    # else
+    #   stored_location_for(resource) || request.referer || root_path
+    # end
+
+    stream_index_path
   end
 
   def current_user
