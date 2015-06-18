35a743209468b79d41091cf469700404a5f7a24b
Christian <christian@buzzn.net>
2015-06-18 12:00:37 +0200

    fix

--- app/controllers/stream_controller.rb
+++ app/controllers/stream_controller.rb
@@ -8,8 +8,6 @@ class StreamController < ApplicationController
                               .where("owner_id in (?) OR recipient_id in (?)", @user_ids, @user_ids)
                               .where("owner_type = (?) OR recipient_type = (?)", "User", "User")
                               .limit(10)
-      @explore_groups       = Group.all.limit(10).decorate
-      @explore_profiles     = User.all.where("id NOT IN (?)", current_user.friend_ids + [current_user.id]).limit(10).collect{|u| u.profile.decorate}
     else
       redirect_to new_user_session_path
     end
