40ca22cd4036368b88863fd56171d39426325190
Felix Faerber <ffaerber@gmail.com>
2015-06-26 14:32:03 +0200

    fix

--- app/models/metering_point.rb
+++ app/models/metering_point.rb
@@ -87,9 +87,9 @@ class MeteringPoint < ActiveRecord::Base
       end
       return {:power => 0, :timestamp => 0}
     else
-      @last_reading = Reading.last_by_metering_point_id(self.id)
-      if @last_reading && @last_reading[:power]
-        {:power => @last_reading[:power]/1000, :timestamp => @last_reading[:timestamp].to_i*1000}
+      last_reading = Reading.last_by_metering_point_id(self.id)
+      if last_reading && last_reading[:power]
+        {:power => last_reading[:power]/1000, :timestamp => last_reading[:timestamp].to_i*1000}
       else
         {:power => 0, :timestamp => 0}
       end
