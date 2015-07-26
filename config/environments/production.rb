9dfa2c97c24fef30a7690d3b868b1025add11494
Felix Faerber <ffaerber@gmail.com>
2015-07-26 11:36:32 +0200

    force ssl on staging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Buzzn::Application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
@@ -62,7 +62,6 @@ Buzzn::Application.configure do
                                         :namespace => "cache"
                                       }
 
-  config.force_ssl = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   config.action_controller.asset_host = Rails.application.secrets.asset_host
