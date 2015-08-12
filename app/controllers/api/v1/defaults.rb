25f60470f9bf01dc15655518f7164bb5af96f660
Felix Faerber <ffaerber@gmail.com>
2015-08-12 21:28:03 +0200

    update grape

--- app/controllers/api/v1/defaults.rb
+++ app/controllers/api/v1/defaults.rb
@@ -7,7 +7,6 @@ module API
         prefix "api"
         version "v1", using: :path
         default_format :json
-        format :json
         formatter :json, Grape::Formatter::ActiveModelSerializers
 
         helpers do
