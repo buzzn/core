7380938c8ef3fe4eea722535a5aa5add9cb4ab72
Christian <christian@buzzn.net>
2015-09-15 13:43:52 +0200

    profile picture

--- app/decorators/profile_decorator.rb
+++ app/decorators/profile_decorator.rb
@@ -24,21 +24,33 @@ class ProfileDecorator < Draper::Decorator
       })
   end
 
-  def picture(size)
-    if model.image
+  def picture(size=nil)
+    if model.image.present?
       if size == 'lg'
-        image_tag current_user.profile.image.lg, class: 'img-circle img-user media-object', alt: ""
-      elsif size == 'sm'
-        image_tag current_user.profile.image.sm, class: 'img-circle img-user media-object', alt: ""
+        image_tag model.image.lg, class: 'img-lg img-circle img-user media-object', alt: ""
       elsif size == 'md'
-        image_tag current_user.profile.image.md, class: 'img-circle img-user media-object', alt: ""
+        image_tag model.image.md, class: 'img-md img-circle img-user media-object', alt: ""
+      elsif size == 'sm'
+        image_tag model.image.sm, class: 'img-sm img-circle img-user media-object', alt: ""
+      elsif size == 'xs'
+        image_tag model.image.md, class: 'img-xs img-circle img-user media-object', alt: ""
       elsif size == 'cover'
-        image_tag current_user.profile.image.cover, class: 'img-circle img-user media-object', alt: ""
+        image_tag model.image.cover, class: 'img-circle img-user media-object', alt: ""
       elsif size == 'big_tumb'
-        image_tag current_user.profile.image.big_tumb, class: 'img-circle img-user media-object', alt: ""
+        image_tag model.image.big_tumb, class: 'img-circle img-user media-object', alt: ""
       end
     else
-
+      if size == 'lg'
+        content_tag(:span, nil, class: 'img-lg imc-circle icon-wrapper-lg icon-circle bg-success fa fa-user fa-5x')
+      elsif size == 'md'
+        content_tag(:span, nil, class: 'img-md imc-circle icon-wrapper-md icon-circle bg-success fa fa-user fa-3x')
+      elsif size == 'sm'
+        content_tag(:span, nil, class: 'img-sm imc-circle icon-wrapper-sm icon-circle bg-success fa fa-user fa-2x')
+      elsif size == 'xs'
+        content_tag(:span, nil, class: 'img-xs imc-circle icon-wrapper-xs icon-circle bg-success fa fa-user')
+      else
+        content_tag(:i, nil, class: 'img-xxs imc-circle icon-wrapper-xxs icon-circle bg-success fa fa-user')
+      end
     end
   end
 
