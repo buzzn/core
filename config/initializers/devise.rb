f6c9b4c107f3ff6bbb239ff71b07ced7c4e08134
Felix Faerber <ffaerber@gmail.com>
2015-09-21 17:03:02 +0200

    remove favebook login

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -268,12 +268,12 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth(
-    :facebook,
-    Rails.application.secrets.facebook_app_id,
-    Rails.application.secrets.facebook_secret,
-    secure_image_url: true
-    )
+  # config.omniauth(
+  #   :facebook,
+  #   Rails.application.secrets.facebook_app_id,
+  #   Rails.application.secrets.facebook_secret,
+  #   secure_image_url: true
+  #   )
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
