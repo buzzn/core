c3fd5a33312dd2fb5880514549366a90362ac1d6
Felix Faerber <ffaerber@gmail.com>
2015-10-21 12:19:08 +0200

    pusher

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,3 +1,5 @@
 Pusher.app_id = Rails.application.secrets.pusher_app_id
 Pusher.key    = Rails.application.secrets.pusher_key
 Pusher.secret = Rails.application.secrets.pusher_secret
+Pusher.host   = Rails.application.secrets.pusher_host
+Pusher.port   = Rails.application.secrets.pusher_port
