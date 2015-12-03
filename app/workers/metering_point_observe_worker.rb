cda30b55f4ca6c1345230fa87cdd836657340173
Christian <christian@buzzn.net>
2015-12-03 12:43:09 +0100

    fix observe

--- app/workers/metering_point_observe_worker.rb
+++ app/workers/metering_point_observe_worker.rb
@@ -10,12 +10,12 @@ class MeteringPointObserveWorker
       if @metering_point.observe_offline && @metering_point.last_observed_timestamp
         if Time.now >= @metering_point.last_observed_timestamp + 1.hour && Time.now <= @metering_point.last_observed_timestamp + 1.hour + 3.minutes
           [@metering_point.users + @metering_point.managers].flatten.uniq.each do |user|
-            Notifier.send_email_notification_meter_offline(user, @metering_point)
+            Notifier.send_email_notification_meter_offline(user, @metering_point).deliver_now
           end
         end
       end
     else
-      @metering_point.last_observed_timestamp = @last_reading[:timestamp]
+      @metering_point.last_observed_timestamp = Time.at(@last_reading[:timestamp]/1000).in_time_zone
       @metering_point.save
       if @current_power < @metering_point.min_watt
         message = I18n.t('metering_point_undershot_min_watt', @metering_point.min_watt)
@@ -31,7 +31,7 @@ class MeteringPointObserveWorker
     if message
       @metering_point.users.each do |user|
         user.send_notification('info', message, @metering_point.name, 0, Rails.application.routes.url_helpers.metering_point_path(@metering_point))
-        Notifier.send_email_metering_point_exceeds_or_undershoots(user, @metering_point, mode)
+        Notifier.send_email_metering_point_exceeds_or_undershoots(user, @metering_point, mode).deliver_now
       end
     end
 
