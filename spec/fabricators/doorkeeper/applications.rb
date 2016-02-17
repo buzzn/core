97545ca88e7f473f91c1f4d41f83d1799b680605
Felix Faerber <ffaerber@gmail.com>
2016-02-17 12:46:46 +0100

    Doorkeeper scopes for Fabricator

--- spec/fabricators/doorkeeper/applications.rb
+++ spec/fabricators/doorkeeper/applications.rb
@@ -1,4 +1,5 @@
 Fabricator :application, class_name: Doorkeeper::Application do
   name { sequence(:name) { |i| "Application #{i}"  } }
   redirect_uri 'https://app.com/callback'
+  scopes ['public', 'write']
 end
