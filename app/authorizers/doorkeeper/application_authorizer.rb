85664a17df70ae43bdf49a6f2c75da1d755dcef2
Felix Faerber <ffaerber@gmail.com>
2016-02-17 12:44:50 +0100

    add Doorkeeper application authorizer

---
+++ app/authorizers/doorkeeper/application_authorizer.rb
@@ -0,0 +1,22 @@
+class Doorkeeper::ApplicationAuthorizer < ApplicationAuthorizer
+
+  def self.creatable_by?(user)
+    true
+  end
+
+  def updatable_by?(user)
+    user.has_role?(:admin) ||
+    user.has_role?(:manager, resource)
+  end
+
+  def deletable_by?(user)
+    user.has_role?(:admin) ||
+    user.has_role?(:manager, resource)
+  end
+
+  def readable_by?(user)
+    user.has_role?(:admin) ||
+    user.has_role?(:manager, resource)
+  end
+
+end
