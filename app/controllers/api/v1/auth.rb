c6c2f6a1f48341207f463fb8e3f431d6b83fdd15
Felix Faerber <ffaerber@gmail.com>
2016-02-19 18:16:54 +0100

    remove cammel case

--- app/controllers/api/v1/auth.rb
+++ app/controllers/api/v1/auth.rb
@@ -7,14 +7,14 @@ module API
 
         desc "Return the Access Token"
         params do
-          requires :authorizationCode, type: String, desc: "Authorization Code"
+          requires :authorization_code, type: String, desc: "Authorization Code"
         end
         post 'token' do
-          authorization_code = params[:authorizationCode]
-          grant = Doorkeeper::AccessGrant.by_token(authorization_code)
-          app   = grant.application
-          client = OAuth2::Client.new(app.uid, app.secret, :site => "http://localhost:3000")
-          token  = client.auth_code.get_token(authorization_code, :redirect_uri => app.redirect_uri)
+          code   = params[:authorization_code]
+          grant  = Doorkeeper::AccessGrant.by_token(code)
+          app    = grant.application
+          client = OAuth2::Client.new(app.uid, app.secret, :site => Rails.application.secrets.hostname)
+          token  = client.auth_code.get_token(code, :redirect_uri => app.redirect_uri)
           return token
         end
 
