f421df288869ed95312c21f2ac5fdacecca01cb3
Felix Faerber <ffaerber@gmail.com>
2016-05-09 11:13:54 +0200

    fix reading tests

--- spec/fabricators/readings.rb
+++ spec/fabricators/readings.rb
@@ -3,3 +3,12 @@ Fabricator :reading do
   watt_hour { sequence(:watt_hour, 271400000) }
   power     { 900 }
 end
+
+
+Fabricator :reading_with_metering_point, from: :reading do
+  metering_point_id { Fabricate(:metering_point).id  }
+end
+
+Fabricator :reading_with_metering_point_and_manager, from: :reading do
+  metering_point_id { Fabricate(:metering_point_with_manager).id  }
+end
