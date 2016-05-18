0bbc0b54d1e6389635de0da8d04f08d1ac3f6b6a
Felix Faerber <ffaerber@gmail.com>
2016-05-18 18:19:24 +0200

    aktivate tests

--- spec/requests/api/v1/aggregate_spec.rb
+++ spec/requests/api/v1/aggregate_spec.rb
@@ -162,16 +162,6 @@ describe "Aggregate API" do
     end
   end
 
-  #
-  # xit 'does aggregate multiple metering_points slp chart by year_to_months as admin' do
-  # end
-  #
-  # xit 'does aggregate multiple metering_points slp chart by month_to_days as admin' do
-  # end
-  #
-  # xit 'does aggregate multiple metering_points slp chart by day_to_minutes as admin' do
-  # end
-
 
   it 'does aggregate multiple metering_points slp chart by hour_to_minutes as admin' do
     access_token = Fabricate(:admin_access_token)
@@ -250,146 +240,143 @@ describe "Aggregate API" do
 
 
 
-  #
-  #
-  # it 'does aggregate slp metering_point power without forecast_kwh_pa as admin' do
-  #   access_token = Fabricate(:admin_access_token)
-  #   metering_point = Fabricate(:metering_point)
-  #
-  #   energy_a_milliwatt_hour = 0
-  #   timestamp = Time.new(2016,2,1)
-  #   (24*4).times do |i|
-  #     Fabricate(:reading,
-  #       source: 'slp',
-  #       timestamp: timestamp,
-  #       energy_a_milliwatt_hour: energy_a_milliwatt_hour,
-  #       power_milliwatt: 930*1000+i
-  #     )
-  #     energy_a_milliwatt_hour += 1300*1000
-  #     timestamp += 15.minutes
-  #   end
-  #
-  #   Timecop.freeze(Time.local(2016,2,1, 1,30,1)) # 6*15 minutes and 1 seconds
-  #   request_params = {
-  #     metering_point_ids: metering_point.id
-  #   }
-  #
-  #   get_with_token "/api/v1/aggregate/power", request_params, access_token.token
-  #
-  #   expect(response).to have_http_status(200)
-  #   expect(response.body.to_i).to eq(930*1000 + 6) # 6*15 minutes
-  # end
-  #
-  #
-  #
-  #
-  # it 'does aggregate slp metering_point power with forecast_kwh_pa as admin' do
-  #   access_token = Fabricate(:admin_access_token)
-  #
-  #   metering_point = Fabricate(:metering_point, forecast_kwh_pa: 3000)
-  #   energy_a_milliwatt_hour = 0
-  #   timestamp = Time.new(2016,2,1)
-  #   (24*4).times do |i|
-  #     Fabricate(:reading,
-  #       source: 'slp',
-  #       timestamp: timestamp,
-  #       energy_a_milliwatt_hour: energy_a_milliwatt_hour,
-  #       power_milliwatt: 900*1000+i
-  #     )
-  #     energy_a_milliwatt_hour += 1300*1000
-  #     timestamp += 15.minutes
-  #   end
-  #
-  #   Timecop.freeze(Time.local(2016,2,1, 1,30,1)) # 6*15 minutes and 1 seconds
-  #   request_params = {
-  #     metering_point_ids: metering_point.id
-  #   }
-  #
-  #   get_with_token "/api/v1/aggregate/power", request_params, access_token.token
-  #
-  #   expect(response).to have_http_status(200)
-  #   expect(response.body.to_i).to eq((900*1000 + 6)*3000/900.0) # forcast_formel = forecast / slp_power
-  # end
-  #
-  #
-  # it 'does aggregate multiple metering_point slp power with forecast_kwh_pa as admin' do
-  #   access_token = Fabricate(:admin_access_token)
-  #
-  #   metering_point1 = Fabricate(:metering_point, forecast_kwh_pa: 3000)
-  #   metering_point2 = Fabricate(:metering_point, forecast_kwh_pa: 3000)
-  #
-  #   energy_a_milliwatt_hour = 0
-  #   timestamp = Time.new(2016,2,1)
-  #   (24*4).times do |i|
-  #     Fabricate(:reading,
-  #       source: 'slp',
-  #       timestamp: timestamp,
-  #       energy_a_milliwatt_hour: energy_a_milliwatt_hour,
-  #       power_milliwatt: 900*1000+i
-  #     )
-  #     energy_a_milliwatt_hour += 1300*1000
-  #     timestamp += 15.minutes
-  #   end
-  #
-  #   Timecop.freeze(Time.local(2016,2,1, 1,30,1)) # 6*15 minutes and 1 seconds
-  #   request_params = {
-  #     metering_point_ids: "#{metering_point1.id},#{metering_point2.id}"
-  #   }
-  #
-  #   get_with_token "/api/v1/aggregate/power", request_params, access_token.token
-  #
-  #   expect(response).to have_http_status(200)
-  #   expect(response.body.to_i).to eq(2*((900*1000 + 6)*3000/900.0)) # forcast_formel = forecast / slp_power
-  # end
-  #
-  #
-  #
-  # it 'does aggregate multiple buzzn metering_points power as admin' do
-  #   access_token = Fabricate(:admin_access_token)
-  #
-  #   meter1 = Fabricate(:meter_smart_with_metering_point)
-  #   meter2 = Fabricate(:meter_smart_with_metering_point)
-  #
-  #   energy_a_milliwatt_hour = 0
-  #   timestamp = Time.new(2016,2,1)
-  #   (3*60*30).times do |i|
-  #
-  #     Fabricate(:reading,
-  #       metering_point_id: meter1.metering_points.first.id,
-  #       timestamp: timestamp,
-  #       energy_a_milliwatt_hour: energy_a_milliwatt_hour,
-  #       power_milliwatt: 900*1000+i
-  #     )
-  #
-  #     Fabricate(:reading,
-  #       metering_point_id: meter2.metering_points.first.id,
-  #       timestamp: timestamp,
-  #       energy_a_milliwatt_hour: energy_a_milliwatt_hour,
-  #       power_milliwatt: 800*1000+i
-  #     )
-  #
-  #     energy_a_milliwatt_hour += 1300*1000
-  #     timestamp += 2.seconds
-  #   end
-  #
-  #   Timecop.freeze(Time.local(2016,2,1, 1,30)) # 6*15 minutes
-  #   request_params = {
-  #     metering_point_ids: "#{meter1.metering_points.first.id},#{meter2.metering_points.first.id}"
-  #   }
-  #
-  #   get_with_token "/api/v1/aggregate/power", request_params, access_token.token
-  #
-  #   expect(response).to have_http_status(200)
-  #   expect(response.body.to_i).to eq((900*1000 + 2700)+((800*1000 + 2700))) # 1 hour and 30 minutes every 2 seconds = (3600+1800)/2 = 2700
-  # end
 
 
-  #
-  # xit 'does aggregate discovergy metering_point power as admin' do
-  # end
-  #
-  # xit 'does aggregate buzzn-api metering_point power as admin' do
-  # end
+  it 'does aggregate slp metering_point power without forecast_kwh_pa as admin' do
+    access_token = Fabricate(:admin_access_token)
+    metering_point = Fabricate(:metering_point)
+
+    energy_a_milliwatt_hour = 0
+    timestamp = Time.new(2016,2,1)
+    (24*4).times do |i|
+      Fabricate(:reading,
+        source: 'slp',
+        timestamp: timestamp,
+        energy_a_milliwatt_hour: energy_a_milliwatt_hour,
+        power_milliwatt: 930*1000+i
+      )
+      energy_a_milliwatt_hour += 1300*1000
+      timestamp += 15.minutes
+    end
+
+    Timecop.freeze(Time.local(2016,2,1, 1,30,1)) # 6*15 minutes and 1 seconds
+    request_params = {
+      metering_point_ids: metering_point.id
+    }
+
+    get_with_token "/api/v1/aggregate/power", request_params, access_token.token
+
+    expect(response).to have_http_status(200)
+    expect(response.body.to_i).to eq(930*1000 + 6) # 6*15 minutes
+  end
+
+
+
+
+  it 'does aggregate slp metering_point power with forecast_kwh_pa as admin' do
+    access_token = Fabricate(:admin_access_token)
+
+    metering_point = Fabricate(:metering_point, forecast_kwh_pa: 3000)
+    energy_a_milliwatt_hour = 0
+    timestamp = Time.new(2016,2,1)
+    (24*4).times do |i|
+      Fabricate(:reading,
+        source: 'slp',
+        timestamp: timestamp,
+        energy_a_milliwatt_hour: energy_a_milliwatt_hour,
+        power_milliwatt: 900*1000+i
+      )
+      energy_a_milliwatt_hour += 1300*1000
+      timestamp += 15.minutes
+    end
+
+    Timecop.freeze(Time.local(2016,2,1, 1,30,1)) # 6*15 minutes and 1 seconds
+    request_params = {
+      metering_point_ids: metering_point.id
+    }
+
+    get_with_token "/api/v1/aggregate/power", request_params, access_token.token
+
+    expect(response).to have_http_status(200)
+    expect(response.body.to_i).to eq((900*1000 + 6)*3000/900.0) # forcast_formel = forecast / slp_power
+  end
+
+
+  it 'does aggregate multiple metering_point slp power with forecast_kwh_pa as admin' do
+    access_token = Fabricate(:admin_access_token)
+
+    metering_point1 = Fabricate(:metering_point, forecast_kwh_pa: 3000)
+    metering_point2 = Fabricate(:metering_point, forecast_kwh_pa: 3000)
+
+    energy_a_milliwatt_hour = 0
+    timestamp = Time.new(2016,2,1)
+    (24*4).times do |i|
+      Fabricate(:reading,
+        source: 'slp',
+        timestamp: timestamp,
+        energy_a_milliwatt_hour: energy_a_milliwatt_hour,
+        power_milliwatt: 900*1000+i
+      )
+      energy_a_milliwatt_hour += 1300*1000
+      timestamp += 15.minutes
+    end
+
+    Timecop.freeze(Time.local(2016,2,1, 1,30,1)) # 6*15 minutes and 1 seconds
+    request_params = {
+      metering_point_ids: "#{metering_point1.id},#{metering_point2.id}"
+    }
+
+    get_with_token "/api/v1/aggregate/power", request_params, access_token.token
+
+    expect(response).to have_http_status(200)
+    expect(response.body.to_i).to eq(2*((900*1000 + 6)*3000/900.0)) # forcast_formel = forecast / slp_power
+  end
+
+
+
+  it 'does aggregate multiple buzzn metering_points power as admin' do
+    access_token = Fabricate(:admin_access_token)
+
+    meter1 = Fabricate(:meter_smart_with_metering_point)
+    meter2 = Fabricate(:meter_smart_with_metering_point)
+
+    energy_a_milliwatt_hour = 0
+    timestamp = Time.new(2016,2,1)
+    (3*60*30).times do |i|
+
+      Fabricate(:reading,
+        metering_point_id: meter1.metering_points.first.id,
+        timestamp: timestamp,
+        energy_a_milliwatt_hour: energy_a_milliwatt_hour,
+        power_milliwatt: 900*1000+i
+      )
+
+      Fabricate(:reading,
+        metering_point_id: meter2.metering_points.first.id,
+        timestamp: timestamp,
+        energy_a_milliwatt_hour: energy_a_milliwatt_hour,
+        power_milliwatt: 800*1000+i
+      )
+
+      energy_a_milliwatt_hour += 1300*1000
+      timestamp += 2.seconds
+    end
+
+    Timecop.freeze(Time.local(2016,2,1, 1,30)) # 6*15 minutes
+    request_params = {
+      metering_point_ids: "#{meter1.metering_points.first.id},#{meter2.metering_points.first.id}"
+    }
+
+    get_with_token "/api/v1/aggregate/power", request_params, access_token.token
+
+    expect(response).to have_http_status(200)
+    expect(response.body.to_i).to eq((900*1000 + 2700)+((800*1000 + 2700))) # 1 hour and 30 minutes every 2 seconds = (3600+1800)/2 = 2700
+  end
+
+
+
+  xit 'does aggregate buzzn-api metering_point power as admin' do
+  end
 
 
 
@@ -415,7 +402,9 @@ describe "Aggregate API" do
     expect(json.count).to eq(1425) # not 1440 because last reading is at 23:45, 15 minutes are missing and day_to_minutes uses Discovergy getDayEveryFifteenMinutes Api
   end
 
-
+  xit 'does aggregate discovergy metering_point power as admin' do
+  end
+  
 
 
 
