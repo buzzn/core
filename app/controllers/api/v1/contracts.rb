882174c01ab933a97b8031d911e1f05043865711
ChipJuggler <scsi.rdx@gmail.com>
2016-06-07 01:39:41 +0700

    reworked contracts api controller, made it more 'grape-like' and refactored tests

--- app/controllers/api/v1/contracts.rb
+++ app/controllers/api/v1/contracts.rb
@@ -3,26 +3,16 @@ module API
     class Contracts < Grape::API
       include API::V1::Defaults
 
-      @@contract_params_names = [
-        'tariff',
-        'status',
-        'customer_number',
-        'contract_number',
-        'signing_user',
-        'terms',
-        'power_of_attorney',
-        'confirm_pricing_model',
-        'commissioning',
-        'mode',
-        'organization_id',
-      ]
-
       resource :contracts do
 
         desc 'Return all Contracts'
-        get root: :contracts do
-          doorkeeper_authorize! :public
-          Contract.all
+        paginate(per_page: per_page=10)
+        get do
+          doorkeeper_authorize! :admin
+          @per_page     = params[:per_page] || per_page
+          @page         = params[:page] || 1
+          @total_pages  = Contract.all.page(@page).per_page(@per_page).total_pages
+          paginate(render(Contract.all, meta: { total_pages: @total_pages }))
         end
 
         desc 'Return a Contract'
@@ -51,38 +41,37 @@ module API
         end
         post do
           doorkeeper_authorize! :admin
-          @params = params.contract || params
-          contract_params = Hash.new
-          @@contract_params_names.each do |param_name|
-            contract_params[param_name] = @params[param_name] if @params[param_name]
-          end
-          @contract = Contract.new(contract_params)
+          @params = declared(params, include_missing: false).contract || declared(params, include_missing: false)
+          @contract = Contract.new(@params)
           @contract.contracting_party = current_user.contracting_party if current_user.contracting_party
-          if @contract.organization.slug == 'buzzn-metering'
-            @contract.username = 'team@localpool.de'
-            @contract.password = 'Zebulon_4711'
-          end
           if @contract.save!
             current_user.add_role :manager, @contract
-            @contract.decorate
           end
+          @contract
         end
 
 
         desc 'Update a Contract'
         params do
           requires :id,           type: String, desc: 'Contract ID'
-          # requires :mode,         type: String, desc: 'Contract description'
+          optional :mode,                   type: String, desc: 'Contract description'
+          optional :organization_id,        type: String, desc: 'Organization id'
+          optional :tariff,                 type: String, desc: 'Tariff'
+          optional :status,                 type: String, desc: 'Status'
+          optional :customer_number,        type: String, desc: 'Customer number'
+          optional :contract_number,        type: String, desc: 'Contract number'
+          optional :signing_user,           type: String, desc: 'Signing user'
+          optional :terms,                  type: Boolean, desc: 'Terms'
+          optional :power_of_attorney,      type: Boolean, desc: 'Power of attorney'
+          optional :confirm_pricing_model,  type: Boolean, desc: 'Confirm pricing model'
+          optional :commissioning,          type: Date, desc: 'Commissioning'
         end
         put do
           doorkeeper_authorize! :admin
-          @params = params.contract || params
+          @params = declared(params, include_missing: false).contract || declared(params, include_missing: false)
           @contract = Contract.find(@params.id)
-          contract_params = Hash.new
-          @@contract_params_names.each do |param_name|
-            contract_params[param_name] = @params[param_name] if @params[param_name]
-          end
-          @contract.update_attributes(contract_params)
+          @params.delete('id')
+          @contract.update_attributes(@params)
           return @contract
         end
 
--- spec/requests/api/v1/contracts_spec.rb
+++ spec/requests/api/v1/contracts_spec.rb
@@ -4,6 +4,19 @@ describe 'Contracts API' do
     @org1 = Fabricate(:metering_point_operator, name: 'buzzn Metering')
     @org2 = Fabricate(:metering_point_operator, name: 'Discovergy')
     @org3 = Fabricate(:metering_point_operator, name: 'MySmartGrid')
+    @contract_param_names = [
+      'tariff',
+      'status',
+      'customer_number',
+      'contract_number',
+      'signing_user',
+      'terms',
+      'power_of_attorney',
+      'confirm_pricing_model',
+      'commissioning',
+      'mode',
+      'organization_id',
+    ]
   end
 
   it 'does not get all contracts without token' do
@@ -15,7 +28,6 @@ describe 'Contracts API' do
     contract_ids = [ Fabricate(:mpoc_buzzn_metering).id, Fabricate(:mpoc_ferraris_0002_amperix).id ]
     access_token = Fabricate(:admin_access_token).token
     get_with_token '/api/v1/contracts', {}, access_token
-    # binding.pry
     expect(response).to have_http_status(200)
     contracts = json['data'].reject { |contract| contract_ids.include?(contract['id']) }
     expect(contracts).to be_empty
@@ -34,47 +46,55 @@ describe 'Contracts API' do
     expect(response).to have_http_status(200)
   end
 
+  it 'does not get contract with token for wrong id' do
+    id = Random.new().rand(10000)
+    access_token  = Fabricate(:access_token).token
+    get_with_token "/api/v1/contracts/#{id}random", {}, access_token
+    expect(response).to have_http_status(404)
+  end
+
   it 'does not create contract without token' do
     contract = Fabricate.build(:mpoc_buzzn_metering)
     request_params = Hash.new
-    [
-      'tariff',
-      'status',
-      'customer_number',
-      'contract_number',
-      'signing_user',
-      'terms',
-      'power_of_attorney',
-      'confirm_pricing_model',
-      'commissioning',
-      'mode'
-    ].each { |param_name| request_params[param_name] = contract[param_name] }
-    request_params['organization_id'] = @org2.id
+    @contract_param_names.each { |param_name| request_params[param_name] = contract[param_name] }
     post_without_token "/api/v1/contracts/", request_params.to_json
-    # binding.pry
     expect(response).to have_http_status(401)
   end
 
   it 'create contract with admin token' do
     contract = Fabricate.build(:mpoc_buzzn_metering)
     request_params = Hash.new
-    [
-      'tariff',
-      'status',
-      'customer_number',
-      'contract_number',
-      'signing_user',
-      'terms',
-      'power_of_attorney',
-      'confirm_pricing_model',
-      'commissioning',
-      'mode',
-    ].each { |param_name| request_params[param_name] = contract[param_name] }
-    request_params['organization_id'] = @org2.id
+    @contract_param_names.each { |param_name| request_params[param_name] = contract[param_name] }
     access_token  = Fabricate(:admin_access_token).token
     post_with_token "/api/v1/contracts/", request_params.to_json, access_token
-    # binding.pry
     expect(response).to have_http_status(201)
+  end
+
+  it 'does not create contract with admin token if some of the params missing' do
+    contract = Fabricate.build(:mpoc_buzzn_metering)
+    request_params = Hash.new
+    incomplete_params = @contract_param_names.sample(@contract_param_names.length / 2)
+    incomplete_params.each { |param_name| request_params[param_name] = contract[param_name] }
+    access_token  = Fabricate(:admin_access_token).token
+    post_with_token "/api/v1/contracts/", request_params.to_json, access_token
+    expect(response).to have_http_status(400)
+  end
+
+  it 'does not create contract with admin token if some params are wrong' do
+    contract = Fabricate.build(:mpoc_buzzn_metering)
+    request_params = Hash.new
+    @contract_param_names.each do |param_name|
+      if contract[param_name].is_a?(Date)
+        request_params[param_name] = false
+      elsif contract[param_name].is_a?(Boolean)
+        request_params[param_name] = "unknown"
+      else
+        request_params[param_name] = contract[param_name]
+      end
+    end
+    access_token  = Fabricate(:admin_access_token).token
+    post_with_token "/api/v1/contracts/", request_params.to_json, access_token
+    expect(response).to have_http_status(400)
   end
 
   it 'does not update contract without token' do
@@ -85,7 +105,6 @@ describe 'Contracts API' do
       mode:  new_contract.mode,
     }.to_json
     put_without_token "/api/v1/contracts/", request_params
-    # binding.pry
     expect(response).to have_http_status(401)
   end
 
@@ -98,9 +117,36 @@ describe 'Contracts API' do
       mode:  new_contract.mode,
     }.to_json
     put_with_token "/api/v1/contracts/", request_params, access_token
-    # binding.pry
     expect(response).to have_http_status(200)
     expect(json['data']['attributes']['mode']).to eq(new_contract.mode)
+  end
+
+  it 'does not update contract with admin token if some params are wrong' do
+    contract_id = Fabricate(:mpoc_buzzn_metering).id
+    new_contract = Fabricate.build(:mpoc_ferraris_0002_amperix)
+    access_token  = Fabricate(:admin_access_token).token
+    request_params = { id: contract_id }
+    @contract_param_names.each do |param_name|
+      if new_contract[param_name].is_a?(Date)
+        request_params[param_name] = false
+      elsif new_contract[param_name].is_a?(Boolean)
+        request_params[param_name] = "unknown"
+      end
+    end
+    put_with_token "/api/v1/contracts/", request_params.to_json, access_token
+    expect(response).to have_http_status(400)
+  end
+
+  it 'does not update contract with admin token and wrong id' do
+    id = Random.new().rand(10000)
+    new_contract = Fabricate.build(:mpoc_ferraris_0002_amperix)
+    access_token  = Fabricate(:admin_access_token).token
+    request_params = {
+      id: "#{id}random",
+      mode:  new_contract.mode,
+    }.to_json
+    put_with_token "/api/v1/contracts/", request_params, access_token
+    expect(response).to have_http_status(404)
   end
 
   it 'does not delete contract without token' do
