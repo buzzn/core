5fdaecdcada58c0f53b27395e4b39784d35c789c
ChipJuggler <scsi.rdx@gmail.com>
2016-06-28 23:00:47 +0300

    roles code removal from comments api fix branch

--- spec/requests/api/v1/roles_spec.rb
+++
@@ -1,9 +0,0 @@
-describe 'Roles API' do
-
-  it 'adds a member role to metering point for some user' do
-    post_without_token '/api/v1/roles/add', {}
-    puts(response.to_a)
-    expect(response).to have_http_status(200)
-  end
-
-end
