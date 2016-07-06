c8e6054d350e8490525b1cba7ee5537cdf5f7f56
Christian Meier <meier.kristian@gmail.com>
2016-07-06 12:47:47 +0200

    add orginazaiotn API

---
+++ app/controllers/api/v1/organizations.rb
@@ -0,0 +1,140 @@
+module API
+  module V1
+    class Organizations < Grape::API
+      include API::V1::Defaults
+      
+      resource :organizations do
+
+        before do
+          doorkeeper_authorize!
+        end
+        
+        desc "Return all organizations"
+        paginate(per_page: per_page=10)
+        get root: :organizations do
+          if current_user
+            if Organization.readable_by?(current_user)
+              per_page     = params[:per_page] || per_page
+              page         = params[:page] || 1
+              total_pages  = Organization.all.page(page).per_page(per_page).total_pages
+              paginate(render(Organization.all, meta: { total_pages: total_pages }))
+            else
+              status 403
+            end
+          else
+            status 401
+          end
+        end
+
+
+
+        desc "Return an Organization"
+        params do
+          requires :id, type: String, desc: "ID of the organization"
+        end
+        get ":id", root: "organization" do
+          if current_user
+            organization = Organization.where(id: permitted_params[:id]).first!
+            if organization.readable_by?(current_user)
+              organization
+            else
+              status 403
+            end
+          else
+            status 401
+          end
+        end
+
+
+
+
+        desc "Create an Organization."
+        params do
+          requires :name,         type: String, desc: "Name of the Organization."
+          requires :phone,        type: String, desc: "Phone number of Organization."
+          requires :fax,          type: String, desc: "Fax number of Organization."
+          optional :website,      type: String, desc: "Website of Organization."
+          requires :email,        type: String, desc: "Email of Organization."
+          requires :description,  type: String, desc: "Description of the Organization."
+          requires :mode,         type: String, desc: 'Mode of Organization', values: Organization.modes
+        end
+        post do
+          if current_user
+            if Organization.creatable_by?(current_user)
+              organization = Organization.new({
+                name:        params.name,
+                phone:       params.phone,
+                fax:         params.fax,
+                website:     params.website,
+                description: params.description,
+                mode:        params.mode,
+                email:       params.email
+              })
+
+              if organization.save!
+                current_user.add_role(:manager, organization)
+                return organization
+              else
+                error!('error saving organization', 500)
+              end
+            else
+              status 403
+            end
+          else
+            status 401
+          end
+        end
+
+
+
+        desc "Update an Organization."
+        params do
+          requires :id, type: String, desc: "Organization ID."
+          requires :name,         type: String, desc: "Name of the Organization."
+          requires :phone,        type: String, desc: "Phone number of Organization."
+          requires :fax,          type: String, desc: "Fax number of Organization."
+          optional :website,      type: String, desc: "Website of Organization."
+          requires :email,        type: String, desc: "Email of Organization."
+          requires :description,  type: String, desc: "Description of the Organization."
+          requires :mode,         type: String, desc: 'Mode of Organization', values: Organization.modes
+        end
+        put do
+          if current_user
+            organization = Organization.find(params[:id])
+            if organization.updatable_by?(current_user)
+              #params = params.organization || params
+              params.delete(:id)
+              organization.update(params)
+              return organization
+            else
+              status 403
+            end
+          else
+            status 401
+          end
+        end
+
+
+
+        desc 'Delete an Organization.'
+        params do
+          requires :id, type: String, desc: "Organization ID"
+        end
+        delete ':id' do
+          if current_user
+            organization = Organization.find(params[:id])
+            if organization.deletable_by?(current_user)
+              organization.destroy
+              status 204
+            else
+              status 403
+            end
+          else
+            status 402
+          end
+        end
+
+      end
+    end
+  end
+end
---
+++ spec/requests/api/v1/organization_spec.rb
@@ -0,0 +1,205 @@
+describe "Organizations API" do
+
+  let(:page_overload) { 11 }
+
+  it 'does not get an organization without token' do
+    organization = Fabricate(:distribution_system_operator)
+    get_without_token "/api/v1/organizations/#{organization.id}"
+    expect(response).to have_http_status(401)
+  end
+
+
+  it 'gets an organization with admin token' do
+    access_token = Fabricate(:admin_access_token)
+    organization = Fabricate(:electricity_supplier)
+    get_with_token "/api/v1/organizations/#{organization.id}", access_token.token
+    expect(response).to have_http_status(200)
+    expect(json['data']['id']).to eq organization.id
+  end
+
+
+  it 'does not get an organization with token' do
+    access_token = Fabricate(:access_token)
+    organization = Fabricate(:metering_point_operator)
+    get_with_token "/api/v1/organizations/#{organization.id}", access_token.token
+    expect(response).to have_http_status(403)
+  end
+
+
+  it 'does not get any organization without token' do
+    Fabricate(:distribution_system_operator)
+    get_without_token "/api/v1/organizations"
+    expect(response).to have_http_status(401)
+  end
+
+
+  it 'does not get any organization with token' do
+    access_token = Fabricate(:access_token)
+    Fabricate(:distribution_system_operator)
+    get_without_token "/api/v1/organizations", access_token.token
+    expect(response).to have_http_status(401)
+  end
+
+
+  it 'gets all organizations with admin token' do
+    access_token = Fabricate(:admin_access_token)
+    organization = Fabricate(:electricity_supplier)
+    get_with_token "/api/v1/organizations", access_token.token
+    expect(response).to have_http_status(200)
+    expect(json['data'].size).to eq Organization.all.size
+    expect(json['data'].last['id']).to eq organization.id
+  end
+  
+
+  it 'paginate organizations' do
+    page_overload.times do
+      Fabricate(:distribution_system_operator)
+    end
+    access_token = Fabricate(:admin_access_token)
+    get_with_token "/api/v1/organizations", access_token.token
+    expect(response).to have_http_status(200)
+    expect(json['meta']['total_pages']).to eq(2)
+  end
+
+
+  it 'does not create an organization with user token' do
+    access_token = Fabricate(:access_token)
+    organization = Fabricate.build(:metering_service_provider)
+
+    request_params = {
+      name:        organization.name,
+      phone:       organization.phone,
+      fax:         organization.fax,
+      website:     organization.website,
+      description: organization.description,
+      mode:        organization.mode,
+      email:       organization.email
+    }.to_json
+
+    post_with_token "/api/v1/organizations", request_params, access_token.token
+
+    expect(response).to have_http_status(403)
+  end
+
+
+  it 'creates an organization with admin token' do
+    access_token = Fabricate(:admin_access_token)
+    organization = Fabricate.build(:metering_service_provider)
+
+    request_params = {
+      name:        organization.name,
+      phone:       organization.phone,
+      fax:         organization.fax,
+      website:     organization.website,
+      description: organization.description,
+      mode:        organization.mode,
+      email:       organization.email
+    }.to_json
+
+    post_with_token "/api/v1/organizations", request_params, access_token.token
+
+    expect(response).to have_http_status(201)
+    expect(json['data']['id']).not_to eq organization.id
+    expect(json['data']['attributes']['name']).to eq organization.name
+    expect(json['data']['attributes']['email']).to eq organization.email
+    expect(json['data']['attributes']['fax']).to eq organization.fax
+    expect(json['data']['attributes']['phone']).to eq organization.phone
+    expect(json['data']['attributes']['website']).to eq organization.website
+    expect(json['data']['attributes']['mode']).to eq organization.mode
+    expect(json['data']['attributes']['description']).to eq organization.description
+  end
+
+
+
+  it 'does not update an organization without token' do
+    organization = Fabricate(:metering_service_provider)
+
+    request_params = {
+      id:          organization.id,
+      name:        'Google',
+      phone:       organization.phone,
+      fax:         organization.fax,
+      website:     organization.website,
+      description: organization.description,
+      mode:        organization.mode,
+      email:       organization.email
+    }.to_json
+
+    put "/api/v1/organizations", request_params
+
+    expect(response).to have_http_status(401)
+  end
+
+
+  it 'does not update an organization with user token' do
+    access_token = Fabricate(:access_token)
+    organization = Fabricate(:metering_service_provider)
+
+    request_params = {
+      id:          organization.id,
+      name:        'Google',
+      phone:       organization.phone,
+      fax:         organization.fax,
+      website:     organization.website,
+      description: organization.description,
+      mode:        organization.mode,
+      email:       organization.email
+    }.to_json
+
+    put_with_token "/api/v1/organizations", request_params, access_token.token
+    expect(response).to have_http_status(403)
+  end
+
+
+  it 'updates an organization with admin token' do
+    access_token = Fabricate(:admin_access_token)
+    organization = Fabricate(:metering_service_provider)
+
+    request_params = {
+      id:          organization.id,
+      name:        'Google',
+      phone:       organization.phone,
+      fax:         organization.fax,
+      website:     organization.website,
+      description: organization.description,
+      mode:        organization.mode,
+      email:       organization.email
+    }.to_json
+
+    put_with_token "/api/v1/organizations", request_params, access_token.token
+
+    expect(response).to have_http_status(200)
+    expect(json['data']['id']).to eq organization.id
+    expect(json['data']['attributes']['name']).to eq 'Google'
+  end
+
+  
+  it 'does not delete an organization without token' do
+    organization_id = Fabricate(:metering_service_provider).id
+
+    delete "/api/v1/organizations/#{organization_id}"
+   
+    expect(response).to have_http_status(401)
+  end
+
+
+  it 'does not delete an organization with user token' do
+    access_token = Fabricate(:access_token)
+    organization_id = Fabricate(:metering_service_provider).id
+
+    delete_with_token "/api/v1/organizations/#{organization_id}", access_token.token
+
+    expect(response).to have_http_status(403)
+  end
+
+  
+  it 'deletes an organization with admin token' do
+    access_token = Fabricate(:admin_access_token)
+    organization_id = Fabricate(:metering_service_provider).id
+
+    delete_with_token "/api/v1/organizations/#{organization_id}", access_token.token
+
+    expect(response).to have_http_status(204)
+  end
+
+end
