7340fe9639dfd606facf0838ac37ecc2b00b62bf
Christian Meier <meier.kristian@gmail.com>
2016-08-18 10:23:46 +0200

    add PATCH verb to CORS setup

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module Buzzn
     config.middleware.insert_before 0, 'Rack::Cors' do
       allow do
         origins "*"
-        resource "*", headers: :any, methods: [:get, :post, :put, :delete, :options]
+        resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete, :options]
       end
     end
 
