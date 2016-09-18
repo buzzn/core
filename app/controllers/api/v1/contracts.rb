3cf9d3480c10bd12c790101d17e3f227d7252f48
ChipJuggler <scsi.rdx@gmail.com>
2016-09-18 23:00:20 +0300

    less strict require param list for contact creation

--- app/controllers/api/v1/contracts.rb
+++ app/controllers/api/v1/contracts.rb
@@ -35,17 +35,18 @@ module API
         desc 'Create a Contract'
         params do
           requires :mode,                   type: String,  desc: 'Contract description'
-          requires :organization_id,        type: String,  desc: 'Organization id'
+          optional :organization_id,        type: String,  desc: 'Organization id'
           requires :tariff,                 type: String,  desc: 'Tariff'
-          requires :status,                 type: String,  desc: 'Status'
-          requires :customer_number,        type: String,  desc: 'Customer number'
-          requires :contract_number,        type: String,  desc: 'Contract number'
-          requires :signing_user,           type: String,  desc: 'Signing user'
+          optional :status,                 type: String,  desc: 'Status'
+          optional :customer_number,        type: String,  desc: 'Customer number'
+          optional :contract_number,        type: String,  desc: 'Contract number'
+          optional :signing_user,           type: String,  desc: 'Signing user'
           requires :terms,                  type: Boolean, desc: 'Terms'
           requires :power_of_attorney,      type: Boolean, desc: 'Power of attorney'
           requires :confirm_pricing_model,  type: Boolean, desc: 'Confirm pricing model'
           requires :commissioning,          type: Date,    desc: 'Commissioning'
           optional :metering_point_id,      type: String,  desc: 'MeteringPoint ID'
+          # TODO: Should username/password be here?
           optional :username,               type: String,  desc: 'Username'
           optional :password,               type: String,  desc: 'Password'
         end
