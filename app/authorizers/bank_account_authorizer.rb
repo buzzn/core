7407494e05dbda60e4df11fb97e501c9bbdda4ef
Christian Meier <meier.kristian@gmail.com>
2016-09-18 11:47:31 +0200

    crud on bank_account model

--- app/authorizers/bank_account_authorizer.rb
+++ app/authorizers/bank_account_authorizer.rb
@@ -1,19 +1,20 @@
 class BankAccountAuthorizer < ApplicationAuthorizer
 
-  def self.creatable_by?(user)
-    !!user
+  def self.creatable_by?(user, parent)
+    parent.updatable_by?(user)
   end
 
   def readable_by?(user)
-    User.admin?(user)
+    # uses scope BankAccount.readable_by(user)
+    readable?(BankAccount, user)
   end
 
   def updatable_by?(user)
-    User.admin?(user)
+    resource.bank_accountable.updatable_by?(user)
   end
 
-  def deletable_by?(user)    
-    User.admin?(user)
+  def deletable_by?(user)
+    resource.bank_accountable.updatable_by?(user)
   end
 
 end
