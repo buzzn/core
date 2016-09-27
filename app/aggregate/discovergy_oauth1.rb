37ebd30ec4886b6e6c0ec141fa878890ca4f5015
Christian <christian@buzzn.net>
2016-09-27 20:20:27 +0200

    add oauth fields to contract

--- app/aggregate/discovergy_oauth1.rb
+++ app/aggregate/discovergy_oauth1.rb
@@ -3,10 +3,6 @@ class DiscovergyOauth1
 
   TIMEOUT = 5 # seconds
 
-  #consumer key + secret are specific for the buzzn app, so they have to be only registered once and don't change
-  @consumer_key = nil
-  @consumer_secret = nil
-
   # how to use
   # discovergy = DiscovergyOauth1.new('test@email.de', 'testpassword')
   # discovergy.meters
@@ -17,42 +13,51 @@ class DiscovergyOauth1
   # this is only for testing the connectivity.
   # the real oauth1 flow will be implemented later.
   # we will also implement using the API with already existing access_token.
-  # but first must the discovergy API go online.
 
 
 
-  def initialize(email, password)
-    @email  = email
-    @password  = password
+  def initialize(contract)
+    # consumer key + secret are specific for the buzzn app, so they have to be only registered once and don't change
+    @consumer_key = 'ugv3tngmrah0e6ph41v78ohq4j'
+    @consumer_secret = 'locutd1nfv43ip4804mkkhd5me'
 
-    #TODO: change url to https://api.discovergy.com
-    @conn = Faraday.new(:url => 'http://localhost:9006', ssl: {verify: false}, request: { timeout: TIMEOUT, open_timeout: TIMEOUT }) do |faraday|
+    @email  = contract.username
+    @password  = contract.password
+
+    @conn = Faraday.new(:url => 'https://api.discovergy.com', ssl: {verify: false}, request: { timeout: TIMEOUT, open_timeout: TIMEOUT }) do |faraday|
       faraday.request  :url_encoded
       faraday.response :logger, Rails.logger if Rails.env == 'development'
       faraday.adapter :net_http
     end
 
-    if !@consumer_secret && !@consumer_key
-      @consumer_token = register_application
-      @consumer_key = @consumer_token['key']
-      @consumer_secret = @consumer_token['secret']
+    consumer = OAuth::Consumer.new(@consumer_key, @consumer_secret, :site => 'https://api.discovergy.com', :request_token_path => '/public/v1/oauth1/request_token', :authorize_path => '/public/v1/oauth1/authorize', :access_token_path => '/public/v1/oauth1/access_token')
+
+    if contract.external_access_token && contract.external_access_token_secret
+      hash = {
+        :oauth_token          => contract.external_access_token,
+        "oauth_token"         => contract.external_access_token,
+        :oauth_token_secret   => contract.external_access_token_secret,
+        "oauth_token_secret"  => contract.external_access_token_secret
+      }
+      @access_token = OAuth::AccessToken.from_hash(consumer, hash)
+    else
+      @request_token = consumer.get_request_token
+      verifier = authorize.split('=')[1] #TODO: catch problems verifying the user credentials
+      @access_token = @request_token.get_access_token(:oauth_verifier => verifier)
+      contract.update_column(:external_access_token, @access_token.token)
+      contract.update_column(:external_access_token_secret, @access_token.secret)
     end
-
-    #TODO: change site to https://api.discovergy.com
-    @consumer = OAuth::Consumer.new(@consumer_key, @consumer_secret, :site => 'http://localhost:9006', :request_token_path => '/public/v1/oauth1/request_token', :authorize_path => '/public/v1/oauth1/authorize', :access_token_path => '/public/v1/oauth1/access_token')
-
-    @request_token = @consumer.get_request_token
-    @verifier = authorize
-    @access_token = @request_token.get_access_token(:oauth_verifier => @verifier)
   end
 
   def register_application
     response = @conn.post do |req|
       req.url '/public/v1/oauth1/consumer_token'
       req.headers['Content-Type'] = "application/x-www-form-urlencoded"
-      req.params['client'] = 'buzzn'
+      req.params['client'] = 'buzzn app'
     end
-    return MultiJson.load(response.body)
+    consumer_token = response.body
+    @consumer_key = consumer_token['key']
+    @consumer_secret = consumer_token['secret']
   end
 
   def authorize
@@ -67,9 +72,54 @@ class DiscovergyOauth1
   end
 
 
+
+
   def meters
     @access_token.get('/public/v1/meters')
-    return @access_token.response.body
+    return @access_token.response
+  end
+
+
+
+
+  def live(meter_id)
+    @access_token.get('/public/v1/readings?meterId=EASYMETER_' + meter_id + '&from=' + ((Time.now - 5.seconds).to_i*1000).to_s)
+    return @access_token.response
+  end
+
+  def hour(meter_id, timestamp)
+    datetime_from  = (Time.at(timestamp.to_i/1000).in_time_zone.beginning_of_hour).to_i*1000
+    datetime_to    = (Time.at(timestamp.to_i/1000).in_time_zone.end_of_hour).to_i*1000
+    @access_token.get('/public/v1/readings?meterId=EASYMETER_' + meter_id + '&from=' + datetime_from.to_s + '&to=' + datetime_to.to_s + '&resolution=raw&fields=power')
+    return @access_token.response
+  end
+
+  def day(meter_id, timestamp)
+    datetime_from  = (Time.at(timestamp.to_i/1000).in_time_zone.beginning_of_day).to_i*1000
+    datetime_to    = (Time.at(timestamp.to_i/1000).in_time_zone.end_of_day).to_i*1000
+    @access_token.get('/public/v1/readings?meterId=EASYMETER_' + meter_id + '&from=' + datetime_from.to_s + '&to=' + datetime_to.to_s + '&resolution=fifteen_minutes&fields=power')
+    return @access_token.response
+  end
+
+  def month(meter_id, timestamp)
+    datetime_from  = (Time.at(timestamp.to_i/1000).in_time_zone.beginning_of_month).to_i*1000
+    datetime_to    = (Time.at(timestamp.to_i/1000).in_time_zone.end_of_month).to_i*1000
+    @access_token.get('/public/v1/readings?meterId=EASYMETER_' + meter_id + '&from=' + datetime_from.to_s + '&to=' + datetime_to.to_s + '&resolution=one_day&fields=energy,energyOut')
+    return @access_token.response
+  end
+
+  def year(meter_id, timestamp)
+    datetime_from  = (Time.at(timestamp.to_i/1000).in_time_zone.beginning_of_year).to_i*1000
+    datetime_to    = (Time.at(timestamp.to_i/1000).in_time_zone.end_of_year).to_i*1000
+    @access_token.get('/public/v1/readings?meterId=EASYMETER_' + meter_id + '&from=' + datetime_from.to_s + '&to=' + datetime_to.to_s + '&resolution=one_month&fields=energy,energyOut')
+    return @access_token.response
+  end
+
+  def reading(meter_id, timestamp)
+    datetime_from  = (Time.at(timestamp.to_i/1000).in_time_zone).to_i*1000
+    datetime_to  = datetime_from + 2000
+    @access_token.get('/public/v1/readings?meterId=EASYMETER_' + meter_id + '&from=' + datetime_from.to_s + '&to=' + datetime_to.to_s + '&resolution=raw')
+    return @access_token.response
   end
 
 
