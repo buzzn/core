cb78db9f23c7e81aa0586174190f97eb00a975e6
Felix Faerber <ffaerber@gmail.com>
2016-11-08 16:09:31 +0100

    separately

--- app/workers/update_metering_point_chart_cache.rb
+++ app/workers/update_metering_point_chart_cache.rb
@@ -1,9 +1,10 @@
 class UpdateMeteringPointChartCache
   include Sidekiq::Worker
+  sidekiq_options retry: false
 
-  def perform(timestamp, resolution)
-    MeteringPoint.all.each do |metering_point|
-      metering_points_hash = Aggregate.sort_metering_points([metering_point])
+  def perform(metering_point_id, timestamp, resolution)
+    if MeteringPoint.exists?(id: metering_point_id)
+      metering_points_hash = Aggregate.sort_metering_points([MeteringPoint.find(metering_point_id)])
       aggregate = Aggregate.new(metering_points_hash)
       aggregate.past(timestamp: timestamp, resolution: resolution, refresh_cache: true)
     end
