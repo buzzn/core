325f571a2ed1c6fd6bb65bde2399ee86a6ec3e98
Felix Faerber <ffaerber@gmail.com>
2016-11-16 11:32:03 +0100

    worker

--- app/workers/update_metering_point_latest_power_cache.rb
+++ app/workers/update_metering_point_latest_power_cache.rb
@@ -1,9 +1,9 @@
-class UpdateMeteringPointLatestPowerCache
+class UpdateRegisterLatestPowerCache
   include Sidekiq::Worker
 
-  def perform(metering_point_id)
-    @cache_id = "/metering_points/#{metering_point_id}/latest_power"
-    @fresh_last_power = MeteringPoint.find(metering_point_id).last_power
+  def perform(register_id)
+    @cache_id = "/registers/#{register_id}/latest_power"
+    @fresh_last_power = Register.find(register_id).last_power
     Rails.cache.write(@cache_id, @fresh_last_power, expires_in: 5.minute)
   end
 end
