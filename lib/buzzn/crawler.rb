1a3d55811d18b1a2b9c8589c33217c706983c804
Christian Meier <meier.kristian@gmail.com>
2016-11-16 16:47:42 +0100

    skeleton for new crawler/aggregate

---
+++ lib/buzzn/crawler.rb
@@ -0,0 +1,49 @@
+require 'buzzn'
+require 'buzzn/discovergy/crawler'
+
+module Buzzn
+
+  class CrawlerError < StandardError; end
+
+  class CrawlerResult
+
+    def initialize
+      @data = []
+    end
+
+    def add(timestamp, power)
+      @data << timestamp
+      @data << power
+    end
+
+    def get(i = 0)
+      [@data[2 * i], @data[2 * i + 1]]
+    end
+
+    def timestamp(i = 0)
+      @data[2 * i]
+    end
+
+    def power(i = 0)
+      @data[2 * i + 1]
+    end
+  end
+
+  class Crawler
+
+    @@discovergy = Buzzn::Discovergy::Crawler.new
+    @@mysmartgrid = Buzzn::Mysmartgrid::Crawler.new
+
+    def self.new(organization)
+      case organization.name
+      when Organization.DISCOVERGY
+        @@discovergy
+      when Organization.MYSMARTGRID
+        @@mysmartgrid
+      else
+        raise "do not have a crawler for #{organization.name}"
+      end
+    end
+
+  end
+end
