f2ca9f215d730059c61cbef4e874321d2e539c94
Christian Meier <meier.kristian@gmail.com>
2016-11-22 15:51:22 +0100

    aggregate result objects

--- lib/buzzn/aggregate.rb
+++ lib/buzzn/aggregate.rb
@@ -5,25 +5,68 @@ module Buzzn
 
   class AggregateError < StandardError; end
 
-  class AggregateResult < Array
+  class TypedResult < Array
 
-    class AggregateResultItem
+    attr_reader :type
 
-      attr_reader :timestamp, :value, :type
-
-      def initialize(timestamp, value, type)
-        unless [:energy_milliwatt_hour, :power_milliwatt,
-                :energy_b_milliwatt_hour, :energy_a_milliwatt_hour].include?(type)
+    def initialize(type)
+      unless [:energy_milliwatt_hour,
+              :power_milliwatt,
+              :energy_b_milliwatt_hour,
+              :energy_a_milliwatt_hour].include?(type)
           raise "unknown type #{type}"
         end
+      @type = type
+      super()
+    end
+
+    
+    def finallize
+      freeze
+      self
+    end
+  end
+
+
+  class PastResult < TypedArray
+
+    class PastItem
+
+      attr_reader :timestamp, :value
+
+      def initialize(timestamp, value)
         @timestamp = timestamp
-        @type = type
         @value = value
       end
     end
 
-    def add(timestamp, value, type)
-      add(AggregateResultItem.new(timestamp, value, type)
+    def add_value(timestamp, value)
+      add(PastItem.new(timestamp, value))
+    end 
+  end
+
+  class PresentResult < TypedArray
+
+    class PresentItem < PastItem
+
+      attr_reader :operator,:timestamp, :value
+
+      def initialize(timestamp, value, operator)
+        @timestamp = timestamp
+        @value = value
+        @operator = operator
+      end
+    end
+
+    attr_reader :sum, :timestamp
+
+    def add_reading(timestamp, value, operator)
+      add(PresentItem.new(timestamp, value, operator))
+    end
+
+    def finallize
+      # TODO calculate sum and timestamp from entries
+      super
     end
   end
 
@@ -33,15 +76,22 @@ module Buzzn
       @factory = crawler_factory
     end
 
+    # same as the current aggregate API
     def register(interval, register)
+      result = result_for(interval)
+
+      # TODO organization or data_source
       organization = register.metering_point_operator_contract.organization
 
       if organization
         crawler = @facotry.crawler_for(organization)
         # TODO
       end
+
+      result.finalize
     end
 
+    # not sure if needed but for present sum of the whole group ?!?
     def group(interval, group)
       # TODO needs query to collect all distinct organizations from all
       #      registers of a group
@@ -51,14 +101,25 @@ module Buzzn
       end
     end
 
+    # for wobbly bubbles
     def each_register(interval, group)
+      result = result_for(interval)
       # TODO needs query to collect all distinct organizations from all
       #      registers of a group
+      #      or is the data_source the one to go from here but what about
+      #      the crawler ....
       group.register_organizations.each do |organization|
-         crawler = @facotry.crawler_for(organization)
+         crawler = @factory.crawler_for(organization)
          # aggregate results from crawler ....
+         # or first collect all results from all crawler and do th e aggregation
       end
+      result.finalize
     end
 
+    private
+
+    def result_for(interval)
+      interval.live? ? PresentResult.new : PastResult.new
+    end
   end
 end
