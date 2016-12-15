a6c3ee9876cfc5c51d9be99743b942b632fd9ac3
Christian Meier <meier.kristian@gmail.com>
2016-12-15 15:09:40 +0100

    adds tests for CurrentPower service component

--- lib/buzzn/data_source_registry.rb
+++ lib/buzzn/data_source_registry.rb
@@ -5,12 +5,13 @@ module Buzzn
     def initialize(map = {})
       @registry = map.dup
       @registry[:discovergy] ||= Buzzn::Discovergy::DataSource.new
-      @registry[:mysmartgrid] ||= Buzzn::Mysmartgrid::DataSource.new
+      #@registry[:mysmartgrid] ||= Buzzn::Mysmartgrid::DataSource.new
       #@registry[:standard_profile] ||= Buzzn::StandardProfile::DataSource.new
     end
 
     def get(data_source)
-      raise "can not handle #{data_source}" unless @registry.key?(data_source)
+      data_source = data_source.to_sym
+      raise ArgumentError.new("can not handle #{data_source}") unless @registry.key?(data_source)
       @registry[data_source]
     end
 
---
+++ spec/lib/buzzn/current_power_spec.rb
@@ -0,0 +1,69 @@
+describe Buzzn::CurrentPower do
+
+  class DummyDataSource
+
+    def method_missing(method, *args)
+      [method] + args
+    end
+
+    def single_aggregated(resource, mode)
+      method_missing(:single_aggregated, resource, mode) unless resource.is_a? Group
+    end
+  end
+
+  class MockDataSource
+
+    attr_accessor :input, :output
+
+    def single_aggregated(resource, mode)
+      mode == :in ? @input : @output
+    end
+
+    def method_missing(method, *args)
+      nil
+    end
+  end
+
+  let(:mock) { MockDataSource.new }
+  subject do
+    Buzzn::CurrentPower.new(Buzzn::DataSourceRegistry.new(dummy: DummyDataSource.new, mock: mock))
+  end
+
+  let(:group) { Fabricate(:group) }
+  let(:register) { Fabricate(:output_register) }
+  let(:dummy_register) do
+    register = Fabricate(:input_register)
+    def register.data_source; 'dummy';end
+    def register.to_s; self.id; end
+    register
+  end
+  let(:group_with_register) do
+    register = Fabricate(:output_register, group: group)
+    def register.data_source; 'mock';end
+    group
+  end
+
+  it 'delivers the right result for a register' do
+    result = subject.for_register(dummy_register)
+    expect(result).to eq [:single_aggregated, dummy_register, :in]
+
+    expect { subject.for_register(register) }.to raise_error ArgumentError
+  end
+
+  it 'delivers the right result for each register in a group' do
+    result = subject.for_each_register_in_group(group)
+    expect(result).to eq [:collection, group, :in, :collection, group, :out]
+  end
+
+  it 'delivers the right result for a group' do
+    mock.input = Buzzn::DataResult.new(Time.current,
+                                       123, nil, :in)
+    mock.output = Buzzn::DataResult.new(Time.current,
+                                        321, nil, :out)
+    result = subject.for_group(group)
+    expect(result.resource_id).to eq group.id
+    expect(result.in).to eq 123
+    expect(result.out).to eq 321    
+  end
+
+end
