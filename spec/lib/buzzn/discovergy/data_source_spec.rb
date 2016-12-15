654cd67e681f659e38fee99992dfeb71d3ed1492
Christian Meier <meier.kristian@gmail.com>
2016-12-15 09:23:20 +0100

    adds tests for collections from discovergy data-source

--- spec/lib/buzzn/discovergy/data_source_spec.rb
+++ spec/lib/buzzn/discovergy/data_source_spec.rb
@@ -2,6 +2,9 @@
 require 'buzzn/discovergy/data_source'
 
 describe Buzzn::Discovergy::DataSource do
+
+  subject { Buzzn::Discovergy::DataSource.new }
+
   let(:meter) { Fabricate(:meter, manufacturer_product_serialnumber: 60009485) }
   let(:broker) { Fabricate(:discovergy_broker, resource: meter, external_id: "EASYMETER_#{meter.manufacturer_product_serialnumber}") }
   let(:small_group) { Fabricate(:group, registers: [Fabricate(:register_60118460), Fabricate(:register_60009441)]) }
@@ -14,6 +17,27 @@ describe Buzzn::Discovergy::DataSource do
       Fabricate(:register_60009393)  #in
     ])
   end
+  let(:empty_group) { Fabricate(:group) }
+  let(:register_with_broker) do
+    register = Fabricate(:input_register, group: empty_group, meter: Fabricate(:meter))
+    Fabricate(:discovergy_broker, resource: register.meter, external_id: 'easy_123')
+    register
+  end
+  let(:register_with_group_broker) do
+    register = register_with_broker
+    Fabricate(:discovergy_broker, resource: register.group, external_id: 'virtual_123')
+    register
+  end
+  let(:virtual_register) do
+    register = Fabricate(:input_register,
+                         meter: Fabricate(:meter),
+                         virtual: true, 
+                         formula_parts: [Fabricate(:fp_plus, operand: Fabricate(:input_register, meter: Fabricate(:meter)))])
+    Fabricate(:discovergy_broker, resource: register.meter, external_id: 'virtual_123')
+    
+    Fabricate(:discovergy_broker, resource: register.formula_parts.first.operand.meter, external_id: 'easy_123')
+    register
+  end
   let(:single_meter_live_response) { "{\"time\":1480606450088,\"values\":{\"power\":1100640}}" }
   let(:single_meter_hour_response) { "[{\"time\":1480604400205,\"values\":{\"power\":1760140}},{\"time\":1480604402205,\"values\":{\"power\":1750440}}]" }
   let(:single_meter_day_response) { "[{\"time\":1480606200000,\"values\":{\"energy\":22968322444644}},{\"time\":1480607100000,\"values\":{\"energy\":22970988922089}},{\"time\":1480608000000,\"values\":{\"energy\":22973229616478}}]" }
@@ -24,22 +48,20 @@ describe Buzzn::Discovergy::DataSource do
   let(:empty_response) { "[]" }
 
   it 'parses single meter live response' do
-    data_source = Buzzn::Discovergy::DataSource.new
     response = single_meter_live_response
     mode = :in
     two_way_meter = false
-    result = data_source.send(:parse_aggregated_live, response, mode, two_way_meter, 'u-i-d')
+    result = subject.send(:parse_aggregated_live, response, mode, two_way_meter, 'u-i-d')
     expect(result.timestamp).to eq Time.at(1480606450.088)
     expect(result.value).to eq 1100
   end
 
   it 'parses single meter hour response' do
-    data_source = Buzzn::Discovergy::DataSource.new
     response = single_meter_hour_response
     interval = Buzzn::Interval.hour(Time.now.to_i*1000)
     mode = :in
     two_way_meter = false
-    result = data_source.send(:parse_aggregated_data, response, interval, mode, two_way_meter, 'u-i-d')
+    result = subject.send(:parse_aggregated_data, response, interval, mode, two_way_meter, 'u-i-d')
     expect(result.in[0].timestamp).to eq Time.at(1480604400.205)
     expect(result.in[0].value).to eq 1760140
     expect(result.in[1].timestamp).to eq Time.at(1480604402.205)
@@ -47,12 +69,11 @@ describe Buzzn::Discovergy::DataSource do
   end
 
   it 'parses single meter day response' do
-    data_source = Buzzn::Discovergy::DataSource.new
     response = single_meter_day_response
     interval = Buzzn::Interval.day(Time.now.to_i*1000)
     mode = :in
     two_way_meter = false
-    result = data_source.send(:parse_aggregated_data, response, interval, mode, two_way_meter, 'u-i-d')
+    result = subject.send(:parse_aggregated_data, response, interval, mode, two_way_meter, 'u-i-d')
     expect(result.in[0].timestamp).to eq Time.at(1480606200)
     expect(result.in[0].value).to eq 1066590.978
     expect(result.in[1].timestamp).to eq Time.at(1480607100)
@@ -60,12 +81,11 @@ describe Buzzn::Discovergy::DataSource do
   end
 
   it 'parses single meter month response' do
-    data_source = Buzzn::Discovergy::DataSource.new
     response = single_meter_month_response
     interval = Buzzn::Interval.month(Time.now.to_i*1000)
     mode = :in
     two_way_meter = false
-    result = data_source.send(:parse_aggregated_data, response, interval, mode, two_way_meter, 'u-i-d')
+    result = subject.send(:parse_aggregated_data, response, interval, mode, two_way_meter, 'u-i-d')
     expect(result.in[0].timestamp).to eq Time.at(1477954800)
     expect(result.in[0].value).to eq 33883.8461
     expect(result.in[1].timestamp).to eq Time.at(1478041200)
@@ -86,22 +106,19 @@ describe Buzzn::Discovergy::DataSource do
   end
 
   it 'parses empty response' do
-    data_source = Buzzn::Discovergy::DataSource.new
     response = empty_response
     interval = Buzzn::Interval.year(Time.now.to_i*1000)
     mode = 'in'
     two_way_meter = false
-    result = data_source.send(:parse_aggregated_data, response, interval, mode, two_way_meter, 'u-i-d')
+    result = subject.send(:parse_aggregated_data, response, interval, mode, two_way_meter, 'u-i-d')
     expect(result).to eq nil
   end
 
   it 'parses virtual meter live response for each meter' do
-    data_source = Buzzn::Discovergy::DataSource.new
     response = virtual_meter_live_response
     mode = :in
     two_way_meter = false
-    external_id = broker.external_id
-    result = data_source.send(:parse_collected_data, response, mode, 'EASYMETER_60009425' => 'some-uid', 'EASYMETER_60009404' => 'other-uid', 'EASYMETER_60009415' => 'last-uid')
+    result = subject.send(:parse_collected_data, response, mode, 'EASYMETER_60009425' => 'some-uid', 'EASYMETER_60009404' => 'other-uid', 'EASYMETER_60009415' => 'last-uid')
     expect(result[0].timestamp).to eq Time.at(1480614249.341)
     expect(result[0].value).to eq 150950
     expect(result[1].timestamp).to eq Time.at(1480614254.195)
@@ -113,55 +130,80 @@ describe Buzzn::Discovergy::DataSource do
   end
 
   it 'parses virtual meter creation response' do
-    data_source = Buzzn::Discovergy::DataSource.new
     response = virtual_meter_creation_response
     mode = 'virtual'
     resource = group
-    result = data_source.send(:parse_virtual_meter_creation, response, mode, resource)
+    result = subject.send(:parse_virtual_meter_creation, response, mode, resource)
     expect(group.discovergy_brokers.size).to eq 1
   end
 
   it 'does not create virtual meters for small group' do
-    data_source = Buzzn::Discovergy::DataSource.new
-    brokers = data_source.create_virtual_meters_for_group(small_group)
+    brokers = subject.create_virtual_meters_for_group(small_group)
     expect(brokers).to eq []
   end
 
   it 'creates virtual meter for group' do |spec|
     VCR.use_cassette("lib/buzzn/discovergy/#{spec.metadata[:description].downcase}") do
       existing_broker = broker
-      data_source = Buzzn::Discovergy::DataSource.new
-      brokers = data_source.create_virtual_meters_for_group(group)
+      brokers = subject.create_virtual_meters_for_group(group)
       expect(brokers).not_to eq []
       expect(group.discovergy_brokers.size).to eq 2
     end
   end
 
-
-
-  subject { Buzzn::Discovergy::DataSource.new }
-
   it 'maps the external id to register ids of group' do
-    group = Fabricate(:group)
-    Fabricate(:discovergy_broker, resource: group, external_id: 'virtual_123')
-    register = Fabricate(:input_register, group: group, meter: Fabricate(:meter))
-    Fabricate(:discovergy_broker, resource: register.meter, external_id: 'easy_123')
-    map = subject.send(:to_map, group)
-    expect(map).to eq('easy_123' => register.id)
+    map = subject.send(:to_map, register_with_group_broker.group)
+    expect(map).to eq('easy_123' => register_with_broker.id)
   end
 
-  it 'maps the external id to register ids' do
-    meter = Fabricate(:meter)
-    register = Fabricate(:input_register,
-                         meter: Fabricate(:meter),
-                         virtual: true, 
-                         formula_parts: [Fabricate(:fp_plus, operand: Fabricate(:input_register, meter: meter))])
-    Fabricate(:discovergy_broker, resource: register.meter, external_id: 'virtual_123')
-    
-    Fabricate(:discovergy_broker, resource: register.formula_parts.first.operand.meter, external_id: 'easy_123')
-    
-    
-    map = subject.send(:to_map, register)
-    expect(map).to eq('easy_123' => register.formula_parts.first.operand.id)
+  it 'maps the external id to register ids' do    
+    map = subject.send(:to_map, virtual_register)
+    expect(map).to eq('easy_123' => virtual_register.formula_parts.first.operand.id)
+  end
+
+  class FacadeMock
+    attr_accessor :result
+
+    def readings(*args)
+      @result
+    end
+  end
+
+  let(:facade) { FacadeMock.new }
+
+  it 'collects data from each register without group broker' do
+    data_source = Buzzn::Discovergy::DataSource.new(facade)
+    Fabricate(:output_register, group: empty_group, meter: Fabricate(:meter))
+    facade.result = single_meter_live_response
+
+    in_result = data_source.collection(register_with_broker.group, :in)
+    out_result = data_source.collection(register_with_broker.group, :out)
+
+    expect(in_result.size).to eq 1
+    expect(out_result.size).to eq 1
+    expect(in_result.first.resource_id).to eq register_with_broker.id
+    expect(out_result.first.resource_id).to eq register_with_broker.id
+
+    # as the facade delivers the same results back for each mode
+    expect(in_result.first.value).to eq out_result.first.value
+    expect(in_result.first.timestamp).to eq out_result.first.timestamp
+  end
+
+  it 'collects data from each register with group broker' do
+    data_source = Buzzn::Discovergy::DataSource.new(facade)
+    Fabricate(:output_register, group: empty_group, meter: Fabricate(:meter))
+    facade.result = virtual_meter_live_response
+
+    in_result = data_source.collection(register_with_group_broker.group, :in)
+    out_result = data_source.collection(register_with_group_broker.group, :out)
+
+    expect(in_result.size).to eq 3
+    expect(out_result.size).to eq 3
+
+    # as the facade delivers the same results back for each mode
+    expect(in_result.first.value).to eq out_result.first.value
+    expect(in_result.first.timestamp).to eq out_result.first.timestamp
+    expect(in_result.last.value).to eq out_result.last.value
+    expect(in_result.last.timestamp).to eq out_result.last.timestamp
   end
 end
