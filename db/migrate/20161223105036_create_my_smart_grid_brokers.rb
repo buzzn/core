bb2dff97e51eb3a1691e1b86b0958606e2415b02
Christian <christian@buzzn.net>
2016-12-27 16:36:16 +0100

    create mysmartgrid broker with inheritance

---
+++ db/migrate/20161223105036_create_my_smart_grid_brokers.rb
@@ -0,0 +1,8 @@
+class CreateMySmartGridBrokers < ActiveRecord::Migration
+  def change
+    rename_index :discovergy_brokers, "index_discovergy_brokers_resources", "index_brokers_resources"
+    rename_index :discovergy_brokers, "index_discovergy_brokers", "index_brokers"
+    rename_table :discovergy_brokers, :brokers
+    add_column :brokers, :type, :string
+  end
+end
