ae74469b514e458daa7e315d601e76ab93cea9f8
Christian Meier <meier.kristian@gmail.com>
2017-01-03 09:27:12 +0100

    get some type safety in place

--- lib/buzzn/mysmartgrid/data_source.rb
+++ lib/buzzn/mysmartgrid/data_source.rb
@@ -1,4 +1,18 @@
 module Buzzn::Mysmartgrid
   class DataSource
+
+    NAME = :my_smart_grid
+
+    def collection(group_or_virtual_register, mode)
+      nil
+    end
+
+    def single_aggregated(register_or_group, mode)
+      nil
+    end
+
+    def aggregated(register_or_group, mode, interval)
+      nil
+    end
   end
 end
