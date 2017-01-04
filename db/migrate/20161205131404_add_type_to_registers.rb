63dd5fcdfb44d26f602dde1e6b1a9bb99bfe74da
Christian Meier <meier.kristian@gmail.com>
2017-01-04 15:38:38 +0100

    adds migration code for the register and meter subclasses

--- db/migrate/20161205131404_add_type_to_registers.rb
+++ db/migrate/20161205131404_add_type_to_registers.rb
@@ -1,5 +1,26 @@
 class AddTypeToRegisters < ActiveRecord::Migration
   def change
-    add_column :registers, :type, :string, null: false
+    add_column :registers, :type, :string
+    reversible do |dir|
+      dir.up do
+        Register::Base.all.each do |r|
+          type =
+            if r.virtual == false
+              if r.mode == 'in'
+                'Register::Input'
+              else
+                'Register::Output'
+              end
+            else
+              'Register::Virtual'
+            end
+          p execute "UPDATE registers SET type='#{type}' where id='#{r.id}';"
+        end
+      end
+      dir.down do
+        raise 'can not down grade'
+      end
+    end
+    change_column_null :registers, :type, false
   end
 end
--- db/migrate/20161225131404_add_type_to_meters.rb
+++ db/migrate/20161225131404_add_type_to_meters.rb
@@ -1,5 +1,35 @@
 class AddTypeToMeters < ActiveRecord::Migration
   def change
-    add_column :meters, :type, :string, null: false
+    add_column :meters, :type, :string
+    reversible do |dir|
+      dir.up do
+        Meter::Base.all.each do |m|
+          r = Register::Base.where(meter_id: m).first 
+          type =
+            if r.virtual == false
+              'Meter::Real'
+            else
+              'Meter::Virtual'
+            end
+          p execute "UPDATE meters SET type='#{type}' where id='#{m.id}';"
+        end
+        puts 'validate meters'
+        Meter::Base.all.each do |m|
+          puts "#{m.id}: #{m.valid?}"
+        end
+        puts 'validates registers'
+        Register::Base.all.each do |r|
+          puts "#{r.id}: #{r.valid?}"
+          if ! r.valid? && r.meter.nil?
+            puts "delete #{r.id}"
+            r.delete
+          end
+        end
+      end
+      dir.down do
+        raise 'can not down grade'
+      end
+    end
+    change_column_null :meters, :type, false
   end
 end
