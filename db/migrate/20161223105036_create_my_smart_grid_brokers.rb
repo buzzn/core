46edc93d5f949eb6e0657dfc0ab97fdffd465829
Christian Meier <meier.kristian@gmail.com>
2017-01-05 08:17:28 +0100

    ensure broker gets deleted with meter or group

--- db/migrate/20161223105036_create_my_smart_grid_brokers.rb
+++ db/migrate/20161223105036_create_my_smart_grid_brokers.rb
@@ -3,6 +3,6 @@ class CreateMySmartGridBrokers < ActiveRecord::Migration
     rename_index :discovergy_brokers, "index_discovergy_brokers_resources", "index_brokers_resources"
     rename_index :discovergy_brokers, "index_discovergy_brokers", "index_brokers"
     rename_table :discovergy_brokers, :brokers
-    add_column :brokers, :type, :string
+    add_column :brokers, :type, :string, null: false
   end
 end
