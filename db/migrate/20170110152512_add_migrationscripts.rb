64a92f82ec164b1a6bd198d1c36ac4bdcbe93e3e
Christian <christian@buzzn.net>
2017-01-10 17:06:55 +0100

    rescue migration error

--- db/migrate/20170110152512_add_migrationscripts.rb
+++ db/migrate/20170110152512_add_migrationscripts.rb
@@ -30,32 +30,36 @@ class AddMigrationscripts < ActiveRecord::Migration
 
         contracts = Contract.all.where("username IS NOT NULL").where('encrypted_password IS NOT NULL')
         puts 'Going to update ' + contracts.count.to_s + 'Contracts'
-        ActiveRecord::Base.transaction do
-          contracts.each do |contract|
-            if contract.contractor && contract.contractor.organization && (contract.contractor.organization.slug == "discovergy" || contract.contractor.organization.slug == "buzzn-metering")
-              organization = contract.contractor.organization
-              register = contract.register
-              if register.nil?
-                next
-              end
-              if register.meter.broker.nil?
-                broker = DiscovergyBroker.create!(
-                  mode: register.input? ? 'in' : 'out',
-                  external_id: "EASYMETER_#{register.meter.manufacturer_product_serialnumber}",
-                  provider_login: contract.username,
-                  provider_password: contract.password,
-                  resource: register.meter
-                )
+
+        contracts.each do |contract|
+          if contract.contractor && contract.contractor.organization && (contract.contractor.organization.slug == "discovergy" || contract.contractor.organization.slug == "buzzn-metering")
+            organization = contract.contractor.organization
+            register = contract.register
+            if register.nil?
+              next
+            end
+            if register.meter.broker.nil?
+              broker = DiscovergyBroker.new(
+                mode: register.input? ? 'in' : 'out',
+                external_id: "EASYMETER_#{register.meter.manufacturer_product_serialnumber}",
+                provider_login: contract.username,
+                provider_password: contract.password,
+                resource: register.meter
+              )
+              begin
                 register.meter.broker = broker
-                register.meter.save!
-              else
-                next
+                register.meter.save
+              rescue => e
+                puts "#{register.meter.manufacturer_product_serialnumber} " + "failed: " + e.inspect
               end
             else
               next
             end
-            puts '.'
+          else
+            next
           end
+          puts '.'
+
         end
         puts 'Finished.'
       end
