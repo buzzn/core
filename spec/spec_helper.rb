9dd49b2aa9438a314be3f1cf4378866bd2467d85
Felix Faerber <ffaerber@gmail.com>
2017-01-18 12:12:21 +0100

    append_after DatabaseCleaner.clean

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -103,20 +103,18 @@ RSpec.configure do |config|
   config.before(:each) do |spec|
     # 'threaded' in description triggers a different DatabaseCleanet strategy
     DatabaseCleaner.strategy = spec.description =~ /threaded/ ? :truncation : :transaction
-  end
-
-  config.before(:each) do
     DatabaseCleaner.start
   end
 
   config.after(:each) do
-    DatabaseCleaner.clean
     Mongoid.purge!
     Rails.cache.clear
     Timecop.travel(Time.local(2016, 7, 2, 10, 5, 0)) # HACK https://github.com/buzzn/buzzn/blob/master/config/environments/test.rb#L43-L44 is not working
   end
 
-
+  config.append_after(:each) do
+    DatabaseCleaner.clean
+  end
 
 
   # show retry status in spec process
