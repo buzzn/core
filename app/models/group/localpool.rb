43efc3fb8ff4af70267fa3b4a422c0fa53896730
Felix Faerber <ffaerber@gmail.com>
2017-02-15 14:14:18 +0100

    move localpool validation to localpool model

--- app/models/group/localpool.rb
+++ app/models/group/localpool.rb
@@ -1,4 +1,17 @@
 module Group
   class Localpool < Base
+
+
+    after_save :validate_localpool
+
+    def validate_localpool
+      if self.contracts.metering_point_operators.empty?
+       #@contract = MeteringPointOperatorContract.new(group: self, organization: Organization.buzzn_systems, username: 'team@localpool.de', password: 'Zebulon_4711')
+      else
+        @contract = self.contracts.metering_point_operators.first
+      end
+      #@contract.save
+    end
+
   end
 end
