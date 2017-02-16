5fe4f25bd3f070389a9b020bbd1ffd7ecde43f6f
Felix Faerber <ffaerber@gmail.com>
2017-02-16 14:58:18 +0100

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170208163547) do
+ActiveRecord::Schema.define(version: 20170215132553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -368,6 +368,7 @@ ActiveRecord::Schema.define(version: 20170208163547) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.float    "closeness"
+    t.string   "type"
   end
 
   add_index "groups", ["readable"], name: "index_groups_on_readable", using: :btree
