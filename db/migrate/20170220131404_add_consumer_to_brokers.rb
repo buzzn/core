d2b0cc86d2b7493c5e115aa49652f0228fdc11b2
Christian Meier <meier.kristian@gmail.com>
2017-02-20 16:56:16 +0100

    move consumer key/secret to discovergy broker
    fixes #785

---
+++ db/migrate/20170220131404_add_consumer_to_brokers.rb
@@ -0,0 +1,14 @@
+class AddConsumerToBrokers < ActiveRecord::Migration
+  def change
+    add_column :brokers, :consumer_key, :string, null: true
+    add_column :brokers, :consumer_secret, :string, null: true
+    reversible do |dir|
+      dir.up do
+        DiscovergyBroker.all.update_all(encrypted_provider_token_key: nil, encrypted_provider_token_secret: nil)
+      end
+      dir.down do
+        DiscovergyBroker.all.update_all(encrypted_provider_token_key: nil, encrypted_provider_token_secret: nil)
+      end
+    end
+  end
+end
