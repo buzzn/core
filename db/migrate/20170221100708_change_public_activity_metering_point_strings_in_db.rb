37c39e9e0f0f433152a47fca572c02af26be868d
Felix Faerber <ffaerber@gmail.com>
2017-02-21 11:10:29 +0100

    migrations

---
+++ db/migrate/20170221100708_change_public_activity_metering_point_strings_in_db.rb
@@ -0,0 +1,35 @@
+class ChangePublicActivityMeteringPointStringsInDb < ActiveRecord::Migration
+  def change
+    reversible do |dir|
+      dir.up do
+        ActiveRecord::Base.transaction do
+          PublicActivity::Activity.where(key: 'user.appointed_metering_point_manager').update_all(key: 'user.appointed_register_manager')
+          PublicActivity::Activity.where(key: 'group_metering_point_membership.create').update_all(key: 'group_register_membership.create')
+          PublicActivity::Activity.where(key: 'group_metering_point_request.create').update_all(key: 'group_register_request.create')
+          PublicActivity::Activity.where(key: 'group_metering_point_request.update').update_all(key: 'group_register_request.update')
+          PublicActivity::Activity.where(key: 'metering_point_user_request.create').update_all(key: 'register_user_request.create')
+          PublicActivity::Activity.where(key: 'metering_point_user_request.update').update_all(key: 'register_user_request.update')
+          PublicActivity::Activity.where(key: 'metering_point_user_request.destroy').update_all(key: 'register_user_request.destroy')
+          PublicActivity::Activity.where(key: 'group_metering_point_request.destroy').update_all(key: 'group_register_request.destroy')
+          PublicActivity::Activity.where(key: 'group_metering_point_invitation.create').update_all(key: 'group_register_invitation.create')
+          PublicActivity::Activity.where(key: 'group_metering_point_membership.cancel').update_all(key: 'group_register_membership.cancel')
+        end
+      end
+
+      dir.down do
+        ActiveRecord::Base.transaction do
+          PublicActivity::Activity.where(key: 'user.appointed_register_manager').update_all(key: 'user.appointed_metering_point_manager')
+          PublicActivity::Activity.where(key: 'group_register_membership.create').update_all(key: 'group_metering_point_membership.create')
+          PublicActivity::Activity.where(key: 'group_register_request.create').update_all(key: 'group_metering_point_request.create')
+          PublicActivity::Activity.where(key: 'group_register_request.update').update_all(key: 'group_metering_point_request.update')
+          PublicActivity::Activity.where(key: 'register_user_request.create').update_all(key: 'metering_point_user_request.create')
+          PublicActivity::Activity.where(key: 'register_user_request.update').update_all(key: 'metering_point_user_request.update')
+          PublicActivity::Activity.where(key: 'register_user_request.destroy').update_all(key: 'metering_point_user_request.destroy')
+          PublicActivity::Activity.where(key: 'group_register_request.destroy').update_all(key: 'group_metering_point_request.destroy')
+          PublicActivity::Activity.where(key: 'group_register_invitation.create').update_all(key: 'group_metering_point_invitation.create')
+          PublicActivity::Activity.where(key: 'group_register_membership.cancel').update_all(key: 'group_metering_point_membership.cancel')
+        end
+      end
+    end
+  end
+end
