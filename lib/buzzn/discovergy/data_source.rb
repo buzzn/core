buzzn/core

commit 68f864bb0fe7503a7db9b9346f604cf351eabf22

virtual register current power test and mode fix in data source

lib/buzzn/discovergy/data_source.rb
@@ -53,10 +53,10 @@ module Buzzn::Discovergy
       brokers.each do |broker|
         two_way_meter = broker.two_way_meter?
 
-        response = @facade.readings(broker, nil, mode, false)
+        response = @facade.readings(broker, nil, (!two_way_meter && mode == :out) ? :in : mode, two_way_meter, false)
 
         # this is because out meters (one_way) at discovergy reveal their energy data within the field 'energy' instead of 'energyOut'
-        result = add(result, parse_aggregated_live(response, (!two_way_meter && mode == :out) ? :in : mode, two_way_meter, register_or_group.id))
+        result = add(result, parse_aggregated_live(response, mode, two_way_meter, register_or_group.id))
       end
       result.freeze if result
       result

spec/lib/buzzn/current_power_spec.rb
@@ -49,6 +49,16 @@ describe Buzzn::CurrentPower do
     register
   end
 
+  let(:virtual_register) do
+    easymeter_60051599 = Fabricate(:easymeter_60051599)
+    easymeter_60051599.broker = Fabricate(:discovergy_broker, mode: 'out', external_id: "EASYMETER_60051599", resource: easymeter_60051599)
+    fichtenweg8 = Fabricate(:virtual_meter_fichtenweg8).register
+    Fabricate(:fp_plus, operand: easymeter_60051599.registers.first, register: fichtenweg8)
+    Fabricate(:fp_plus, operand: easymeter_60051599.registers.first, register: fichtenweg8)
+    Fabricate(:fp_minus, operand: easymeter_60051599.registers.first, register: fichtenweg8)
+    fichtenweg8
+  end
+
   it 'delivers the right result for a register' do
     result = subject.for_register(dummy_register)
     expect(result).to eq [:single_aggregated, dummy_register, :in]
@@ -75,6 +85,14 @@ describe Buzzn::CurrentPower do
 
     expect { subject.for_group(group, 'a') }.to raise_error ArgumentError
     expect { subject.for_group(Object.new) }.to raise_error ArgumentError
+  end
+
+  it 'delivers the right current power for a virtual register' do |spec|
+    VCR.use_cassette("lib/buzzn/#{spec.metadata[:description].downcase}") do
+      result = subject.for_register(virtual_register)
+      result_single = subject.for_register(virtual_register.formula_parts.first.operand)
+      expect(result).to eq result_single
+    end
   end
 
 end

spec/vcr_cassettes/lib/buzzn/delivers_the_right_current_power_for_a_virtual_register.yml
@@ -1,0 +1,214 @@
+---
+http_interactions:
+- request:
+    method: post
+    uri: https://api.discovergy.com/public/v1/oauth1/consumer_token?client=buzzn+app+test
+    body:
+      encoding: US-ASCII
+      string: ''
+    headers:
+      User-Agent:
+      - Faraday v0.9.2
+      Content-Type:
+      - application/x-www-form-urlencoded
+  response:
+    status:
+      code: 200
+      message:
+    headers:
+      server:
+      - nginx/1.10.0
+      date:
+      - Tue, 28 Feb 2017 16:47:21 GMT
+      content-type:
+      - application/json
+      transfer-encoding:
+      - chunked
+      connection:
+      - keep-alive
+      access-control-allow-origin:
+      - "*"
+      access-control-allow-methods:
+      - GET, POST, DELETE, PUT
+      access-control-allow-headers:
+      - X-Requested-With, Content-Type
+    body:
+      encoding: ASCII-8BIT
+      string: '{"key":"cb1pf7u799dlifqabnm6pitgg","secret":"jtnm64joct7037h9q8o9e8qt9b","owner":"buzzn
+        app test","attributes":{},"principal":null}'
+    http_version:
+  recorded_at: Sat, 02 Jul 2016 08:05:00 GMT
+- request:
+    method: post
+    uri: https://api.discovergy.com/public/v1/oauth1/request_token
+    body:
+      encoding: UTF-8
+      string: ''
+    headers:
+      Accept-Encoding:
+      - gzip;q=1.0,deflate;q=0.6,identity;q=0.3
+      Accept:
+      - "*/*"
+      User-Agent:
+      - OAuth gem v0.5.1
+      Content-Length:
+      - '0'
+      Authorization:
+      - OAuth oauth_callback="oob", oauth_consumer_key="cb1pf7u799dlifqabnm6pitgg",
+        oauth_nonce="A80dd1JHWfobklCxVTraeeOFf6p4NCvDl6IAg7knT8", oauth_signature="MMGQHrpu2qrbSITmBihMRzCPBKM%3D",
+        oauth_signature_method="HMAC-SHA1", oauth_timestamp="1467446700", oauth_version="1.0"
+  response:
+    status:
+      code: 200
+      message: OK
+    headers:
+      Server:
+      - nginx/1.10.0
+      Date:
+      - Tue, 28 Feb 2017 16:47:21 GMT
+      Content-Type:
+      - application/x-www-form-urlencoded
+      Content-Length:
+      - '126'
+      Connection:
+      - keep-alive
+      Access-Control-Allow-Origin:
+      - "*"
+      Access-Control-Allow-Methods:
+      - GET, POST, DELETE, PUT
+      Access-Control-Allow-Headers:
+      - X-Requested-With, Content-Type
+    body:
+      encoding: UTF-8
+      string: oauth_token=21baad6904ed445d95467c03618144a4&oauth_token_secret=be1ca0d9050a45daa61dab347db47e56&oauth_callback_confirmed=true
+    http_version:
+  recorded_at: Sat, 02 Jul 2016 08:05:00 GMT
+- request:
+    method: get
+    uri: https://api.discovergy.com/public/v1/oauth1/authorize?email=team%40localpool.de&oauth_token=21baad6904ed445d95467c03618144a4&password=Zebulon_4711
+    body:
+      encoding: US-ASCII
+      string: ''
+    headers:
+      User-Agent:
+      - Faraday v0.9.2
+      Content-Type:
+      - text/plain
+  response:
+    status:
+      code: 200
+      message:
+    headers:
+      server:
+      - nginx/1.10.0
+      date:
+      - Tue, 28 Feb 2017 16:47:22 GMT
+      content-type:
+      - application/x-www-form-urlencoded
+      content-length:
+      - '47'
+      connection:
+      - keep-alive
+      access-control-allow-origin:
+      - "*"
+      access-control-allow-methods:
+      - GET, POST, DELETE, PUT
+      access-control-allow-headers:
+      - X-Requested-With, Content-Type
+    body:
+      encoding: UTF-8
+      string: oauth_verifier=7d15481c5308471f9352f095a3389c9c
+    http_version:
+  recorded_at: Sat, 02 Jul 2016 08:05:01 GMT
+- request:
+    method: post
+    uri: https://api.discovergy.com/public/v1/oauth1/access_token
+    body:
+      encoding: UTF-8
+      string: ''
+    headers:
+      Accept-Encoding:
+      - gzip;q=1.0,deflate;q=0.6,identity;q=0.3
+      Accept:
+      - "*/*"
+      User-Agent:
+      - OAuth gem v0.5.1
+      Content-Length:
+      - '0'
+      Authorization:
+      - OAuth oauth_consumer_key="cb1pf7u799dlifqabnm6pitgg", oauth_nonce="z7nxQxc5ucZyfHXHmifcB8JvqfJlmTT4wULzegTrzHQ",
+        oauth_signature="nvivvITwXFc1Q3LAJlUAAwda8MA%3D", oauth_signature_method="HMAC-SHA1",
+        oauth_timestamp="1467446701", oauth_token="21baad6904ed445d95467c03618144a4",
+        oauth_verifier="7d15481c5308471f9352f095a3389c9c", oauth_version="1.0"
+  response:
+    status:
+      code: 200
+      message: OK
+    headers:
+      Server:
+      - nginx/1.10.0
+      Date:
+      - Tue, 28 Feb 2017 16:47:22 GMT
+      Content-Type:
+      - application/x-www-form-urlencoded
+      Content-Length:
+      - '96'
+      Connection:
+      - keep-alive
+      Access-Control-Allow-Origin:
+      - "*"
+      Access-Control-Allow-Methods:
+      - GET, POST, DELETE, PUT
+      Access-Control-Allow-Headers:
+      - X-Requested-With, Content-Type
+    body:
+      encoding: UTF-8
+      string: oauth_token=dacf771f00e046309b03ee045cfba06d&oauth_token_secret=1b86ff19a06a49299fa2cf936d7f8114
+    http_version:
+  recorded_at: Sat, 02 Jul 2016 08:05:01 GMT
+- request:
+    method: get
+    uri: https://api.discovergy.com/public/v1/last_reading?each=false&fields=power&meterId=EASYMETER_60051599
+    body:
+      encoding: US-ASCII
+      string: ''
+    headers:
+      Accept-Encoding:
+      - gzip;q=1.0,deflate;q=0.6,identity;q=0.3
+      Accept:
+      - "*/*"
+      User-Agent:
+      - OAuth gem v0.5.1
+      Authorization:
+      - OAuth oauth_consumer_key="cb1pf7u799dlifqabnm6pitgg", oauth_nonce="Zf9PEoOSAdWcxOdb6pARUCsV7gTE6XuHGNMqiSfMA",
+        oauth_signature="B0cL4jY2MC%2BCFBsya75pzirMxi8%3D", oauth_signature_method="HMAC-SHA1",
+        oauth_timestamp="1467446701", oauth_token="dacf771f00e046309b03ee045cfba06d",
+        oauth_version="1.0"
+  response:
+    status:
+      code: 200
+      message: OK
+    headers:
+      Server:
+      - nginx/1.10.0
+      Date:
+      - Tue, 28 Feb 2017 16:47:22 GMT
+      Content-Type:
+      - text/html;charset=iso-8859-1
+      Content-Length:
+      - '340'
+      Connection:
+      - keep-alive
+      Access-Control-Allow-Origin:
+      - "*"
+      Access-Control-Allow-Methods:
+      - GET, POST, DELETE, PUT
+      Access-Control-Allow-Headers:
+      - X-Requested-With, Content-Type
+      Cache-Control:
+      - must-revalidate,no-cache,no-store
+    body:
+      encoding: UTF-8
+      string: "{\"time\":1467446702088,\"values\":{\"power\":1100640}}"
+    http_version:
+  recorded_at: Sat, 02 Jul 2016 08:05:01 GMT

spec/vcr_cassettes/lib/calculates_autarchy.yml
@@ -679,4 +679,162 @@ http_interactions:
       string: '[{"time":1480606200000,"values":{"energy":22968322444644}},{"time":1480607100000,"values":{"energy":22970988922089}},{"time":1480608000000,"values":{"energy":22973229616478}}]'
     http_version: 
   recorded_at: Thu, 05 Jan 2017 16:39:23 GMT
+- request:
+    method: get
+    uri: https://api.discovergy.com/public/v1/readings?each=false&fields=energy&from=1475618400000&meterId=EASYMETER_60051559&resolution=fifteen_minutes&to=1475704800000
+    body:
+      encoding: US-ASCII
+      string: ''
+    headers:
+      Accept-Encoding:
+      - gzip;q=1.0,deflate;q=0.6,identity;q=0.3
+      Accept:
+      - "*/*"
+      User-Agent:
+      - OAuth gem v0.5.1
+      Authorization:
+      - OAuth oauth_consumer_key="ac55og2suqqetopa2evflmt14d", oauth_nonce="PTxi9QyDDFpguAbtvcRGYMLgTGknmFPXLQ19EKJBZ8",
+        oauth_signature="VnmwiyfJD9eofrDRcGI27o616y4%3D", oauth_signature_method="HMAC-SHA1",
+        oauth_timestamp="1467446701", oauth_token="95696c85a73e4b3ba805f4441adb0c4b",
+        oauth_version="1.0"
+  response:
+    status:
+      code: 401
+      message: Unauthorized
+    headers:
+      Server:
+      - nginx/1.10.0
+      Date:
+      - Tue, 28 Feb 2017 17:14:48 GMT
+      Content-Type:
+      - text/html;charset=iso-8859-1
+      Content-Length:
+      - '336'
+      Connection:
+      - keep-alive
+      Www-Authenticate:
+      - OAuth realm="default"
+      Access-Control-Allow-Origin:
+      - "*"
+      Access-Control-Allow-Methods:
+      - GET, POST, DELETE, PUT
+      Access-Control-Allow-Headers:
+      - X-Requested-With, Content-Type
+      Cache-Control:
+      - must-revalidate,no-cache,no-store
+    body:
+      encoding: UTF-8
+      string: |
+        <html>
+        <head>
+        <meta http-equiv="Content-Type" content="text/html;charset=ISO-8859-1"/>
+        <title>Error 401 </title>
+        </head>
+        <body>
+        <h2>HTTP ERROR: 401</h2>
+        <p>Problem accessing /public/v1/readings. Reason:
+        <pre>    Unauthorized</pre></p>
+        <hr /><a href="http://eclipse.org/jetty">Powered by Jetty:// 9.3.z-SNAPSHOT</a><hr/>
+        </body>
+        </html>
+    http_version: 
+  recorded_at: Sat, 02 Jul 2016 08:05:01 GMT
+- request:
+    method: post
+    uri: https://api.discovergy.com/public/v1/oauth1/consumer_token?client=buzzn+app+test
+    body:
+      encoding: US-ASCII
+      string: ''
+    headers:
+      User-Agent:
+      - Faraday v0.9.2
+      Content-Type:
+      - application/x-www-form-urlencoded
+  response:
+    status:
+      code: 200
+      message: 
+    headers:
+      server:
+      - nginx/1.10.0
+      date:
+      - Tue, 28 Feb 2017 17:14:48 GMT
+      content-type:
+      - application/json
+      transfer-encoding:
+      - chunked
+      connection:
+      - keep-alive
+      access-control-allow-origin:
+      - "*"
+      access-control-allow-methods:
+      - GET, POST, DELETE, PUT
+      access-control-allow-headers:
+      - X-Requested-With, Content-Type
+    body:
+      encoding: ASCII-8BIT
+      string: '{"key":"8061ltgcsgtons5sm9rrgd155b","secret":"rt5hkca4mckm4mq4e24r8g8vk","owner":"buzzn
+        app test","attributes":{},"principal":null}'
+    http_version: 
+  recorded_at: Sat, 02 Jul 2016 08:05:01 GMT
+- request:
+    method: get
+    uri: https://api.discovergy.com/public/v1/readings?each=false&fields=energy&from=1475618400000&meterId=EASYMETER_60051559&resolution=fifteen_minutes&to=1475704800000
+    body:
+      encoding: US-ASCII
+      string: ''
+    headers:
+      Accept-Encoding:
+      - gzip;q=1.0,deflate;q=0.6,identity;q=0.3
+      Accept:
+      - "*/*"
+      User-Agent:
+      - OAuth gem v0.5.1
+      Authorization:
+      - OAuth oauth_consumer_key="ac55og2suqqetopa2evflmt14d", oauth_nonce="HAQzaX3NmqQp6rCfp5e7gzVcFce2ce4VZ1HxRdhY8Dw",
+        oauth_signature="9IlRvAiKaJCYm27R%2B7X0%2BKjJhCo%3D", oauth_signature_method="HMAC-SHA1",
+        oauth_timestamp="1467446701", oauth_token="28c4d352801c4d7fa2a7e08cbcb1bd1c",
+        oauth_version="1.0"
+  response:
+    status:
+      code: 401
+      message: Unauthorized
+    headers:
+      Server:
+      - nginx/1.10.0
+      Date:
+      - Tue, 28 Feb 2017 17:14:48 GMT
+      Content-Type:
+      - text/html;charset=iso-8859-1
+      Content-Length:
+      - '336'
+      Connection:
+      - keep-alive
+      Www-Authenticate:
+      - OAuth realm="default"
+      Access-Control-Allow-Origin:
+      - "*"
+      Access-Control-Allow-Methods:
+      - GET, POST, DELETE, PUT
+      Access-Control-Allow-Headers:
+      - X-Requested-With, Content-Type
+      Cache-Control:
+      - must-revalidate,no-cache,no-store
+    body:
+      encoding: UTF-8
+      string: |
+        <html>
+        <head>
+        <meta http-equiv="Content-Type" content="text/html;charset=ISO-8859-1"/>
+        <title>Error 401 </title>
+        </head>
+        <body>
+        <h2>HTTP ERROR: 401</h2>
+        <p>Problem accessing /public/v1/readings. Reason:
+        <pre>    Unauthorized</pre></p>
+        <hr /><a href="http://eclipse.org/jetty">Powered by Jetty:// 9.3.z-SNAPSHOT</a><hr/>
+        </body>
+        </html>
+    http_version: 
+  recorded_at: Sat, 02 Jul 2016 08:05:01 GMT
 recorded_with: VCR 3.0.3

commit d41c4aba0bd5f155ba31d9f19203bd0e4e79da0d

fix regression introduced by 68f864bb

lib/buzzn/discovergy/data_source.rb
@@ -53,7 +53,7 @@ module Buzzn::Discovergy
       brokers.each do |broker|
         two_way_meter = broker.two_way_meter?
 
-        response = @facade.readings(broker, nil, (!two_way_meter && mode == :out) ? :in : mode, two_way_meter, false)
+        response = @facade.readings(broker, nil, (!two_way_meter && mode == :out) ? :in : mode, false)
 
         # this is because out meters (one_way) at discovergy reveal their energy data within the field 'energy' instead of 'energyOut'
         result = add(result, parse_aggregated_live(response, mode, two_way_meter, register_or_group.id))
@@ -284,7 +284,6 @@ module Buzzn::Discovergy
 
     def parse_virtual_meter_creation(response, mode, resource)
       json = MultiJson.load(response)
-      #binding.pry
       # TODO: Move credentials into secrets
       broker = Broker::Discovergy.create!(
         mode: mode,