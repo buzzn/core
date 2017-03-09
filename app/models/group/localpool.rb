8bec228e038497fb2895b5f02b4e00153064e9e3
Christian <christian@buzzn.net>
2017-03-09 13:07:50 +0100

    multiple address at localpool

--- app/models/group/localpool.rb
+++ app/models/group/localpool.rb
@@ -4,5 +4,13 @@ module Group
     has_one  :metering_point_operator_contract, class_name: Contract::MeteringPointOperator, foreign_key: :localpool_id
     has_one  :localpool_processing_contract,    class_name: Contract::LocalpoolProcessing, foreign_key: :localpool_id
 
+    has_many :addresses, as: :addressable, dependent: :destroy
+
+    # use first address as main address
+    # maybe improve this so that the user can select between all addresses
+    def main_address
+      self.addresses.order("created_at ASC").first
+    end
+
   end
 end
