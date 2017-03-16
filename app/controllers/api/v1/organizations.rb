e7e086a4ada056568144c10fa08ee2de86eb9996
Christian Meier <meier.kristian@gmail.com>
2017-03-16 16:05:55 +0100

    add guarded_nested to guarded_crud module
    and use it for arganization bank_account and address API
    fixes #888

--- app/controllers/api/v1/organizations.rb
+++ app/controllers/api/v1/organizations.rb
@@ -72,7 +72,7 @@ module API
         oauth2 false
         get ':id/address' do
           organization = Organization.guarded_retrieve(current_user, permitted_params)
-          organization.address.guarded_retrieve(current_user)
+          organization.guarded_nested_retrieve(:address, current_user)
         end
 
 
@@ -83,7 +83,7 @@ module API
         oauth2 :full
         get ':id/bank_account' do
           organization = Organization.guarded_retrieve(current_user, permitted_params)
-          organization.bank_account #.guarded_retrieve(current_user)
+          organization.guarded_nested_retrieve(:bank_account, current_user)
         end
 
 
