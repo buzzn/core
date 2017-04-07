8dba01fb4ef601237c95a1fac8b8cb7be1c5d299
Christian Meier <meier.kristian@gmail.com>
2017-04-07 08:36:37 +0200

    remove obsolete organizations endpoints

--- app/controllers/api/v1/organizations.rb
+++ app/controllers/api/v1/organizations.rb
@@ -5,17 +5,6 @@ module API
 
       resource :organizations do
 
-        desc "Return all organizations"
-        params do
-          optional :filter, type: String, desc: "Search query using #{Base.join(Organization.search_attributes)}"
-        end
-        oauth2 false
-        get do
-          OrganizationResource.all(current_user, permitted_params[:filter])
-        end
-
-
-
         desc "Return an Organization"
         params do
           requires :id, type: String, desc: "ID of the organization"
@@ -24,31 +13,6 @@ module API
         get ":id" do
           OrganizationResource.retrieve(current_user, permitted_params)
         end
-
-
-        desc 'Return the related managers of an organization'
-        params do
-          requires :id, type: String, desc: 'ID of the organization'
-        end
-        oauth2 false
-        get [':id/managers', ':id/relationships/managers'] do
-          OrganizationResource
-            .retrieve(current_user, permitted_params)
-            .managers
-        end
-
-
-        desc 'Return the related members of an organization'
-        params do
-          requires :id, type: String, desc: 'ID of the organization'
-        end
-        oauth2 false
-        get [':id/members', ':id/relationships/members'] do
-          OrganizationResource
-            .retrieve(current_user, permitted_params)
-            .members
-        end
-
 
         desc 'Return the related address for an organization'
         params do
@@ -71,152 +35,6 @@ module API
           OrganizationResource
             .retrieve(current_user, permitted_params)
             .bank_account!
-        end
-
-
-        desc "Create an Organization."
-        params do
-          requires :name,         type: String, desc: "Name of the Organization."
-          requires :phone,        type: String, desc: "Phone number of Organization."
-          requires :email,        type: String, desc: "Email of Organization."
-          requires :mode,         type: String, desc: 'Mode of Organization', values: Organization.modes
-          optional :fax,          type: String, desc: "Fax number of Organization."
-          optional :website,      type: String, desc: "Website of Organization."
-          optional :description,  type: String, desc: "Description of the Organization."
-        end
-        oauth2 :full
-        post do
-          created_response(OrganizationResource
-                            .create(current_user, permitted_params))
-        end
-
-
-
-        desc "Update an Organization."
-        params do
-          requires :id, type: String, desc: "Organization ID."
-          optional :name,         type: String, desc: "Name of the Organization."
-          optional :phone,        type: String, desc: "Phone number of Organization."
-          optional :fax,          type: String, desc: "Fax number of Organization."
-          optional :website,      type: String, desc: "Website of Organization."
-          optional :email,        type: String, desc: "Email of Organization."
-          optional :description,  type: String, desc: "Description of the Organization."
-          optional :mode,         type: String, desc: 'Mode of Organization', values: Organization.modes
-        end
-        oauth2 :full
-        patch ':id' do
-          OrganizationResource
-            .retrieve(current_user, permitted_params)
-            .update(permitted_params)
-        end
-
-
-
-        desc 'Delete an Organization.'
-        params do
-          requires :id, type: String, desc: "Organization ID"
-        end
-        oauth2 :full
-        delete ':id' do
-          deleted_response(
-            OrganizationResource
-              .retrieve(current_user, permitted_params)
-              .delete)
-        end
-
-
-        desc 'Add user to organization managers'
-        params do
-          requires :id, type: String, desc: "Organization ID"
-          requires :data, type: Hash do
-            requires :id, type: String, desc: "ID of the user"
-          end
-        end
-        oauth2 :full
-        post ':id/relationships/managers' do
-          organization = Organization.guarded_retrieve(current_user, permitted_params)
-          user         = User.unguarded_retrieve(permitted_params[:data][:id])
-          organization.managers.add(current_user, user)
-          status 204
-        end
-
-
-        desc 'Replace organization managers'
-        params do
-          requires :id, type: String, desc: "ID of the organization"
-          requires :data, type: Array do
-            requires :id, type: String, desc: "ID of the user"
-          end
-        end
-        oauth2 :full
-        patch ':id/relationships/managers' do
-          organization = Organization.guarded_retrieve(current_user, permitted_params)
-          organization.managers.replace(current_user, data_id_array)
-          status 200
-        end
-
-
-        desc 'Remove user from organization managers'
-        params do
-          requires :id, type: String, desc: "Organization ID"
-          requires :data, type: Hash do
-            requires :id, type: String, desc: "ID of the user"
-          end
-        end
-        oauth2 :full
-        delete ':id/relationships/managers' do
-          organization = Organization.guarded_retrieve(current_user, permitted_params)
-          user         = User.unguarded_retrieve(permitted_params[:data][:id])
-          organization.managers.remove(current_user, user)
-          status 204
-        end
-
-
-        desc 'Add user to organization members'
-        params do
-          requires :id, type: String, desc: "Organization ID"
-          requires :data, type: Hash do
-            requires :id, type: String, desc: "ID of the user"
-          end
-        end
-        oauth2 :full
-        post ':id/relationships/members' do
-          organization = Organization.guarded_retrieve(current_user, permitted_params)
-          user         = User.unguarded_retrieve(permitted_params[:data][:id])
-          organization.members.add(current_user, user)
-          status 204
-        end
-
-
-        desc 'Replace organization members'
-        params do
-          requires :id, type: String, desc: "ID of the organization"
-          requires :data, type: Array do
-            requires :id, type: String, desc: "ID of the user"
-          end
-        end
-        oauth2 :full
-        patch ':id/relationships/members' do
-          organization = Organization.guarded_retrieve(current_user, permitted_params)
-          organization.members.replace(current_user, data_id_array)
-          status 200
-        end
-
-
-        desc 'Remove user from organization members'
-        params do
-          requires :id, type: String, desc: "Organization ID"
-          requires :data, type: Hash do
-            optional :type, type: String, values: [Organization.to_s], default: Organization.to_s
-            requires :id, type: String, desc: "ID of the user"
-          end
-        end
-        oauth2 :full
-        delete ':id/relationships/members' do
-          organization = Organization.guarded_retrieve(current_user, permitted_params)
-          user         = User.unguarded_retrieve(permitted_params[:data][:id])
-          organization.members.remove(current_user, user)
-          status 204
         end
       end
     end
