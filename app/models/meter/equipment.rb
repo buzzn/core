3fef2552ad152fa3efcf53109dd4dec110b3130c
Christian <christian@buzzn.net>
2017-04-07 13:02:50 +0200

    add validation to equipment

--- app/models/meter/equipment.rb
+++ app/models/meter/equipment.rb
@@ -1,5 +1,14 @@
 module Meter
   class Equipment < ActiveRecord::Base
-    belongs_to :meter, class_name: Meter::Base
+    belongs_to :meter, class_name: Meter::Base, foreign_key: 'meter_id'
+
+    before_destroy :check_for_main_equipment
+
+    def check_for_main_equipment
+      if meter.main_equipment == self
+        meter.errors.add(:main_equipment, 'meter must have one main eqipment')
+        raise Buzzn::NestedValidationError.new(:meter, meter.errors)
+      end
+    end
   end
 end
