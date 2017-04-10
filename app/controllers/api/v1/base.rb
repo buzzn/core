6d5290896ed147726a0d323c9d77b859de9b20fe
Christian Meier <meier.kristian@gmail.com>
2017-04-10 08:53:22 +0200

    reintroduce validation on registers' label

--- app/controllers/api/v1/base.rb
+++ app/controllers/api/v1/base.rb
@@ -18,7 +18,6 @@ module API
 
       mount API::V1::Users
       mount API::V1::Meters
-      mount API::V1::Registers
       mount API::V1::Readings
       mount API::V1::Aggregates
       mount API::V1::Groups
