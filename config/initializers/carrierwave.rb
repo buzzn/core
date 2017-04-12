fca73518d1eeb3139af864bed0e755e98e4f9635
Felix Faerber <ffaerber@gmail.com>
2017-04-12 11:22:09 +0200

    CarrierWave s3 in production_legacy

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,7 @@ CarrierWave.configure do |config|
 
   config.asset_host = Rails.application.secrets.asset_host
 
-  if Rails.env.production? || Rails.env.staging?
+  if Rails.env.production? || Rails.env.staging? || Rails.env.production_legacy?
     config.storage = :fog
     config.fog_credentials  = {
       provider:               'AWS',
