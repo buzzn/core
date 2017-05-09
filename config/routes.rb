a694e66da6b2b2dc24ff44565bf639d7b415d5a6
Felix Faerber <ffaerber@gmail.com>
2017-05-09 18:17:35 +0200

    ActiveAdmin route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Buzzn::Application.routes.draw do
-
   default_url_options host: Rails.application.secrets.hostname
 
   use_doorkeeper do
     controllers :applications => 'oauth/applications'
   end
+
+  ActiveAdmin.routes(self)
 
   mount API::Base, at: "/"
   mount GrapeSwaggerRails::Engine, at: "/"
