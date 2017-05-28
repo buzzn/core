5fa23b0caf1558a3702aa9e67f2beeaef547ec52
Felix Faerber <ffaerber@gmail.com>
2017-05-28 15:40:31 +0200

    serve_static_files in staging and production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Buzzn::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_files = false
+  config.serve_static_files = true
 
   # Compress JavaScripts and CSS.
   config.assets.compress = true
