f0101d516c1673b0208015606e9642c5a59af039
Christian Meier <meier.kristian@gmail.com>
2017-05-31 16:25:30 +0200

    add permissions tests for display

--- app/roda/core_roda.rb
+++ app/roda/core_roda.rb
@@ -32,48 +32,54 @@ class CoreRoda < Roda
 
   route do |r|
 
-    r.on 'display' do
-      r.run Display::BaseRoda
-    end
-
-    r.on 'admin' do
-      r.run Admin::BaseRoda
-    end
-
-    # TODO remove thos deprecated paths
-
-    r.on 'api/v1' do
-
-      r.on 'me' do
-        r.run MeRoda
+    r.on 'api' do
+      r.on 'display' do
+        r.run Display::BaseRoda
       end
 
-      r.on 'bank-accounts' do
-        r.run BankAccountRoda
+      r.on 'admin' do
+        r.run Admin::BaseRoda
       end
 
-      r.on 'groups' do
-        r.run GroupRoda
-      end
+      # TODO remove thos deprecated paths
 
-      r.on 'organizations' do
-        r.run OrganizationLegacyRoda
-      end
+      r.on 'v1' do
 
-      r.on 'meters' do
-        r.run MeterLegacyRoda
-      end
+        r.on 'aggregate' do
+          r.run AggregateRoda
+        end
 
-      r.on 'registers' do
-        r.run RegisterLegacyRoda
-      end
+        r.on 'me' do
+          r.run MeRoda
+        end
 
-      r.on 'contracts' do
-        r.run ContractLegacyRoda
-      end
+        r.on 'bank-accounts' do
+          r.run BankAccountRoda
+        end
 
-      r.on 'users' do
-        r.run UserLegacyRoda
+        r.on 'groups' do
+          r.run GroupRoda
+        end
+
+        r.on 'organizations' do
+          r.run OrganizationLegacyRoda
+        end
+
+        r.on 'meters' do
+          r.run MeterLegacyRoda
+        end
+
+        r.on 'registers' do
+          r.run RegisterLegacyRoda
+        end
+
+        r.on 'contracts' do
+          r.run ContractLegacyRoda
+        end
+
+        r.on 'users' do
+          r.run UserLegacyRoda
+        end
       end
     end
 
---
+++ spec/permissions/display/group_spec.rb
@@ -0,0 +1,98 @@
+# coding: utf-8
+describe Display::GroupPermissions do
+
+  entity(:admin) { Fabricate(:admin) }
+  entity(:manager) { Fabricate(:user) }
+  entity(:other) { Fabricate(:user) }
+  let(:anonymous) { nil }
+
+  entity!(:tribe) do
+    group = Fabricate(:tribe)
+    Fabricate(:real_meter).registers.first.update(group: group)
+    manager.add_role(:manager, group)
+    group
+  end
+
+  entity!(:localpool)  do
+    group = Fabricate(:localpool)
+    Fabricate(:virtual_meter).register.update(group: group)
+    manager.add_role(:manager, group)
+    group
+  end
+
+  [:admin, :manager, :other, :anonymous].each do |user|
+    context "user<#{user}>" do
+      let(:all) { Display::GroupResource.all(send(user)) }
+      it "all - groups" do
+        expect(all.collect { |l| l.object }).to match_array [tribe, localpool]
+      end
+
+      [:tribe, :localpool].each do |type|
+        context "group<#{type}>" do
+          let(:group) { all.retrieve(send(type).id) }
+
+          it "retrieve" do
+            expect(group.object).to eq send(type)
+          end
+
+          it 'update' do
+            expect { group.update({}) }.to raise_error Buzzn::PermissionDenied
+          end
+
+          it 'delete' do
+            expect { group.delete }.to raise_error Buzzn::PermissionDenied
+          end
+
+          context 'registers' do
+            let(:registers) { group.registers }
+
+            it 'all' do
+              expect(registers.collect { |l| l.object }).to match_array group.object.registers.consumption_production
+            end
+            let(:register) { registers.retrieve(registers.first.id) }
+
+            it "retrieve" do
+              expect(register.object).to eq registers.first.object
+            end
+
+            it 'update' do
+              expect { register.update({}) }.to raise_error Buzzn::PermissionDenied
+            end
+
+            it 'delete' do
+              expect { register.delete }.to raise_error Buzzn::PermissionDenied
+            end
+          end
+
+          context 'mentors' do
+            let(:mentors) { group.mentors }
+
+            it 'all' do
+              expect(mentors.collect{ |l| l.object }).to match_array group.object.managers.collect {|m| m }
+            end
+            let(:mentor) { mentors.retrieve(mentors.first.id) }
+
+            it "retrieve" do
+              expect(mentor.object).to eq mentors.first.object
+            end
+
+            it 'update' do
+              expect { mentor.update({}) }.to raise_error Buzzn::PermissionDenied
+            end
+
+            it 'delete' do
+              expect { mentor.delete }.to raise_error Buzzn::PermissionDenied
+            end
+          end
+
+          context 'scores' do
+            let(:scores) { group.scores(interval: :day) }
+            it "retrieve" do
+              expect(scores.to_a).to eq []
+            end
+          end
+        end
+      end
+    end
+  end
+end
