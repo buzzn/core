20156ded9cc22f50cf97a4f289f87e76a4e11baa
Christian Meier <meier.kristian@gmail.com>
2017-05-31 16:25:30 +0200

    add permissions tests for display

--- app/roda/core_roda.rb
+++ app/roda/core_roda.rb
@@ -32,52 +32,54 @@ class CoreRoda < Roda
 
   route do |r|
 
-    r.on 'display' do
-      r.run Display::BaseRoda
-    end
-
-    r.on 'admin' do
-      r.run Admin::BaseRoda
-    end
-
-    # TODO remove thos deprecated paths
-
-    r.on 'api/v1' do
-
-      r.on 'aggregates' do
-        r.run AggregateRoda
+    r.on 'api' do
+      r.on 'display' do
+        r.run Display::BaseRoda
       end
 
-      r.on 'me' do
-        r.run MeRoda
+      r.on 'admin' do
+        r.run Admin::BaseRoda
       end
 
-      r.on 'bank-accounts' do
-        r.run BankAccountRoda
-      end
+      # TODO remove thos deprecated paths
 
-      r.on 'groups' do
-        r.run GroupRoda
-      end
+      r.on 'v1' do
 
-      r.on 'organizations' do
-        r.run OrganizationLegacyRoda
-      end
+        r.on 'aggregates' do
+          r.run AggregateRoda
+        end
 
-      r.on 'meters' do
-        r.run MeterLegacyRoda
-      end
+        r.on 'me' do
+          r.run MeRoda
+        end
 
-      r.on 'registers' do
-        r.run RegisterLegacyRoda
-      end
+        r.on 'bank-accounts' do
+          r.run BankAccountRoda
+        end
 
-      r.on 'contracts' do
-        r.run ContractLegacyRoda
-      end
+        r.on 'groups' do
+          r.run GroupRoda
+        end
 
-      r.on 'users' do
-        r.run UserLegacyRoda
+        r.on 'organizations' do
+          r.run OrganizationLegacyRoda
+        end
+
+        r.on 'meters' do
+          r.run MeterLegacyRoda
+        end
+
+        r.on 'registers' do
+          r.run RegisterLegacyRoda
+        end
+
+        r.on 'contracts' do
+          r.run ContractLegacyRoda
+        end
+
+        r.on 'users' do
+          r.run UserLegacyRoda
+        end
       end
     end
 
