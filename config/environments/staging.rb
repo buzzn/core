7bcd39e2d804b9a4340b9724a5b18d8093100aa2
Felix Faerber <ffaerber@gmail.com>
2017-06-06 12:31:26 +0200

    remove static loglevel from staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -43,8 +43,6 @@ Buzzn::Application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = false
 
-  # Set to :debug to see everything in the log.
-  config.log_level = :info
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
