65dcf872388c5ef498b21157ea4e68fc77891f10
Phillip Oertel <me@phillipoertel.com>
2017-09-07 21:00:35 +0200

    Remove actived admin (routes, gem and dependencies, config files)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -51,10 +51,6 @@ class ApplicationController < ActionController::Base
     redirect
   end
 
-  def authenticate_admin_user!
-    raise SecurityError unless current_user.has_role?(:admin)
-  end
-
   rescue_from SecurityError do |exception|
     redirect_to "/"
   end
