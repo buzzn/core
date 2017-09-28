9858c897a288bdd9564abd848a21192c57d95b8a
Phillip Oertel <me@phillipoertel.com>
2017-09-28 14:16:15 +0200

    Add specs that the fabricators work and set up associations as expected.

--- spec/fabricators/_fabricators_spec.rb
+++ spec/fabricators/_fabricators_spec.rb
@@ -3,10 +3,49 @@ describe "Fabricators produce valid records" do
   context "Device" do
     subject { Fabricate(:new_device) }
     it { is_expected.to be_valid }
+    it "has a register" do
+      expect(subject.register).to be_instance_of(Register::Input)
+    end
   end
 
   context "Localpool" do
     subject { Fabricate(:new_localpool) }
     it { is_expected.to be_valid }
+  end
+
+  context "Register::Input" do
+    subject { Fabricate(:new_register_input) }
+    it { is_expected.to be_valid }
+    it "has a group" do
+      expect(subject.group).to be_instance_of(Group::Localpool)
+    end
+    it "has a meter" do
+      expect(subject.meter).to be_instance_of(Meter::Real)
+    end
+    it "can override meter" do
+      meter    = Fabricate(:new_meter_real)
+      register = Fabricate(:new_register_input, meter: meter)
+      expect(register.meter).to eq(meter)
+      expect(register).to be_valid
+      meter.reload
+      expect(meter.registers.size).to eq(2)
+    end
+  end
+
+  context "Meter::Real" do
+    subject { Fabricate(:new_meter_real) }
+    it { is_expected.to be_valid }
+    it "has a group" do
+      expect(subject.group).to be_instance_of(Group::Localpool)
+    end
+    it "has a register" do
+      expect(subject.registers.first).to be_instance_of(Register::Input)
+    end
+    it "can override registers" do
+      register = Fabricate(:new_register_input)
+      meter    = Fabricate(:new_meter_real, registers: [register])
+      expect(meter.registers).to eq([register])
+      expect(meter).to be_valid
+    end
   end
 end
