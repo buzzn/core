a8d923b7551622fef410aaa230004c3dea634b27
Phillip Oertel <me@phillipoertel.com>
2017-10-04 18:10:06 +0200

    Move fixture files to a location where they are more expected. Will remove them after the revised fixtures have landed.

--- spec/fabricators/persons.rb
+++ spec/fabricators/persons.rb
@@ -9,7 +9,7 @@ Fabricator :person do
   first_name  { FFaker::Name.first_name.slice(0...30) }
   last_name   { FFaker::Name.last_name.slice(0...30) }
   phone       { FFaker::PhoneNumber.phone_number }
-  image       { File.new(Rails.root.join('db', 'seed_assets', 'profiles', "#{((i+=1)%60) + 1}.jpg")) }
+  image       { File.new(Rails.root.join('spec/fixture_files', 'profiles', "#{((i+=1)%60) + 1}.jpg")) }
 end
 
 
@@ -17,7 +17,7 @@ Fabricator :person_felix, from: :person do
   first_name  'Felix'
   last_name   'Faerber'
   title       nil
-#  image       { File.new(Rails.root.join('db', 'seed_assets', 'persons', 'felix.jpg')) }
+#  image       { File.new(Rails.root.join('spec/fixture_files', 'persons', 'felix.jpg')) }
 end
 
 
@@ -31,70 +31,70 @@ Fabricator :person_justus, from: :person do
   first_name  'Justus'
   last_name   'Schütze'
   title       nil
- # image       { File.new(Rails.root.join('db', 'seed_assets', 'persons', 'justus.jpg')) }
+ # image       { File.new(Rails.root.join('spec/fixture_files', 'persons', 'justus.jpg')) }
 end
 
 Fabricator :person_danusch, from: :person do
   first_name  'Danusch'
   last_name   'Mahmoudi'
   title       nil
-  #image       { File.new(Rails.root.join('db', 'seed_assets', 'persons', 'danusch.jpg')) }
+  #image       { File.new(Rails.root.join('spec/fixture_files', 'persons', 'danusch.jpg')) }
 end
 
 Fabricator :person_thomas, from: :person do
   first_name  'Thomas'
   last_name   'Theenhaus'
   title       nil
-  #image       { File.new(Rails.root.join('db', 'seed_assets', 'persons', 'thomas.jpg')) }
+  #image       { File.new(Rails.root.join('spec/fixture_files', 'persons', 'thomas.jpg')) }
 end
 
 Fabricator :person_eva, from: :person do
   first_name  'Eva'
   last_name   'Klopp'
   title       nil
-  #image       { File.new(Rails.root.join('db', 'seed_assets', 'persons', 'martina.jpg')) }
+  #image       { File.new(Rails.root.join('spec/fixture_files', 'persons', 'martina.jpg')) }
 end
 
 Fabricator :person_stefan, from: :person do
   first_name  'Stefan'
   last_name   'Erbacher'
   title       nil
- # image       { File.new(Rails.root.join('db', 'seed_assets', 'persons', 'stefan.jpg')) }
+ # image       { File.new(Rails.root.join('spec/fixture_files', 'persons', 'stefan.jpg')) }
 end
 
 Fabricator :person_karin, from: :person do
   first_name  'Karin'
   last_name   'Smith'
   title       nil
-  #image       { File.new(Rails.root.join('db', 'seed_assets', 'persons', 'karin.jpg')) }
+  #image       { File.new(Rails.root.join('spec/fixture_files', 'persons', 'karin.jpg')) }
 end
 
 Fabricator :person_pavel, from: :person do
   first_name  'Pavel'
   last_name   'Gorbachev'
   title       nil
-  #image       { File.new(Rails.root.join('db', 'seed_assets', 'persons', 'ole.jpg')) }
+  #image       { File.new(Rails.root.join('spec/fixture_files', 'persons', 'ole.jpg')) }
 end
 
 Fabricator :person_philipp, from: :person do
   first_name  'Philipp'
   last_name   'Osswald'
   title       nil
-  #image       { File.new(Rails.root.join('db', 'seed_assets', 'persons', 'philipp.jpg')) }
+  #image       { File.new(Rails.root.join('spec/fixture_files', 'persons', 'philipp.jpg')) }
 end
 
 Fabricator :person_christian, from: :person do
   first_name  'Christian'
   last_name   'Widmann'
   title       nil
-#  image       { File.new(Rails.root.join('db', 'seed_assets', 'persons', 'christian.jpg')) }
+#  image       { File.new(Rails.root.join('spec/fixture_files', 'persons', 'christian.jpg')) }
 end
 
 Fabricator :person_mustafa, from: :person do
   first_name  'Mustafa'
   last_name   'Akman'
   title       nil
- # image       { File.new(Rails.root.join('db', 'seed_assets', 'persons', "13.jpg")) }
+ # image       { File.new(Rails.root.join('spec/fixture_files', 'persons', "13.jpg")) }
 end
 
 Fabricator :person_kristian, from: :person do
@@ -109,14 +109,14 @@ Fabricator :person_jangerdes, from: :person do
   first_name  'Jan'
   last_name   'Gerdes'
   title       nil
-  #image       { File.new(Rails.root.join('db', 'seed_assets', 'persons', 'christian.jpg')) }
+  #image       { File.new(Rails.root.join('spec/fixture_files', 'persons', 'christian.jpg')) }
 end
 
 Fabricator :person_christian_schuetze, from: :person do
   first_name  'Christian'
   last_name   'Schütze'
   title       nil
-  #image       { File.new(Rails.root.join('db', 'seed_assets', 'persons', 'christian_schuetze.jpg')) }
+  #image       { File.new(Rails.root.join('spec/fixture_files', 'persons', 'christian_schuetze.jpg')) }
 end
 
 
