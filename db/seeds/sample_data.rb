29649b70f5afeddfcefa17266ad658416522a6d5
Phillip Oertel <me@phillipoertel.com>
2017-10-09 16:12:43 +0200

    Every powertaker gets a bank account; remove all delete_all calls (expecting an empty DB).

--- db/seeds/sample_data.rb
+++ db/seeds/sample_data.rb
@@ -9,7 +9,6 @@ FactoryGirl.find_definitions
 #
 # Create sample groups
 #
-Group::Localpool.destroy_all
 localpool_people_power  = FactoryGirl.create(:localpool, :people_power, owner: SeedsRepository.persons.wolfgang)
 localpool_hell_und_warm = FactoryGirl.create(:localpool, :hell_und_warm)
 
@@ -31,24 +30,25 @@ brumbauer.add_role(Role::GROUP_ADMIN, localpool_people_power) # can admin the or
 brumbauer.add_role(Role::ORGANIZATION, localpool_hell_und_warm) # can see the organization
 
 powertakers = {
-  pt1:  FactoryGirl.create(:person, first_name: 'Sabine',    last_name: 'Powertaker1', title: 'Prof.', prefix: 'F'),
-  pt2:  FactoryGirl.create(:person, first_name: 'Claudia',   last_name: 'Powertaker2', title: 'Prof. Dr.', prefix: 'F'),
-  pt3:  FactoryGirl.create(:person, first_name: 'Bernd',     last_name: 'Powertaker3'),
-  pt4:  FactoryGirl.create(:person, first_name: 'Karlheinz', last_name: 'Powertaker4'),
-  pt5a: FactoryGirl.create(:person, first_name: 'Sylvia',    last_name: 'Powertaker5a (zieht ein)', prefix: 'F'),
-  pt5b: FactoryGirl.create(:person, first_name: 'Fritz',     last_name: 'Powertaker5b (zieht aus)'),
-  pt6:  FactoryGirl.create(:person, first_name: 'Horst',     last_name: 'Powertaker6 (drittbeliefert)'),
-  pt7:  FactoryGirl.create(:person, first_name: 'Karla',     last_name: 'Powertaker7 (Mentor)', prefix: 'F'),
-  pt8:  FactoryGirl.create(:person, first_name: 'Geoffrey',  last_name: 'Powertaker8', preferred_language: 'english'),
-  pt9:  FactoryGirl.create(:person, first_name: 'Justine',   last_name: 'Powertaker9', prefix: 'F'),
-  pt10: FactoryGirl.create(:person, first_name: 'Mohammed',  last_name: 'Powertaker10')
+  pt1:  FactoryGirl.create(:person, :with_bank_account, first_name: 'Sabine',    last_name: 'Powertaker1', title: 'Prof.', prefix: 'F'),
+  pt2:  FactoryGirl.create(:person, :with_bank_account, first_name: 'Claudia',   last_name: 'Powertaker2', title: 'Prof. Dr.', prefix: 'F'),
+  pt3:  FactoryGirl.create(:person, :with_bank_account, first_name: 'Bernd',     last_name: 'Powertaker3'),
+  pt4:  FactoryGirl.create(:person, :with_bank_account, first_name: 'Karlheinz', last_name: 'Powertaker4'),
+  pt5a: FactoryGirl.create(:person, :with_bank_account, first_name: 'Sylvia',    last_name: 'Powertaker5a (zieht ein)', prefix: 'F'),
+  pt5b: FactoryGirl.create(:person, :with_bank_account, first_name: 'Fritz',     last_name: 'Powertaker5b (zieht aus)'),
+  pt6:  FactoryGirl.create(:person, :with_bank_account, first_name: 'Horst',     last_name: 'Powertaker6 (drittbeliefert)'),
+  pt7:  FactoryGirl.create(:person, :with_bank_account, first_name: 'Karla',     last_name: 'Powertaker7 (Mentor)', prefix: 'F'),
+  pt8:  FactoryGirl.create(:person, :with_bank_account, first_name: 'Geoffrey',  last_name: 'Powertaker8', preferred_language: 'english'),
+  pt9:  FactoryGirl.create(:person, :with_bank_account, first_name: 'Justine',   last_name: 'Powertaker9', prefix: 'F'),
+  pt10: FactoryGirl.create(:person, :with_bank_account, first_name: 'Mohammed',  last_name: 'Powertaker10')
 }
 powertakers.each do |_key, person|
-  FactoryGirl.create(:contract, :localpool_powertaker,
+  contract = FactoryGirl.create(:contract, :localpool_powertaker,
     localpool: localpool_people_power,
     contractor: localpool_people_power.owner,
     customer: person
   )
+  ap contract.register
   person.add_role(Role::GROUP_MEMBER, localpool_people_power) # can see his group
 end
 powertakers[:pt1].add_role(Role::SELF, localpool_people_power) # can see his profile
@@ -56,12 +56,10 @@ powertakers[:pt1].add_role(Role::CONTRACT, localpool_people_power) # can see his
 
 powertakers[:pt7].add_role(Role::GROUP_ENERGY_MENTOR, localpool_people_power)
 
-
 #FactoryGirl.create(:meter_real, :two_way, group: localpool_people_power)
 
 __END__
 
-[Reading::Continuous, Reading::Single].each(&:destroy_all)
 puts "\n* Readings"
 
 import_csv(:readings,
@@ -69,16 +67,7 @@ import_csv(:readings,
            fields: %i(date raw_value reason read_by comment)
 )
 
-Person.destroy_all
-import_csv(:persons,
-           converters: {preferred_language: Converters::PreferredLanguage },
-           fields: %i(first_name last_name email phone fax title  prefix preferred_language)
-)
-Person.all.each do |person|
-  FactoryGirl.create(:bank_account, contracting_party: person)
-end
 
-Organization.destroy_all
 import_csv(:organizations,
            converters: {preferred_language: Converters::PreferredLanguage },
            fields: %i(name description slug image email edifactemail phone fax website mode market_place_id)
@@ -87,7 +76,6 @@ Organization.all.each do |organization|
   FactoryGirl.create(:bank_account, contracting_party: organization)
 end
 
-Device.destroy_all
 import_csv(:devices,
            converters: { watt_peak: Converters::Number, watt_hour_pa: Converters::Number, commissioning: Converters::Date, mobile: Converters::Boolean, primary_energy: Converters::Downcase },
            fields: %i(manufacturer_name manufacturer_product_name manufacturer_product_serialnumber mode law category primary_energy watt_peak watt_hour_pa commissioning mobile)
@@ -99,5 +87,4 @@ import_csv(:payments,
 )
 
 puts "\n* Energy Classifications"
-EnergyClassification.destroy_all
 %i(buzzn germany).each { |trait| FactoryGirl.create(:energy_classification, trait) }
