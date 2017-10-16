301d8b02d528f8574990e1f1a679870334d9cc2a
Phillip Oertel <me@phillipoertel.com>
2017-10-16 11:42:56 +0200

    Remove unused file

--- spec/support/delete_all_records.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.eager_load!
-ActiveRecord::Base.connection.disable_referential_integrity do
-  ActiveRecord::Base.descendants.each { |model| model.delete_all unless model.abstract_class? }
-end
