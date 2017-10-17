19298b34e6e69aa1db5f34578b9190a794fd8513
Phillip Oertel <me@phillipoertel.com>
2017-10-17 14:45:24 +0200

    Adapt factory to removed "source" field.

--- db/factories/payments.rb
+++ db/factories/payments.rb
@@ -2,8 +2,7 @@ FactoryGirl.define do
   factory :payment, class: 'Contract::Payment' do
     price_cents  55_00
     begin_date   Date.parse("2016-01-01")
-    cycle        Contract::Payment::MONTHLY
-    source       Contract::Payment::CALCULATED
+    cycle        Contract::Payment.cycles[:monthly]
 
     before(:create) do |payment, _transients|
       payment.contract ||= FactoryGirl.create(:contract, :metering_point_operator)
