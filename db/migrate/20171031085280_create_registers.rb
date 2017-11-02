4ef3f230debb8b35031056712a45f3f002267ba5
Christian Meier <meier.kristian@gmail.com>
2017-11-02 08:49:36 +0100

    add references to grid_consumption and gred_feeding registers to localpool

--- db/migrate/20171031085280_create_registers.rb
+++ db/migrate/20171031085280_create_registers.rb
@@ -12,8 +12,14 @@ class CreateRegisters < ActiveRecord::Migration
     add_belongs_to :registers, :meter, type: :uuid, index: true, null: false
     add_belongs_to :registers, :group, reference: :register, type: :uuid, index: true, null: true
 
+    add_belongs_to :groups, :grid_consumption_register, reference: :registers, type: :uuid, index: true, null: true
+    add_belongs_to :groups, :grid_feeding_register, reference: :registers, type: :uuid, index: true, null: true
+
     add_foreign_key :registers, :meters, name: :fk_registers_meter
     add_foreign_key :registers, :groups, name: :fk_registers_group
+
+    add_foreign_key :groups, :registers, name: :fk_groups_grid_consumption, column: :grid_consumption_register_id
+    add_foreign_key :groups, :registers, name: :fk_groups_grid_feeding, column: :grid_feeding_register_id
 
     add_index :registers, [:meter_id, :obis], unique: true
     add_index :registers, [:meter_id, :direction], unique: true
