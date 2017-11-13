7c1890ecab2b9354bb178e9b3e407cc27a1c2b34
Phillip Oertel <me@phillipoertel.com>
2017-11-13 19:32:15 +0100

    Move all rspec before and after hooks to their own file.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,7 +16,6 @@ class ::Address
   end
 end
 
-
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
@@ -77,43 +76,6 @@ RSpec.configure do |config|
 
   def last_email
     ActionMailer::Base.deliveries.last
-  end
-
-  config.after(:context) do
-    Mongoid.purge!
-  end
-
-  config.before(:suite) do
-    puts "seeds: for specs"
-    load 'db/setup_data/specs.rb'
-  end
-
-  config.before(:context) do
-    first = true
-    ActiveRecord::Base.connection.tables.each do |table|
-      next if table.match(/\Aschema_migrations\Z/)
-      klass = table.singularize.camelize.safe_constantize
-      if klass
-        if klass.class.is_a?(Module)
-          klass = (klass.const_get 'Base' rescue nil)
-        end
-        if klass
-          if (klass == PersonsRole && klass.count != 4) ||
-             (klass == Role && klass.count != 4) ||
-             (klass == Account::Base && klass.count != 3) ||
-             (klass == Person && klass.count != 3) ||
-             (klass == Organization && klass.count != 7) ||
-             (klass != Organization && klass != PersonsRole && klass != Role && klass != Person && klass != Account::Base && klass.count > 0)
-            if first
-              first = false
-              warn '-' * 80
-              warn 'DB cleaning failed'
-            end
-            warn "#{klass}: #{klass.count}"
-          end
-        end
-      end
-    end
   end
 
   module ClassMethods
@@ -182,12 +144,6 @@ RSpec.configure do |config|
       super
     end
   end
-
-  config.append_after(:each) do |spec|
-    Redis.current.flushall
-    Rails.cache.clear
-  end
-
 
   # show retry status in spec process
   config.verbose_retry = true
---
+++ spec/support/rspec_hooks.rb
@@ -0,0 +1,52 @@
+RSpec.configure do |config|
+
+  #
+  # BEFORE
+  #
+
+  config.before(:suite) do
+    puts "seeds: for specs"
+    load 'db/setup_data/specs.rb'
+  end
+
+  config.before(:context) do
+    first = true
+    ActiveRecord::Base.connection.tables.each do |table|
+      next if table.match(/\Aschema_migrations\Z/)
+      klass = table.singularize.camelize.safe_constantize
+      if klass
+        if klass.class.is_a?(Module)
+          klass = (klass.const_get 'Base' rescue nil)
+        end
+        if klass
+          if (klass == PersonsRole && klass.count != 4) ||
+             (klass == Role && klass.count != 4) ||
+             (klass == Account::Base && klass.count != 3) ||
+             (klass == Person && klass.count != 3) ||
+             (klass == Organization && klass.count != 7) ||
+             (klass != Organization && klass != PersonsRole && klass != Role && klass != Person && klass != Account::Base && klass.count > 0)
+            if first
+              first = false
+              warn '-' * 80
+              warn 'DB cleaning failed'
+            end
+            warn "#{klass}: #{klass.count}"
+          end
+        end
+      end
+    end
+  end
+
+  #
+  # AFTER
+  #
+
+  config.after(:context) do
+    Mongoid.purge!
+  end
+
+  config.append_after(:each) do |spec|
+    Redis.current.flushall
+    Rails.cache.clear
+  end
+end
