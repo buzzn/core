7ab6b3b6a94ffcdf607a7f4b63855488997d456b
Christian Meier <meier.kristian@gmail.com>
2017-11-17 16:10:22 +0100

    move old invariant specs of group invariant to new place

--- spec/schemas/invariants/group_spec.rb
+++ spec/schemas/invariants/group_spec.rb
@@ -16,11 +16,12 @@ describe 'Schemas::Invariants::Group::Localpool' do
     it 'invalid' do
       localpool.update(distribution_system_operator: Organization.transmission_system_operator.first)
       expect(localpool.invariant.errors[:distribution_system_operator]).not_to be_nil
+      expect(localpool.distribution_system_operator).not_to be_nil
     end
 
     it 'valid' do
       localpool.update(distribution_system_operator: Organization.distribution_system_operator.first)
-      expect(localpool).to have_valid_invariants
+      expect(localpool.invariant.errors[:distribution_system_operator]).to be_nil
       expect(localpool.distribution_system_operator).not_to be_nil
     end
   end
@@ -32,11 +33,12 @@ describe 'Schemas::Invariants::Group::Localpool' do
     it 'invalid' do
       localpool.update(transmission_system_operator: Organization.distribution_system_operator.first)
       expect(localpool.invariant.errors[:transmission_system_operator]).not_to be_nil
+      expect(localpool.transmission_system_operator).not_to be_nil
     end
 
     it 'valid' do
       localpool.update(transmission_system_operator: Organization.transmission_system_operator.first)
-      expect(localpool).to have_valid_invariants
+      expect(localpool.invariant.errors[:transmission_system_operator]).to be_nil
       expect(localpool.transmission_system_operator).not_to be_nil
     end
   end
@@ -48,58 +50,66 @@ describe 'Schemas::Invariants::Group::Localpool' do
     it 'invalid' do
       localpool.update(electricity_supplier: Organization.distribution_system_operator.first)
       expect(localpool.invariant.errors[:electricity_supplier]).not_to be_nil
+      expect(localpool.electricity_supplier).not_to be_nil
     end
 
     it 'valid' do
       localpool.update(electricity_supplier: Organization.electricity_supplier.first)
-      expect(localpool).to have_valid_invariants
+      expect(localpool.invariant.errors[:electricity_supplier]).to be_nil
       expect(localpool.electricity_supplier).not_to be_nil
     end
   end
+
+  context 'grid_consumption_register' do
+    entity(:register) { Fabricate(:input_meter).input_register }
+    before { localpool.update(grid_consumption_register: register) }
+    context 'no meter.group' do
+      before { register.meter.group = nil }
+      context 'meter.register.group' do
+        before { register.group = localpool }
+        it 'is broken' do
+          expect(localpool.invariant.errors[:grid_consumption_register]).not_to be_nil
+        end
+      end
+      context 'no meter.register.group' do
+        before { register.group = nil }
+        it 'is broken' do
+          expect(localpool.invariant.errors[:grid_consumption_register]).not_to be_nil
+        end
+      end
+    end
+
+    context 'right meter.group' do
+      before { register.meter.group = localpool }
+      context 'no meter.register.group' do
+        before { register.group = nil }
+        it 'is valid' do
+          expect(localpool.invariant.errors[:grid_consumption_register]).to be_nil
+        end
+      end
+      context 'meter.register.group' do
+        before { register.group = localpool }
+        it 'is broken' do
+          expect(localpool.invariant.errors[:grid_consumption_register]).not_to be_nil
+        end
+      end
+    end
+
+    context 'wrong meter.group' do
+      entity(:other) { Fabricate(:localpool, owner: nil) }
+      before { register.meter.group = other }
+      context 'no meter.register.group' do
+        before { register.group = nil }
+        it 'is broken' do
+          expect(localpool.invariant.errors[:grid_consumption_register]).not_to be_nil
+        end
+      end
+      context 'meter.register.group' do
+        before { register.group = localpool }
+        it 'is broken' do
+          expect(localpool.invariant.errors[:grid_consumption_register]).not_to be_nil
+        end
+      end
+    end
+  end
 end
-
-  # context 'invariants' do
-
-  #   it 'valid' do
-  #     resources.each do |contract|
-  #       contract.object.update begin_date: nil, termination_date: nil, end_date: nil
-  #       expect(contract).to have_valid_invariants Schemas::Invariants::Contract::Base
-
-  #       contract.object.update begin_date: Date.today
-  #       expect(contract).to have_valid_invariants Schemas::Invariants::Contract::Base
-
-  #       contract.object.update termination_date: Date.today
-  #       expect(contract).to have_valid_invariants Schemas::Invariants::Contract::Base
-
-  #       contract.object.update end_date: Date.today
-  #       expect(contract).to have_valid_invariants Schemas::Invariants::Contract::Base
-  #     end
-  #   end
-
-  #   context 'invalid' do
-  #     it 'missing begin_date and termination_date' do
-  #       resources.each do |contract|
-  #         contract.object.update begin_date: nil, termination_date: nil, end_date: Date.today
-  #         expect(contract).not_to have_valid_invariants Schemas::Invariants::Contract::Base
-  #         expect(Schemas::Invariants::Contract::Base.call(contract).messages).to eq({:begin_date=>["must be filled"], :termination_date=>["must be filled"]})
-  #       end
-  #     end
-
-  #     it 'missing begin_date' do
-  #       resources.each do |contract|
-  #         contract.object.update begin_date: nil, termination_date: Date.today, end_date: Date.today
-  #         expect(contract).not_to have_valid_invariants Schemas::Invariants::Contract::Base
-  #         expect(Schemas::Invariants::Contract::Base.call(contract).messages).to eq({:begin_date=>["must be filled"]})
-  #       end
-  #     end
-
-  #     it 'missing termination date' do
-  #       resources.each do |contract|
-  #         contract.object.update begin_date: Date.today, termination_date: nil, end_date: Date.today
-  #         expect(contract).not_to have_valid_invariants Schemas::Invariants::Contract::Base
-  #         expect(Schemas::Invariants::Contract::Base.call(contract).messages).to eq({:termination_date=>["must be filled"]})
-  #       end
-  #     end
-  #   end
-  # end
-#end
