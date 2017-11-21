1225829e0389abff32689bf0823b8f897d54aa27
Phillip Oertel <me@phillipoertel.com>
2017-11-21 19:02:40 +0100

    Output debug info on exceptions

--- lib/beekeeper/import.rb
+++ lib/beekeeper/import.rb
@@ -17,7 +17,12 @@ class Beekeeper::Import
     Beekeeper::MinipoolObjekte.to_import.each do |record|
       # puts
       # puts record.converted_attributes.map { |k, v| "#{k}: #{v}" }.join("\n")
-      Group::Localpool.create!(record.converted_attributes)
+      begin
+        Group::Localpool.create!(record.converted_attributes)
+      rescue => e
+        ap e
+        ap record.converted_attributes
+      end
     end
   end
 
