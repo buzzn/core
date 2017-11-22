547c0462a77c85a368e6451530eebf8b7aa05620
Phillip Oertel <me@phillipoertel.com>
2017-11-22 12:43:02 +0100

    Disable MailView (removed the gem in PR :-/)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,11 +14,12 @@ Buzzn::Application.configure do
   config.action_controller.perform_caching = false
 
   # Mail
-  config.middleware.use MailView::Mapper, [MailPreview]
-  config.action_mailer.default_url_options    = { :host => 'localhost:3000' }
-  config.action_mailer.delivery_method        = :smtp
-  config.action_mailer.smtp_settings          = { :address => "localhost", :port => 1025 }
-  config.action_mailer.raise_delivery_errors  = false
+  # ... is currently not configured, see https://goo.gl/sYxHeh for details.
+  # config.middleware.use MailView::Mapper, [MailPreview]
+  # config.action_mailer.default_url_options    = { :host => 'localhost:3000' }
+  # config.action_mailer.delivery_method        = :smtp
+  # config.action_mailer.smtp_settings          = { :address => "localhost", :port => 1025 }
+  # config.action_mailer.raise_delivery_errors  = false
   #config.action_mailer.perform_deliveries     = true
 
   # Print deprecation notices to the Rails logger.
