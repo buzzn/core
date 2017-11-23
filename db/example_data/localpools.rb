3323ee4a89b27ce9351041563024b5a4467ef6f5
Phillip Oertel <me@phillipoertel.com>
2017-11-23 10:44:59 +0100

    Add a localpool that has an organizsation as owner, and fix the factory to assign the org contact person as localpool owner (role-wise).

--- db/example_data/localpools.rb
+++ db/example_data/localpools.rb
@@ -9,5 +9,15 @@ SampleData.localpools = OpenStruct.new(
       { name: "Hausstrom - Standard" },
       { name: "Hausstrom - Reduziert", energyprice_cents_per_kwh: 24.9 },
     ]
+  ),
+  green_warriors: create(:localpool, name: "Green warriors",
+    owner: FactoryGirl.create(:organization, :contracting_party),
+    distribution_system_operator: Organization.distribution_system_operator.last,
+    transmission_system_operator: Organization.transmission_system_operator.last,
+    electricity_supplier: Organization.electricity_supplier.last,
+    tariffs_attrs: [
+      { name: "Hausstrom - Standard" },
+      { name: "Hausstrom - Reduziert", energyprice_cents_per_kwh: 24.9 },
+    ]
   )
 )
