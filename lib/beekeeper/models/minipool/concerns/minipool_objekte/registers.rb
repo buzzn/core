96412e59799cc308b2ddaff1df253986ba8f8052
Phillip Oertel <me@phillipoertel.com>
2017-11-29 22:47:54 +0100

    Preliminary way to exclude virtual registers/meters from import. Not ideal since they still show up in the warnings.

--- lib/beekeeper/models/minipool/concerns/minipool_objekte/registers.rb
+++ lib/beekeeper/models/minipool/concerns/minipool_objekte/registers.rb
@@ -12,9 +12,15 @@ class Beekeeper::Minipool::MinipoolObjekte < Beekeeper::Minipool::BaseRecord
         attrs = zählwerk.converted_attributes
         add_warning("register '#{attrs[:name]}'", zählwerk.warnings) if zählwerk.warnings.present?
         add_warning("meter '#{attrs[:name]}'", zählwerk.msb_gerät.warnings) if zählwerk.msb_gerät.warnings.present?
-        register_class = attrs[:type].constantize
-        register_class.new(attrs.except(:type))
-      end
+        # this is not the right place to control that,
+        # but I didn't want to add that method to our register
+        if zählwerk.skip_import?
+          nil
+        else
+          register_class = attrs[:type].constantize
+          register_class.new(attrs.except(:type))
+        end
+      end.compact # remove nil values
     end
   end
 end
