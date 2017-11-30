2d163c5c162a7802f7ca6a71ed63719c6f833a34
Phillip Oertel <me@phillipoertel.com>
2017-11-30 21:12:18 +0100

    Output some debug info

--- lib/beekeeper/models/minipool/concerns/minipool_objekte/registers.rb
+++ lib/beekeeper/models/minipool/concerns/minipool_objekte/registers.rb
@@ -8,19 +8,17 @@ class Beekeeper::Minipool::MinipoolObjekte < Beekeeper::Minipool::BaseRecord
     private
 
     def registers
-      msb_zählwerk_daten.map do |zählwerk|
+      msb_zählwerk_daten.reject(&:skip_import?).map do |zählwerk|
         attrs = zählwerk.converted_attributes
         add_warning("register '#{attrs[:name]}'", zählwerk.warnings) if zählwerk.warnings.present?
         add_warning("meter '#{attrs[:name]}'", zählwerk.msb_gerät.warnings) if zählwerk.msb_gerät.warnings.present?
-        # this is not the right place to control that,
-        # but I didn't want to add that method to our register
-        if zählwerk.skip_import?
-          nil
-        else
-          register_class = attrs[:type].constantize
-          register_class.new(attrs.except(:type))
-        end
-      end.compact # remove nil values
+        register_class = attrs[:type].constantize
+        register = register_class.new(attrs.except(:type))
+        # debug = "#{zählwerk.buzznid}: #{register.label} (#{register.name})"
+        # debug << " MPID #{register.metering_point_id}" if register.metering_point_id
+        # puts debug
+        register
+      end
     end
   end
 end
