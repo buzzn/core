f09b287c9443740376f7771b44b2d829478e8442
Christian Meier <meier.kristian@gmail.com>
2017-12-05 08:15:02 +0100

    add binsing.pry check on source-code test

--- spec/source_file_spec.rb
+++ spec/source_file_spec.rb
@@ -1,37 +1,5 @@
 describe "Source File" do
 
-  Dir[File.expand_path('../../../../../app/controllers/api/v1/*.rb', __FILE__)].each do |file|
-
-    if !file.end_with?('base.rb') && !file.end_with?('defaults.rb')
-      it "#{file.sub(/.*api/, 'api')} only permitted_params are allowed" do
-        content = File.read(file)
-        content.each_line do |line|
-          if line =~ /params/
-            expect(line).to match /(params do)|(permitted_params)/
-          end
-        end
-      end
-    end
-
-    if !file.end_with?('users.rb')
-      it "#{file.sub(/.*api/, 'api')} only guarded/unguarded retrieve/delete" do
-        content = File.read(file)
-        content.each_line do |line|
-          expect(line).not_to match /find[^_]/
-          if line =~ /delete/
-            expect(line).to match /(delete.*do)|(guarded_delete)|(deleted_response)/
-          end
-        end
-      end
-    end
-    it "#{file.sub(/.*api/, 'api')} only guarded create/update" do
-      content = File.read(file)
-      content.each_line do |line|
-        expect(line).not_to match /(cre|upd)ate\!/
-      end
-    end
-  end
-
   (Dir['lib/**/*rb'] + Dir['app/**/*rb']).each do |file|
     it "source code files do not use Time.now in #{file}" do
       next if file == 'lib/buzzn/utils/chronos.rb'
@@ -48,4 +16,13 @@ describe "Source File" do
       end
     end
   end
+
+   (Dir['lib/**/*rb'] + Dir['app/**/*rb'] + Dir['app/**/*rb']).each do |file|
+     it "source code files do not use binding.pry in #{file}" do
+       content = File.read(file)
+       content.each_line do |line|
+         expect(line).not_to match /binding.pry/
+       end
+     end
+   end
 end
