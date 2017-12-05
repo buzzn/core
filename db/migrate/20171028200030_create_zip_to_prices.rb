f9de9b55acbef647c6ade4a18550cbe0dd2de2df
Christian Meier <meier.kristian@gmail.com>
2017-12-05 10:43:22 +0100

    fix field length on zip-to-prices table

--- db/migrate/20171028200030_create_zip_to_prices.rb
+++ db/migrate/20171028200030_create_zip_to_prices.rb
@@ -12,9 +12,9 @@ class CreateZipToPrices < ActiveRecord::Migration
       t.float   :measurement_euro_year_et, null: false
       t.float   :ka, null: false
       t.string  :state, null: false, limit: 32
-      t.string  :community, null: false, limit: 32
+      t.string  :community, null: false, limit: 64
       t.integer :vdewid, null: false, limit: 8
-      t.string  :dso, limit: 64, null: false
+      t.string  :dso, limit: 32, null: false
       t.boolean :updated, null: false
     end
     add_index :zip_to_prices, :zip
