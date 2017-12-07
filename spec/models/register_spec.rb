9bab4361a6a714da327dc5104978f5a89462ab5b
Phillip Oertel <me@phillipoertel.com>
2017-12-07 22:59:53 +0100

    Add tests for correct, hard-coded obis.

--- spec/models/register_spec.rb
+++ spec/models/register_spec.rb
@@ -128,4 +128,19 @@ describe Register do
     expect{register.store_reading_at(Time.current, Reading::Continuous::REGULAR_READING)}.to raise_error ArgumentError
     expect(Reading::Continuous.all.by_register_id(register.id).at(Time.current.beginning_of_day).size).to eq 1
   end
+
+  describe "obis" do
+    context "when register is base" do
+      it { expect(Register::Input.new.obis).to be_nil }
+    end
+    context "when register is real" do
+      it { expect { Register::Real.new.obis }.to raise_error(RuntimeError, 'not implemented') }
+    end
+    context "when register is input" do
+      it { expect(Register::Input.new.obis).to eq("1-0:1.8.0") }
+    end
+    context "when register is output" do
+      it { expect(Register::Output.new.obis).to eq("1-0:2.8.0") }
+    end
+  end
 end
