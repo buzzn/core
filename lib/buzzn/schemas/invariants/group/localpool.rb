5997d929fcfd4014ec970c019c215c232755b00e
Phillip Oertel <me@phillipoertel.com>
2017-12-08 10:16:58 +0100

    Fix grid feeding register validation with Christian.

--- lib/buzzn/schemas/invariants/group/localpool.rb
+++ lib/buzzn/schemas/invariants/group/localpool.rb
@@ -35,28 +35,16 @@ module Schemas
             end
           end
 
-          def at_most_one_grid_feeding_register?(input)
-            registers = ::Group::Base.find(input).registers
-            registers.grid_feeding.size <= 1
+          def at_most_one_register_with_same_label?(register)
+            register.meter.group.registers.where(label: register.attributes['label']).count <= 1
           end
-
-          # def at_most_one_grid_consumption_register?(input)
-          #   registers = ::Group::Base.find(input).registers
-          #   registers.grid_consumption.size <= 1
-          # end
         end
 
         required(:distribution_system_operator).maybe { distribution_system_operator? }
         required(:transmission_system_operator).maybe { transmission_system_operator? }
         required(:electricity_supplier).maybe { electricity_supplier? }
         required(:owner).maybe { has_owner_role? }
-        # I don't know how to define a rule on the whole object where I can execute regular active record code, and still pass in the :id
-        required(:id).maybe { at_most_one_grid_feeding_register? }
-        # required(:id).maybe { at_most_one_grid_consumption_register? }
-        # rule(at_most_one_grid_feeding_register: [:id]) do |id|
-        #   at_most_one_grid_feeding_register?(id)
-        # end
-        # required(:registers).maybe { at_most_one_grid_feeding_register? }
+        required(:grid_feeding_register).maybe { at_most_one_register_with_same_label? }
       end
     end
   end
