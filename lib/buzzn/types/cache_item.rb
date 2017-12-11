58f06709004dc67a6d59101ab0c0cf0dc95abe0c
Christian Meier <meier.kristian@gmail.com>
2017-12-11 07:52:19 +0100

    add generic response caching

---
+++ lib/buzzn/types/cache_item.rb
@@ -0,0 +1,21 @@
+require_relative '../types'
+
+class Types::CacheItem
+  extend Dry::Initializer
+
+  option :json, Types::Strict::String
+  option :digest, Types::Strict::String
+  option :time_to_live, Types::Strict::Int
+
+  attr_reader :expires_at
+
+  def initialize(**kwargs)
+    if kwargs[:json] && kwargs[:digest].nil?
+      kwargs[:digest] = Digest::SHA256.base64digest(kwargs[:json])
+    end
+    super(kwargs)
+    @expires_at = Time.at(Buzzn::Utils::Chronos.now.to_f + time_to_live)
+  end
+
+  def to_json(*); json; end
+end
---
+++ spec/services/cache_spec.rb
@@ -0,0 +1,29 @@
+require 'buzzn/services/cache'
+
+describe Services::Cache do
+
+  let(:subject) { Import.global('service.cache') }
+
+  it 'puts json and returns cache-item' do
+    item = subject.put('key', '{}', 10)
+    expect(item.to_json).to eq '{}'
+    expect(item.time_to_live).to eq 10
+    item = subject.get('key')
+    expect(item.to_json).to eq '{}'
+    expect(item.time_to_live).to eq 10
+  end
+
+  it 'expires after time-to-live' do
+    item = subject.put('key', '{}', 2)
+    expect(item).not_to be_nil
+    # can not use Timecop as Redis does not know about it
+    sleep 1
+    item = subject.get('key')
+    expect(item).not_to be_nil
+    expect(item.time_to_live).to eq 1
+    sleep 1
+    item = subject.get('key')
+    expect(item).to be_nil
+  end
+
+end
