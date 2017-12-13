9d83ae78706cb00cfc5fc12165aef526234ec68f
Phillip Oertel <me@phillipoertel.com>
2017-12-13 18:30:45 +0100

    Remove obsolete files from the config directory.

--- config/schedule.rb
+++
@@ -1,7 +0,0 @@
-env :PATH, '/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/aws/bin:/opt/aws/bin'
-
-set :output, 'log/cron.log'
-
-every 1.day, :at => '4:00 am' do
-  runner "Group::Base.calculate_scores"
-end
