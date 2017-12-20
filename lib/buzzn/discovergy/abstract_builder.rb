520896d67cb5d47a1df3ecd996f2becf4b6878e2
Christian Meier <meier.kristian@gmail.com>
2017-12-20 12:19:47 +0100

    make comment on unit comprehendable

--- lib/buzzn/discovergy/abstract_builder.rb
+++ lib/buzzn/discovergy/abstract_builder.rb
@@ -8,7 +8,7 @@ class Discovergy::AbstractBuilder
   protected
 
   def to_watt(response)
-    # POWER(X) is the (phase) power in 10^-3 W
+    # power in 10^-3 W
     response['values']['power'] / 1000
   end
 end
