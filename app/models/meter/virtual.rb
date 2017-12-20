cb88467e4fb09fcb0da292261339a09788e502a2
Christian Meier <meier.kristian@gmail.com>
2017-12-20 12:10:28 +0100

    remove old obsolete validations

--- app/models/meter/virtual.rb
+++ app/models/meter/virtual.rb
@@ -2,33 +2,11 @@ module Meter
   class Virtual < Base
 
     has_one :register, class_name: Register::Virtual, dependent: :destroy, foreign_key: :meter_id
-    validates_associated :register
-
-    validates :register, presence: true
-    validates :product_name, presence: false
-    validates :product_serialnumber, presence: false, uniqueness: true, allow_nil: true, length: { in: 2..128 }
-
-
-    def validate_invariants
-      errors.add(:manufacturer_name, 'not allowed') unless manufacturer_name.nil?
-      errors.add(:direction_number, 'not allowed') unless direction_number.nil?
-    end
 
     def initialize(attr = {})
       attr[:register] = Register::Virtual.new(attr[:register] || {}) if attr && attr[:register].is_a?(Hash)
       super
       register.meter = self if register
-    end
-
-    # work around AR short-comings
-
-    def valid?(*args)
-      if ! super && !errors[:register].empty? && register
-        register.errors.each do |key, err|
-          errors.add(:"register.#{key}", err)
-        end
-      end
-      errors.empty?
     end
   end
 end
