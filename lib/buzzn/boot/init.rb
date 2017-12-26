bf0590dfc1cfa53b6c415b1eaeca23c7e762c7ff
Christian Meier <meier.kristian@gmail.com>
2017-12-26 16:33:14 +0100

    switch to dry-dependency-injection gem

--- lib/buzzn/boot/init.rb
+++ lib/buzzn/boot/init.rb
@@ -4,7 +4,7 @@ require_relative 'active_record'
 require_relative 'main_container'
 require_relative '../services'
 require 'dry/auto_inject'
-require 'dry/more/container/singleton'
+require 'dry-dependency-injection'
 
 # core extensions
 require_relative '../core/number'
@@ -20,6 +20,9 @@ module Buzzn
 
   module Boot
     class Init
+      class Singletons
+        include Dry::DependencyInjection::Singletons
+      end
 
       class << self
 
@@ -27,10 +30,10 @@ module Buzzn
           @logger = Logger.new(self)
 
           # preload singletons
-          services = Dry::More::Container::Singleton.new
-          services.config.lazy = false
-          operations = Dry::More::Container::Singleton.new
-          operations.config.lazy = false
+          singletons = Singletons.new
+          singletons.config.lazy = false
+          importer = Dry::DependencyInjection::Importer.new(singletons)
+
           Application.config.paths['lib'].dup.tap do |app|
             app.glob = "buzzn/services/**/*.rb"
             app.to_a.each do |path|
@@ -41,7 +44,7 @@ module Buzzn
               if clazz # TODO remove if
                 if clazz.is_a?(Class)
                   name = main.gsub('/', '.')
-                  services.register("#{name.sub(/services/, 'service')}", clazz)
+                  singletons.register("#{name.sub(/services/, 'service')}", clazz)
                 end
               else
                 # TODO remove old namespace handling
@@ -49,23 +52,14 @@ module Buzzn
                 name = File.basename(path).sub(/\.rb/,'')
                 cname = name.split('_').collect {|n| n.capitalize }.join
                 clazz = Buzzn::Services.const_get(cname)
-                services.register("service.#{name}", clazz) if clazz.is_a?(Class)
+                singletons.register("service.#{name}", clazz) if clazz.is_a?(Class)
               end
             end
-            app.glob = "buzzn/operations/**/*.rb"
-            app.to_a.each do |path|
-              require path
-              main = path.gsub(/^.*buzzn\/|.rb$/, '')
-              clazz = main.camelize.safe_constantize
-              name = main.gsub('/', '.')
-              if clazz.is_a?(Class)
-                operations.register("#{name}", clazz)
-              end
-            end
+
           end
-          MainContainer.merge(services).merge(operations)
-          services.finalize
-          operations.finalize
+          importer.import('lib/buzzn', 'operations')
+          MainContainer.merge(singletons)
+          singletons.finalize
 
           # eager require some files
           %w(resource resources roda permissions schemas).each do |dir|
