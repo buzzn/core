da053c89e2950de0792c3f3fc3f59f94722751f8
Phillip Oertel <me@phillipoertel.com>
2018-01-10 16:19:45 +0100

    Improve debug output.

--- script/beekeeper_import_check_consumption_common.rb
+++ script/beekeeper_import_check_consumption_common.rb
@@ -5,12 +5,24 @@ def inspect_register(register)
   "#{register.name} (id: #{register.id})"
 end
 
-Group::Base.where("start_date <= ?", Date.today).order(:name, :start_date).each do |group|
+num_registers            = 0
+groups_without_registers = []
+
+relevant_groups = Group::Base.where("start_date <= ?", Date.today).where.not("name LIKE 'Localpool%'").order(:name, :start_date)
+
+relevant_groups.each do |group|
 
   puts
   puts "#{group.name} (start: #{group.start_date})"
   puts "-" * 80
 
-registers = group.registers.consumption_common
+  registers = group.registers.consumption_common
+  num_registers += registers.size
+  groups_without_registers << group if registers.empty?
+
   registers.each { |r| puts inspect_register(r) }
 end
+
+puts
+puts "Total of #{num_registers} common registers."
+puts "#{groups_without_registers.size} of #{relevant_groups.size} groups without common registers."
