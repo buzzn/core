deb87a99287262e1eee8c2fa5eb5b995dc1620b0
Phillip Oertel <me@phillipoertel.com>
2018-01-16 13:53:20 +0100

    Move the caching of the created meters by buzznid to the meter registry

--- lib/beekeeper/meter_registry.rb
+++ lib/beekeeper/meter_registry.rb
@@ -6,16 +6,22 @@ module Beekeeper
     class << self
       def set(buzznid, meter_instance)
         @meters ||= {}
+        verify!(buzznid)
         raise "Not a meter instance: #{meter_instance}" unless meter_instance.is_a?(Meter::Base)
         raise "Meter for #{buzznid} already set, not overwriting: #{@meters[buzznid].attributes}" if @meters[buzznid]
         @meters[buzznid] = meter_instance
       end
 
       def get(buzznid)
+        verify!(buzznid)
         @meters ||= {}
         meter = @meters[buzznid]
-        puts "No meter for #{buzznid}" unless meter
+        # puts "No meter for #{buzznid}" unless meter
         meter
+      end
+
+      def verify!(buzznid)
+        raise "buzznid unreliable!!" if buzznid !~ /^([0-9])+\/([0-9])+$/
       end
     end
   end
