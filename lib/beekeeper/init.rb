38af7ddd926f9a8b70fcfceba77008a32174412b
Phillip Oertel <me@phillipoertel.com>
2018-01-17 14:22:29 +0100

    Move all includes to the init file; define new Beekeeper::Import module and load code from directory for import classes (to be added).

--- lib/beekeeper/init.rb
+++ lib/beekeeper/init.rb
@@ -6,4 +6,11 @@ Dir.glob('lib/beekeeper/models/{minipool,buzzn}/*.rb').each do |file|
   require file
 end
 
+# wire up invariant with AR and raise error on invalid in before_save
+require 'buzzn/schemas/support/enable_dry_validation'
+require 'buzzn/types/discovergy'
+require 'beekeeper/meter_registry'
+
+Dir.glob('lib/beekeeper/importer/*').each { |importer| require importer }
+
 require_relative 'import'
