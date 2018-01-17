5b574bfdce86470841a412de94226e0065c8a039
Phillip Oertel <me@phillipoertel.com>
2018-01-17 11:32:23 +0100

    Allow skipping banks import with env variable

--- db/setup_data/application.rb
+++ db/setup_data/application.rb
@@ -1,5 +1,5 @@
 Buzzn::Logger.root.info "seeds: loading application setup data"
-if Bank.count == 0
+if Bank.count == 0 && !ENV['SKIP_BANK_SETUP']
   file = Dir["db/banks/*"].sort.last
   Buzzn::Logger.root.info "seeds: loading bank data from #{file}"
   Bank.update_from_file(file)
