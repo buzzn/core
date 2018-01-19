7efc640cee79b62c7bb6d94839545730f1537ae7
Phillip Oertel <me@phillipoertel.com>
2018-01-19 11:32:54 +0100

    Create and deduplicate organizations attached to contracts (WIP; needs to be refactored).

--- lib/beekeeper/importer/powertaker_contracts.rb
+++ lib/beekeeper/importer/powertaker_contracts.rb
@@ -4,17 +4,18 @@ class Beekeeper::Importer::PowerTakerContracts
 
   def initialize(logger)
     @logger = logger
+    logger.level = Logger::INFO
   end
 
   def run(localpool, powertaker_contracts, registers)
     powertaker_contracts.each do |contract|
       begin
         ActiveRecord::Base.transaction do
-          customer = find_or_create_powertaker(contract[:powertaker])
+          customer = find_or_create_customer(contract[:powertaker])
           create_contract(localpool, customer, contract, registers)
         end
-      rescue => e
-        logger.error("#{e} (meter buzznid: #{contract[:buzznid]})")
+      # rescue => e
+      #   logger.error("#{e} (meter buzznid: #{contract[:buzznid]})")
       end
     end
   end
@@ -37,10 +38,21 @@ class Beekeeper::Importer::PowerTakerContracts
     Contract::LocalpoolPowerTaker.create!(contract_attributes)
   end
 
-  # Make sure we don't create the same person twice.
-  def find_or_create_powertaker(unsaved_person)
+  # As a temporary solution to importing the actual virtual registers (separate story), we create a fake, empty one.
+  def create_fake_virtual_register(buzznid)
+    logger.error("No meter/register for #{buzznid}, creating a fake temporary one.")
+    meter = Meter::Real.create!(product_serialnumber: 'FAKE-FOR-IMPORT', legacy_buzznid: buzznid)
+    Register::Input.create!(name: "FAKE-FOR-IMPORT", share_with_group: false, meter: meter)
+  end
+
+  # Make sure we don't create the same person or organization twice.
+  def find_or_create_customer(unsaved_record)
+    unsaved_record.is_a?(Person) ? find_or_create_person(unsaved_record) : find_or_create_organization(unsaved_record)
+  end
+
+  def find_or_create_person(unsaved_record)
     # Unfortunately some persons can have the same email address in Beekeeper, so we need to add first and last name.
-    uniqueness_attrs = unsaved_person.attributes.slice("email", "first_name", "last_name")
+    uniqueness_attrs = unsaved_record.attributes.slice("email", "first_name", "last_name")
     person = Person.find_by(uniqueness_attrs)
     if person
       logger.debug "#{unsaved_person.name} (#{unsaved_person.email}): using existing person #{person.id}"
@@ -66,4 +78,83 @@ class Beekeeper::Importer::PowerTakerContracts
     meter = Meter::Real.create!(product_serialnumber: 'FAKE-FOR-IMPORT', legacy_buzznid: buzznid)
     Register::Input.create!(name: "FAKE-FOR-IMPORT", share_with_group: false, meter: meter)
   end
+
+  # Deduplication of the beekeeper data
+  def find_or_create_organization(unsaved_record)
+    puts unsaved_record.name
+    org_is_buzzn       = unsaved_record.name =~ /buzzn/
+    account_new_fibunr = ORGANIZATION_DATA_LOOKUPS[:account_new].find { |pattern, fibunr| unsaved_record.name =~ pattern }&.at(1)
+    kontaktdaten_id    = ORGANIZATION_DATA_LOOKUPS[:kontaktdaten].find { |pattern, fibunr| unsaved_record.name =~ pattern }&.at(1)
+    org_with_same_slug = Organization.find_by(slug: Buzzn::Slug.new(unsaved_record.name))
+
+    if org_is_buzzn
+      puts "Organization is buzzn."
+      Organization.buzzn
+    elsif account_new_fibunr
+      puts "Taking organization from account_new"
+      # get the data to use for our record
+      source_record = Beekeeper::Buzzn::AccountNew.find_by(fibunr: account_new_fibunr)
+      # check if we already created it
+      slug = Buzzn::Slug.new(source_record.converted_attributes[:name])
+      org = Organization.find_by(slug: slug)
+      if org
+        # if we do, return that record
+        org
+      else
+        # if not, create it and return it
+        Organization.create!(source_record.converted_attributes)
+      end
+    elsif kontaktdaten_id
+      puts "Taking organization from kontaktdaten"
+      # get the data to use for our record
+      source_record = Beekeeper::Minipool::Kontaktdaten.find_by(kontaktdaten_id: kontaktdaten_id)
+      # check if we already created it
+      slug = Buzzn::Slug.new(source_record.converted_attributes[:name])
+      org = Organization.find_by(slug: slug)
+      if org
+        # if we do, return that record
+        org
+      else
+        # if not, create it and return it
+        Organization.create!(source_record.converted_attributes)
+      end
+    elsif org_with_same_slug
+      puts "Using existing org with same slug"
+      org_with_same_slug
+    else # lookup is configured for this record, import data as is, no deduplication
+      puts "Creating new organization"
+      unsaved_record.save!
+      unsaved_record
+    end
+  end
+
+  # Only placed at the end because the regexp patterns as hash keys mess up my syntax highlighting ...
+
+  ORGANIZATION_DATA_LOOKUPS = {
+
+    # key: pattern for kontaktdaten.firma
+    # value: account_new.fibunr of the record that should be used for data
+    account_new: {
+      /wagnis/i => 70069,
+      /Cohaus/ => 80792,
+      /IGEWO/  => 80583
+    },
+
+    # key: pattern for kontaktdaten.firma
+    # value: kontaktdaten.kontaktdaten_id of the record that should be used for data
+    kontaktdaten: {
+      /Parkgelände GmbH/                         => 624,
+      /BEG Remstal/                              => 775,
+      /Diakonie Stetten/                         => 790,
+      /Gemeinde Hallbergmoos/                    => 777,
+      /IFAGE Grundstücksverwaltungs GmbH/        => 442,
+      /Ritter Baukontor GmbH/                    => 1097,
+      /Umbreit, Olé Madrid/                      => 46,
+      /VR Dachau Immobilien GmbH/                => 894,
+      /WEG Hausverwaltung Alte Rommelshauser St/ => 1070,
+      /WHG Wohnungsbau/                          => 430,
+      /Wogeno München eG/                        => 998,
+    }
+  }
+
 end
