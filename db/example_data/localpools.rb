b93f29fe0d13ed399c82a35e66dce775d06531f9
Phillip Oertel <me@phillipoertel.com>
2018-01-19 14:55:14 +0100

    Remove hell & warm from our example data since we now import it from beekeeper and would have a duplicate otherwise.

--- db/example_data/localpools.rb
+++ db/example_data/localpools.rb
@@ -1,10 +1,11 @@
 SampleData.localpools = OpenStruct.new(
   people_power: create(:localpool,
     name: "People Power Group (Testgruppe)",
+    slug: 'people-power-group-testgruppe', # hard-code the slug because we share the link publicly, it shouldn't change
     description: "Power to the people!",
     show_display_app: true,
     owner: SampleData.persons.group_owner,
-    admins: [ SampleData.persons.brumbauer],
+    admins: [ SampleData.persons.group_admin ],
     distribution_system_operator: Organization.distribution_system_operator.first,
     transmission_system_operator: Organization.transmission_system_operator.first,
     electricity_supplier: Organization.electricity_supplier.first,
--- lib/beekeeper/importer/log_import_summary.rb
+++ lib/beekeeper/importer/log_import_summary.rb
@@ -24,9 +24,9 @@ class Beekeeper::Importer::LogImportSummary
     logger.info("group orga owner with bank-accounts  : #{Group::Localpool.where('owner_organization_id IS NOT NULL').select {|g| !g.owner.bank_accounts.empty? }.count}")
     logger.info("group orga contacts                  : #{Group::Localpool.where('owner_organization_id IS NOT NULL').select {|g| g.owner.contact_id }.count}")
     logger.info("group orga contact addresses         : #{Organization.where(id: Group::Localpool.where('owner_organization_id IS NOT NULL').select(:owner_organization_id)).joins(:contact).where('persons.address_id IS NOT NULL').count}")
-    logger.info("localpool powertaker contracts       : #{Contract::LocalpoolPowerTaker.count}")
     logger.info("registers                            : #{Register::Real.count}")
     logger.info("readings                             : #{Reading::Single.count}")
     logger.info("meters                               : #{Meter::Real.count}")
+    logger.info("localpool powertaker contracts       : #{Contract::LocalpoolPowerTaker.count}")
   end
 end
