ce9a4f9f366838d3a7fb740f94da02c462d1f077
Christian Meier <meier.kristian@gmail.com>
2018-01-23 11:51:00 +0100

    cleanup rack-timeout

--- lib/buzzn/roda/core_roda.rb
+++ lib/buzzn/roda/core_roda.rb
@@ -15,8 +15,7 @@ class CoreRoda < CommonRoda
   use Rack::CommonLogger, logger
 
 #  use Rack::Timeout, service_timeout: 29
-
-  Rack::Timeout::Logger.disable
+#  Rack::Timeout::Logger.disable
 
   use Rack::Cors, debug: logger.debug? do
     allow do
