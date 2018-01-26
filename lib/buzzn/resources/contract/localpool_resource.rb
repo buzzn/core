f7571458c3dc6809d833b54e7221794e13db902d
Christian Meier <meier.kristian@gmail.com>
2018-01-26 10:04:39 +0100

    add localpool to contract-resource

---
+++ lib/buzzn/resources/contract/localpool_resource.rb
@@ -0,0 +1,9 @@
+require_relative 'base_resource'
+require_relative '../group_resource'
+
+module Contract
+  class LocalpoolResource < BaseResource
+
+    has_one :localpool, GroupResource
+  end
+end
