f4a518256b261d474591c56bceb13381727a0d59
Christian Meier <meier.kristian@gmail.com>
2018-02-01 07:48:25 +0100

    add min-size constraint on group.name

--- lib/buzzn/schemas/constraints/group.rb
+++ lib/buzzn/schemas/constraints/group.rb
@@ -1,7 +1,7 @@
 require_relative '../constraints'
 
 Schemas::Constraints::Group = Schemas::Support.Form do
-  required(:name).filled(:str?, max_size?: 64)
+  required(:name).filled(:str?, max_size?: 64, min_size?: 4)
   optional(:description).filled(:str?, max_size?: 256)
   optional(:start_date).filled(:date?)
   optional(:show_object).filled(:bool?)
