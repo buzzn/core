694a71e455640431ec36c8ea06aa1ecef6b984ed
Phillip Oertel <me@phillipoertel.com>
2018-02-01 17:32:00 +0100

    Add contract#begin_reading and #end_reading to simplify things and easily write a validation script.

--- app/models/contract/localpool_power_taker.rb
+++ app/models/contract/localpool_power_taker.rb
@@ -5,5 +5,12 @@ module Contract
 
     belongs_to :register, class_name: 'Register::Input'
 
+    def begin_reading
+      register.readings.find_by(date: begin_date)
+    end
+
+    def end_reading
+      register.readings.find_by(date: end_date)
+    end
   end
 end
