2426f5718091a53cb7bffa74234d9c4b38f54a10
Phillip Oertel <me@phillipoertel.com>
2018-02-06 19:22:06 +0100

    Add new script to check for all missing readings

--- app/models/contract/localpool_power_taker.rb
+++ app/models/contract/localpool_power_taker.rb
@@ -12,13 +12,5 @@ module Contract
     def end_reading
       register.readings.find_by(date: end_date)
     end
-
-    def begin_reading
-      register.readings.find_by(date: begin_date)
-    end
-
-    def end_reading
-      register.readings.find_by(date: end_date)
-    end
   end
 end
