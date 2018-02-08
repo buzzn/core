890163c4e7b2a5241a51a39f98337af3af2b7f24
Phillip Oertel <me@phillipoertel.com>
2018-02-08 19:25:23 +0000

    Fix duplicate method

--- app/models/contract/base.rb
+++ app/models/contract/base.rb
@@ -98,12 +98,4 @@ module Contract
 
     # permissions helpers
     scope :permitted, ->(uuids) { where(id: uuids) }
-
-    # It order to be continuous, a contract's end_date is the same as the start_date of the following contract.
-    # This is technically correct but unexpected by humans. That's why we have the last_date, which will show
-    # the human-expected last date of the contract.
-    def last_date
-      end_date && (end_date - 1.day)
-    end
-  end
 end
