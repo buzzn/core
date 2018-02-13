7dc98ac06e3e9c64c94e013e29c5e42374565ae1
Christian Meier <meier.kristian@gmail.com>
2018-02-13 14:19:35 +0100

    make size? in constraints work on migration and swagger

--- lib/buzzn/schemas/constraints/market_location.rb
+++ lib/buzzn/schemas/constraints/market_location.rb
@@ -3,6 +3,5 @@ require_relative '../constraints'
 Schemas::Constraints::MarketLocation = Schemas::Support.Form do
   required(:name).filled(:str?, max_size?: 64)
   required(:group_id).filled(:int?, gteq?: 1)
-  # TODO: use size?: 11 here. If I do that though, the DB column limit isn't set to 11.
-  optional(:market_location_id).filled(:str?, min_size?: 11, max_size?: 11)
+  optional(:market_location_id).filled(:str?, size?: 11)
 end
