d1540b2bdfbbfb565ae44690550644fba7e397b3
Christian Meier <meier.kristian@gmail.com>
2018-02-20 10:32:59 +0100

    fix the label contraints to be required on the db
    add optional on the PATCH request

--- lib/buzzn/schemas/constraints/register/common.rb
+++ lib/buzzn/schemas/constraints/register/common.rb
@@ -1,7 +1,7 @@
 require_relative '../register'
 
 Schemas::Constraints::Register::Common = Schemas::Support.Form do
-  required(:label).value(included_in?: Register::Base.labels.values)
+  optional(:label).value(included_in?: Register::Base.labels.values)
   optional(:metering_point_id).filled(:str?, max_size?: 64)
   optional(:observer_enabled).filled(:bool?)
   optional(:observer_min_threshold).filled(:int?, gteq?: 0)
