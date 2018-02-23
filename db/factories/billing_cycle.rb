411156bd3b78728720b41b85573295368c90b923
Christian Meier <meier.kristian@gmail.com>
2018-02-23 12:21:34 +0100

    simplify billing_cycle factory

--- db/factories/billing_cycle.rb
+++ db/factories/billing_cycle.rb
@@ -1,11 +1,8 @@
 FactoryGirl.define do
-  factory :billing_cycle, class: 'BillingCycle' do
+  factory :billing_cycle do
     name                      { generate(:billing_cycle_name) }
     begin_date                Date.parse('2000-01-01')
     end_date                  Date.parse('2001-01-01')
-
-    after(:build) do |cycle|
-      cycle.localpool = FactoryGirl.build(:localpool) unless cycle.localpool
-    end
+    localpool                 { FactoryGirl.build(:localpool) }
   end
 end
