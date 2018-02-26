113225e0ce8a9986674f9ad1cf56e3f457790132
Christian Meier <meier.kristian@gmail.com>
2018-02-26 08:35:11 +0100

    add substitute meter for example_data

--- db/example_data/brokers.rb
+++ db/example_data/brokers.rb
@@ -27,11 +27,12 @@ SampleData.brokers = OpenStruct.new(
   pt5a: broker(external_id: 'EASYMETER_60327613', resource: SampleData.contracts.pt5a.market_location.register.meter),
   pt6:  broker(external_id: 'EASYMETER_60327614', resource: SampleData.contracts.pt6.market_location.register.meter),
   # not sure what meter to use here instead but this one already exists (ecar below)
-  #pt7a: broker(external_id: 'EASYMETER_60327605', resource: SampleData.contracts.pt7a.market_location.register.meter),
+  pt7a: broker(external_id: 'EASYMETER_60327605', resource: SampleData.contracts.pt7a.market_location.register.meter),
   pt8:  broker(external_id: 'EASYMETER_60327606', resource: SampleData.contracts.pt8.market_location.register.meter),
   pt9:  broker(external_id: 'EASYMETER_60327607', resource: SampleData.contracts.pt9.market_location.register.meter),
-  pt10: broker(external_id: 'EASYMETER_60327608', resource: SampleData.contracts.pt10.market_location.register.meter),
-  ecar: broker(external_id: 'EASYMETER_60327605', resource: SampleData.registers.ecar.meter),
+  # using substitute here
+  # pt10: broker(external_id: 'EASYMETER_60327608', resource: SampleData.contracts.pt10.market_location.register.meter),
+  ecar: broker(external_id: 'EASYMETER_60327608', resource: SampleData.registers.ecar.meter),
 
   pv:   broker(external_id: 'EASYMETER_60327687', resource: SampleData.registers.pv.meter),
   grid: broker(external_id: 'EASYMETER_60300855', mode: :in_out, resource: SampleData.meters.grid)
--- db/example_data/registers.rb
+++ db/example_data/registers.rb
@@ -24,26 +24,5 @@ SampleData.registers = OpenStruct.new(
   water: create(:register, :production_water,
     meter: build(:meter, :real, :one_way, group: SampleData.localpools.people_power),
     market_location: create_market_location('Produktion Wasserkraft')
-               ),
-  # This virtual register sums up the consumption of all powertakers supplied by buzzn.
-  # That's why the formula parts subtract powertaker 6's register, since he is supplied by a third party.
-
-  #
-  # FIXME creating the following registers fails due to a DB constraint error:
-  # PG::NotNullViolation: ERROR:  null value in column "meter_id" violates not-null constraint
-  # I wasn't able to debug it or understand why this doesn't happen on the real meters above.
-  # Maybe we won't be using the virtual registers like this any more soon, so I don't debug the creation any further.
-  #
-  # grid_consumption_corrected: create(:register, :virtual_input,
-  #   meter: build(:meter, :virtual, group: SampleData.localpools.people_power, registers: []),
-  #   label: Register::Base.labels[:grid_consumption_corrected],
-  #   formula_parts: [
-  #     build(:formula_part, :plus,  operand: SampleData.meters.grid.registers.input.first, register: nil),
-  #     build(:formula_part, :minus, operand: SampleData.contracts.pt6.register, register: nil)
-  #   ]
-  # ),
-  # grid_feeding_corrected: create(:register, :virtual_output,
-  #   meter: build(:meter, :virtual, group: SampleData.localpools.people_power),
-  #   label: Register::Base.labels[:grid_feeding_corrected],
-  # )
+               )
 )
