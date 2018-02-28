5916147556a695668eff2c22e982fbd0a6035750
Phillip Oertel <me@phillipoertel.com>
2018-02-28 11:39:43 +0100

    Use :closed to assert the status has actually been set; :open is the default

--- spec/models/billing_brick_spec.rb
+++ spec/models/billing_brick_spec.rb
@@ -1,7 +1,7 @@
 describe 'BillingBrick' do
 
   it 'can be initialized with values' do
-    attrs = { status: :open, type: :gap, begin_date: Date.new(2018, 1, 1), end_date: Date.new(2018, 12, 31), market_location: 'A market location' }
+    attrs = { status: :closed, type: :gap, begin_date: Date.new(2018, 1, 1), end_date: Date.new(2018, 12, 31), market_location: 'A market location' }
     brick = BillingBrick.new(attrs)
     attrs.each { |key, value| expect(brick.send(key)).to equal(value) }
   end
