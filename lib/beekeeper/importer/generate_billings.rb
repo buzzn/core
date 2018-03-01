92a0627818c5653c493c9bc2837d6859ba0c7555
Phillip Oertel <me@phillipoertel.com>
2018-03-01 13:07:27 +0100

    Add invoice number to billing, use brickbuilder for brick

--- lib/beekeeper/importer/generate_billings.rb
+++ lib/beekeeper/importer/generate_billings.rb
@@ -6,17 +6,19 @@ class Beekeeper::Importer::GenerateBillings
 
   attr_reader :logger
 
+  MAX_DATE_RANGE = Date.new(2000, 1, 1)..Date.new(2020, 1, 1)
   def initialize(logger)
     @logger = logger
     logger.level = Import.global('config.log_level')
   end
 
   def run(localpool)
-    localpool.market_locations.consumption.each do |ml|
+    localpool.market_locations.consumption.each.with_index(1) do |ml, index|
       ended_contracts = ml.contracts.to_a.select { |c| c.status.ended? }
       ended_contracts.each do |contract|
         attrs = {
           status: :closed,
+          invoice_number: "BK-IMPORT-LP#{localpool.id}-#{index}",
           begin_date: contract.begin_date,
           end_date: contract.end_date,
           total_energy_consumption_kwh: 0,
@@ -24,14 +26,17 @@ class Beekeeper::Importer::GenerateBillings
           prepayments_cents: 0,
           receivables_cents: 0,
           localpool_power_taker_contract_id: contract.id,
-          bricks: [BillingBrick.new(begin_date: contract.begin_date,
-                                    end_date: contract.end_date)]
+          bricks: [brick_for_contract(contract)]
         }
         if Billing.create!(attrs)
           logger.info("Created billing for ended contract #{contract.id} (#{contract.begin_date}..#{contract.end_date})")
         end
       end
     end
+  end
+
+  def brick_for_contract(contract)
+    Billing::BrickBuilder.from_contract(contract, MAX_DATE_RANGE)
   end
 
 end
