6ae80f7235d60e8e85fefb58a0762b9294a5a792
Phillip Oertel <me@phillipoertel.com>
2018-03-01 12:25:32 +0100

    Complete BillingBrick active record model.

--- app/models/billing_brick.rb
+++ app/models/billing_brick.rb
@@ -1,9 +1,6 @@
 #
 # A billing brick stores a part of the energy consumption within a billing.
 #
-# NOTICE: it's a simple value object for now, but will likely become an ActiveRecord model
-# to be persisted along with the completed billings. Not using dry-struct here because of that.
-#
 class BillingBrick < ActiveRecord::Base
 
   self.inheritance_column = '_type' # we use type as a regular attribute, not for STI
@@ -15,21 +12,6 @@ class BillingBrick < ActiveRecord::Base
 
   enum status: %i(open closed).each_with_object({}).each {|k, map| map[k] = k.to_s }
   enum type: %i(power_taker third_party gap).each_with_object({}).each {|k, map| map[k] = k.to_s }
-
-  # Minimal table
-  # [x] - begin_date
-  # [x] - end_date
-  # [x] - status (with enum)
-  # [x] - type (with enum)
-  # [x] - create PG enum
-  # Associations
-  # [x] - billing_brick --> billing
-  # [x] - billing --> billing_brick
-  # Implement date range
-  # [x] set with range
-  # [x] set with dates
-  # -----
-  # [ ] set status from billing in brickbuilder
 
   def date_range=(new_range)
     self[:begin_date] = new_range.first
