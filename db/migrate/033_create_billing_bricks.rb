51c7a06a9e3e6e90ede7f3798b1948ede31436f2
Phillip Oertel <me@phillipoertel.com>
2018-03-01 16:20:46 +0100

    Fix & enable foreign keys

--- db/migrate/033_create_billing_bricks.rb
+++ db/migrate/033_create_billing_bricks.rb
@@ -8,11 +8,11 @@ class CreateBillingBricks < ActiveRecord::Migration
     SCHEMA.up(:billing_bricks, self)
     change_column_default :billing_bricks, :status, :open
     add_belongs_to :billing_bricks, :billing, references: :billing, index: true, null: false
-    # add_foreign_key :billing_bricks, :billing, name: :fk_billing_bricks_billing, column: :billing_id
+    add_foreign_key :billing_bricks, :billings, name: :fk_billing_bricks_billing, column: :billing_id
   end
 
   def down
-    # remove_foreign_key :billing_bricks, :groups, name: :fk_billing_bricks_localpool, column: :localpool_id
+    remove_foreign_key :billing_bricks, :billings, name: :fk_billing_bricks_localpool, column: :localpool_id
     remove_belongs_to :billing_bricks, :billing, references: :billing, index: true, null: false
     SCHEMA.down(:billing_bricks, self)
   end
