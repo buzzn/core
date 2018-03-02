55457993695ac1a4dc4c2aa68baa10975b65c464
Phillip Oertel <me@phillipoertel.com>
2018-03-02 12:02:41 +0100

    PT2

--- spec/models/market_location_spec.rb
+++ spec/models/market_location_spec.rb
@@ -26,83 +26,20 @@ describe MarketLocation do
     end
   end
 
-  describe 'contracts_in_date_range' do
-    let(:billing_date_range) { Date.new(2018, 1, 1)..Date.new(2019, 1, 1) }
-    let(:contract)           { create(:contract, :localpool_powertaker, begin_date: contract_date_range.first, end_date: contract_date_range.last) }
-    let(:market_location)    { create(:market_location, contracts: [contract]) }
-    subject                  { market_location.contracts_in_date_range(billing_date_range) }
-
-    context 'when market location has one contract' do
-      #
-      # begin before billing date range
-      #
-      context 'when contract begins and ends before billing range' do
-        let(:contract_date_range) { Date.new(2017, 5, 1)..Date.new(2017, 5, 31) }
-        it { is_expected.to eq([]) }
-      end
-
-      context 'when contract begins before and ends on billing range start' do
-        let(:contract_date_range) { Date.new(2017, 5, 1)..billing_date_range.first }
-        it { is_expected.to eq([]) }
-      end
-
-      context 'when contract begins before and ends in billing range' do
-        let(:contract_date_range) { Date.new(2017, 5, 1)..Date.new(2018, 5, 31) }
-        it { is_expected.to eq([contract]) }
-      end
-
-      context 'when contract begins before and ends after billing range' do
-        let(:contract_date_range) { Date.new(2017, 5, 1)..Date.new(2019, 5, 31) }
-        it { is_expected.to eq([contract]) }
-      end
-
-      #
-      # begin in billing date range
-      #
-      context 'when contract begins in and ends in billing range' do
-        let(:contract_date_range) { Date.new(2018, 5, 1)..Date.new(2018, 5, 31) }
-        it { is_expected.to eq([contract]) }
-      end
-
-      context 'when contract begins in and ends after billing range' do
-        let(:contract_date_range) { Date.new(2018, 5, 1)..Date.new(2019, 5, 31) }
-        it { is_expected.to eq([contract]) }
-      end
-
-      #
-      # begin on billing date range end
-      #
-      context 'when contract begins on billing range end' do
-        let(:contract_date_range) { billing_date_range.last..Date.new(2019, 5, 31) }
-        it { is_expected.to eq([]) }
-      end
-
-      #
-      # begin after billing date range
-      #
-      context 'when contract begins after billing range' do
-        let(:contract_date_range) { Date.new(2019, 5, 1)..Date.new(2019, 5, 31) }
-        it { is_expected.to eq([]) }
-      end
-
+  describe 'consumption?' do
+    let(:market_location) { build(:market_location, register: register_trait) }
+    subject               { market_location.consumption? }
+    context 'when register has label consumption' do
+      let(:register_trait) { :consumption }
+      it { is_expected.to eq(true) }
     end
-
-    describe 'consumption?' do
-      let(:market_location) { build(:market_location, register: register_trait) }
-      subject               { market_location.consumption? }
-      context 'when register has label consumption' do
-        let(:register_trait) { :consumption }
-        it { is_expected.to eq(true) }
-      end
-      context 'when register has label consumption_common' do
-        let(:register_trait) { :consumption_common }
-        it { is_expected.to eq(true) }
-      end
-      context 'when register has label production_water' do
-        let(:register_trait) { :production_water }
-        it { is_expected.to eq(false) }
-      end
+    context 'when register has label consumption_common' do
+      let(:register_trait) { :consumption_common }
+      it { is_expected.to eq(true) }
     end
-
+    context 'when register has label production_water' do
+      let(:register_trait) { :production_water }
+      it { is_expected.to eq(false) }
+    end
   end
 end
