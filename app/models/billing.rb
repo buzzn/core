3572254f01be9c226bdbd267bd411eefcd735684
Phillip Oertel <me@phillipoertel.com>
2018-03-02 12:00:21 +0100

    PT2

--- app/models/billing.rb
+++ app/models/billing.rb
@@ -10,4 +10,15 @@ class Billing < ActiveRecord::Base
   belongs_to :contract, -> { where(type: %w(Contract::LocalpoolPowerTaker Contract::LocalpoolGap Contract::LocalpoolThirdParty)) }, class_name: 'Contract::Base'
 
   has_many :bricks, class_name: 'BillingBrick'
+
+  # TODO: DRY this up when Christian has created the module for date ranges
+  def date_range=(new_range)
+    self.begin_date = new_range.first
+    self.end_date   = new_range.last
+  end
+
+  def date_range
+    begin_date..end_date
+  end
+
 end
