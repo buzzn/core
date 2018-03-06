3699b5245c0921a2cf7708e6a7ed4656e6a0d6f8
Phillip Oertel <me@phillipoertel.com>
2018-03-06 19:45:31 +0100

    Extract reason enums, they may be helpful later in other contexts.

---
+++ app/models/concerns/reading_enums.rb
@@ -0,0 +1,54 @@
+module ReadingEnums
+
+  extend ActiveSupport::Concern
+
+  # reason constants
+  DEVICE_SETUP            = 'device_setup'
+  DEVICE_CHANGE_1         = 'device_change_1'
+  DEVICE_CHANGE_2         = 'device_change_2'
+  DEVICE_REMOVAL          = 'device_removal'
+  REGULAR_READING         = 'regular_reading'         # "Turnusablesung"
+  MIDWAY_READING          = 'midway_reading'          # "Zwischenablesung"
+  CONTRACT_CHANGE         = 'contract_change'
+  DEVICE_PARAMETER_CHANGE = 'device_parameter_change'
+  BALANCING_ZONE_CHANGE   = 'balancing_zone_change'
+  OTHER                   = 'other'                   # also used four source
+
+  # quality constants
+  NOT_USABLE                 = 'not_usable'
+  SUBSTITUE_VALUE            = 'substitue_value'
+  ENERGY_QUANTITY_SUMMARIZED = 'energy_quantity_summarized'
+  FORECAST_VALUE             = 'forecast_value'
+  READ_OUT                   = 'read_out'                   # abgelesen
+  PROPOSED_VALUE             = 'proposed_value'
+
+  # source constants
+  BUZZN               = 'buzzn_systems'
+  CUSTOMER_LSG        = 'customer_lsg'        # lsg = localpool strom geber
+  LSN                 = 'lsn'                 # lsn = localpool strom nehmer
+  VNB                 = 'vnb'                 # vnb = verteilnetzbetreiber
+  THIRD_PARTY_MSB_MDL = 'third_party_msb_mdl' # msb = messstellenbetreiber, mdl = messdienstleister
+  USER_INPUT          = 'user_input'
+  SLP                 = 'slp'
+  SEP_PV              = 'sep_pv'
+  SEP_BHKW            = 'sep_bhkw'
+
+  class_methods do
+
+    def reasons
+      [DEVICE_SETUP, DEVICE_CHANGE_1, DEVICE_CHANGE_2, DEVICE_REMOVAL, REGULAR_READING,
+        MIDWAY_READING, CONTRACT_CHANGE, DEVICE_PARAMETER_CHANGE, BALANCING_ZONE_CHANGE, OTHER]
+    end
+
+    def qualities
+      [NOT_USABLE, SUBSTITUE_VALUE, ENERGY_QUANTITY_SUMMARIZED, FORECAST_VALUE, READ_OUT,
+        PROPOSED_VALUE]
+    end
+
+    def sources
+      [BUZZN, CUSTOMER_LSG, LSN, VNB, THIRD_PARTY_MSB_MDL, OTHER, USER_INPUT, SLP, SEP_PV, SEP_BHKW]
+    end
+
+  end
+
+end
