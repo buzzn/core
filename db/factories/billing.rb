0c2505783b9e9e65fd312c936550290fb66d5945
Phillip Oertel <me@phillipoertel.com>
2018-03-06 18:40:57 +0100

    Remove currently unneeded billing attributes.
    We'll probably only store this info on the billing bricks and sum
    them up on the fly to prevent duplication.

--- db/factories/billing.rb
+++ db/factories/billing.rb
@@ -3,10 +3,6 @@ FactoryGirl.define do
     status                       :open
     begin_date                   { Date.new(2017, 1, 1) }
     end_date                     { Date.new(2017, 12, 31) }
-    total_energy_consumption_kwh 2_500
-    total_price_cents            { total_energy_consumption_kwh * 25.5 }
-    prepayments_cents            { total_price_cents * 0.80 }
-    receivables_cents            { total_price_cents * 0.20 }
     invoice_number               { generate(:billing_invoice_number) }
   end
 end
--- lib/buzzn/schemas/constraints/billing.rb
+++ lib/buzzn/schemas/constraints/billing.rb
@@ -2,11 +2,6 @@ require_relative '../constraints'
 
 Schemas::Constraints::Billing = Schemas::Support.Form do
   required(:status).value(included_in?: Billing.status.values)
-  required(:total_energy_consumption_kwh).filled(:int?, gteq?: 0)
-  required(:total_price_cents).filled(:int?, gteq?: 0)
-  required(:prepayments_cents).filled(:int?, gteq?: 0)
-  #may be negative if LSG has to pay back money
-  required(:receivables_cents).filled(:int?)
   required(:begin_date).filled(:date?)
   required(:end_date).filled(:date?)
   optional(:invoice_number).filled(:str?, max_size?: 64)
