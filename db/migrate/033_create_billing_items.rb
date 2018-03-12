d37140c4a4375b8b934e04561a51428b53fd5d29
Phillip Oertel <me@phillipoertel.com>
2018-03-12 18:58:31 +0100

    Document why we can't enable the null checks yet

--- db/migrate/033_create_billing_items.rb
+++ db/migrate/033_create_billing_items.rb
@@ -9,12 +9,15 @@ class CreateBillingItems < ActiveRecord::Migration
     add_belongs_to :billing_items, :billing, references: :billing, index: true, null: false
     add_foreign_key :billing_items, :billings, name: :fk_billing_items_billing, column: :billing_id
 
+    # TODO: enable the null: false checks for begin and end reading once we can fetch readings from Discovergy
+    # to fill up the missing ones.
     add_belongs_to :billing_items, :begin_reading, references: :reading, index: true #, null: false
     # add_foreign_key :billing_items, :readings, name: :fk_billing_items_begin_reading, column: :begin_reading_id
-
     add_belongs_to :billing_items, :end_reading, references: :reading, index: true #, null: false
     # add_foreign_key :billing_items, :readings, name: :fk_billing_items_end_reading, column: :end_reading_id
 
+    # TODO: enable the null: false check once we don't create billing items for 3rd party contracts any more.
+    # They are the reason this can't be enabled, because they don't have a tariff.
     add_belongs_to :billing_items, :tariff, references: :tariff, index: true #, null: false
     # add_foreign_key :billing_items, :tariffs, name: :fk_billing_items_tariff, column: :tariff_id
   end
