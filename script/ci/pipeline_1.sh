10b30c8977a04aae197f730f1398aadd65a76a25
Phillip Oertel <me@phillipoertel.com>
2018-03-13 12:20:33 +0100

    Remove script/ci as these files aren't used (it's just confusing).

--- script/ci/pipeline_1.sh
+++
@@ -1,8 +0,0 @@
-# Run the following commands in the CI shell process with:
-# source script/ci/pipeline_1.sh
-
-export RAILS_ENV=development
-bundle exec rake db:drop db:create db:migrate db:structure:dump
-bash -c 'if [ -z "$(git status --porcelain db)" ]; then exit 0; else git diff db| cat -; exit 1; fi'
-bundle exec rake db:seed:example_data
-bundle exec rake banks:import
