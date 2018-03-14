6174c066a9792303841ea48439eba2988f746b6c
Christian Meier <meier.kristian@gmail.com>
2018-03-14 17:03:08 +0100

    fix tariff immutable and specs

--- spec/models/contract/tariff_spec.rb
+++ spec/models/contract/tariff_spec.rb
@@ -3,17 +3,16 @@ describe 'Contract::Tariff' do
   describe 'readonly?' do
     context 'when tariff is new record' do
       let(:tariff) { build(:tariff) }
-      it "is false" do
-        expect(tariff).not_to be_readonly
+      it 'is false' do
         expect { tariff.save! }.not_to raise_error
       end
     end
 
     context 'when tariff is saved record' do
       let(:tariff) { create(:tariff) }
-      it "is true" do
-        expect(tariff).to be_readonly
-        expect { tariff.save! }.to raise_error(ActiveRecord::ReadOnlyRecord)
+      before { tariff.begin_date = Date.today }
+      it 'is true' do
+        expect { tariff.save! }.to raise_error(ActiveRecord::RecordNotSaved)
       end
     end
   end
