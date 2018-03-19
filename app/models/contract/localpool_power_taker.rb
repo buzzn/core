cb34670ca27c79060434de1d3e3bd55ed330baa0
Christian Meier <meier.kristian@gmail.com>
2018-03-19 15:56:27 +0100

    remove end_reading/begin_reading from localpool_powertaker_contract

--- app/models/contract/localpool_power_taker.rb
+++ app/models/contract/localpool_power_taker.rb
@@ -5,13 +5,5 @@ module Contract
 
     belongs_to :register, class_name: 'Register::Input'
 
-    def begin_reading
-      market_location.register.readings.find_by(date: begin_date)
-    end
-
-    def end_reading
-      market_location.register.readings.find_by(date: end_date)
-    end
-
   end
 end
