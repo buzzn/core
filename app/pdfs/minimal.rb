d7165284a1f24d0efcbb3dea325071ca168ae581
Christian Meier <meier.kristian@gmail.com>
2018-03-19 10:17:19 +0100

    fix finding slim file for commandline rendering

--- app/pdfs/minimal.rb
+++ app/pdfs/minimal.rb
@@ -3,8 +3,8 @@ require 'buzzn/pdf_generator'
 module Buzzn::Pdfs
   class Minimal < Buzzn::PdfGenerator
 
-    TEMPLATE = if Import.global?('config.slim.file')
-                 Import.global('config.slim.file')
+    TEMPLATE = if Import.global?('config.slim_file')
+                 Import.global('config.slim_file').sub('app/pdfs/', '')
                else
                  'minimal.slim'
                end
