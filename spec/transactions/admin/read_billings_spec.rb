9a99378b64b5a040adebec4466b1b88385f485e6
Christian Meier <meier.kristian@gmail.com>
2018-03-22 11:07:23 +0100

    Revert "Skipping tests" and fix it
    This reverts commit 0fcc8e25b2d1225d2ef59a6029e8018efcbd7f76.

--- spec/transactions/admin/read_billings_spec.rb
+++ spec/transactions/admin/read_billings_spec.rb
@@ -10,49 +10,48 @@ describe Transactions::Admin::BillingCycle::ReadBillings do
   entity(:billing_cycle_resource) { Admin::LocalpoolResource.all(account).first.billing_cycles.first }
   entity(:transaction) { Transactions::Admin::BillingCycle::ReadBillings.for(billing_cycle_resource) }
 
-  # skip: giving up, I can't get this test to run reliably.
-  context 'result', :skip do
+  context 'result' do
     subject { transaction.call(billing_cycle_resource) }
 
     context 'without market_location' do
       it { is_expected.to be_success }
       it { expect(subject.value[:array]).to be_empty }
-    end
 
-    context 'with market_location' do
-      let!(:market_location) { create(:market_location, :consumption, group: localpool) }
-      let(:first_location)   { subject.value[:array].first }
+      context 'with market_location' do
+        let!(:market_location) { create(:market_location, :consumption, group: localpool) }
+        let(:last_location) { subject.value[:array].last }
 
-      context 'without billing' do
-        it { is_expected.to be_success }
-        it { expect(first_location.keys).to eq(%i(id type name bars)) }
-      end
+        context 'without billing' do
+          it { is_expected.to be_success }
+          it { expect(last_location.keys).to eq(%i(id type name bars)) }
 
-      context 'with billing' do
+          context 'with billing' do
 
-        let!(:contract)     { create(:contract, :localpool_powertaker, market_location: market_location) }
-        let!(:billing)      { create(:billing, contract: contract, billing_cycle: billing_cycle) }
-        let!(:billing_item) { create(:billing_item, billing: billing, contract_type: :power_taker) }
+            let!(:contract)     { create(:contract, :localpool_powertaker, market_location: market_location) }
+            let!(:billing)      { create(:billing, contract: contract, billing_cycle: billing_cycle) }
+            let!(:billing_item) { create(:billing_item, billing: billing, contract_type: :power_taker) }
 
-        let(:expected_keys) do
-          %w(billing_id contract_type begin_date end_date status consumed_energy_kwh price_cents errors)
-        end
-        it { is_expected.to be_success }
-        it { expect(first_location.keys).to eq(%i(id type name bars)) }
+            let(:expected_keys) do
+              %w(billing_id contract_type begin_date end_date status consumed_energy_kwh price_cents errors)
+            end
+            it { is_expected.to be_success }
+            it { expect(last_location.keys).to eq(%i(id type name bars)) }
 
-        it { expect(first_location[:bars][:array].first.keys).to eq(expected_keys) }
+            it { expect(last_location[:bars][:array].first.keys).to eq(expected_keys) }
 
-        context 'with errors' do
-          it 'JSON has all errors and messages' do
-            errors = first_location[:bars][:array].first['errors']
-            expect(errors).to eq(
-              'begin_reading' => ['begin_reading must be filled'],
-              'end_reading' => ['end_reading must be filled'],
-              'tariff' => ['tariff must be filled']
-            )
+            context 'with errors' do
+              it 'JSON has all errors and messages' do
+                errors = last_location[:bars][:array].first['errors']
+                expect(errors).to eq(
+                  'begin_reading' => ['begin_reading must be filled'],
+                  'end_reading' => ['end_reading must be filled'],
+                  'tariff' => ['tariff must be filled']
+                )
+              end
+            end
+
           end
         end
-
       end
     end
   end
