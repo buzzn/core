4405851f2c23e8ecb2f31d0b156ce2f2219daa37
Christian Meier <meier.kristian@gmail.com>
2018-05-15 18:24:39 +0200

    adds invariants on meter regarind datasource/manufacture_name/measurement_method

--- lib/buzzn/schemas/invariants/meter/base.rb
+++ lib/buzzn/schemas/invariants/meter/base.rb
@@ -14,6 +14,25 @@ module Schemas
 
         required(:group).maybe
         required(:registers).filled
+        required(:manufacturer_name).filled
+        required(:edifact_measurement_method).filled
+        required(:datasource).filled
+
+        rule(:manufacturer_name) do
+          value(:datasource).eql?('discovergy').then(value(:manufacturer_name).eql?('easy_meter'))
+        end
+
+        rule(:edifact_measurement_method) do
+          value(:datasource)
+            .eql?('discovergy').then(value(:edifact_measurement_method).eql?('AMR'))
+            .and(value(:datasource).eql?('standard_profile').then(value(:edifact_measurement_method).eql?('MMR')))
+        end
+
+        rule(:datasource) do
+          value(:edifact_measurement_method)
+            .eql?('AMR').then(value(:datasource).eql?('discovergy'))
+            .and(value(:edifact_measurement_method).eql?('MMR').then(value(:datasource).eql?('standard_profile')))
+        end
 
         rule(group: [:group, :registers]) do |group, registers|
           group.filled?.then(registers.match_group?(group))
--- spec/schemas/invariants/meter_base_spec.rb
+++ spec/schemas/invariants/meter_base_spec.rb
@@ -2,9 +2,77 @@ require 'buzzn/schemas/invariants/register/substitute'
 
 describe 'Schemas::Invariants::Meter::Base' do
 
-  entity(:localpool) { create(:localpool) }
+  context 'discovergy' do
+    entity(:meter) { create(:meter, :real, group: nil) }
+    subject { meter.invariant.errors[:datasource] }
+    before { meter.discovergy! }
+    it do
+      meter.easy_meter!
+      meter.remote!
+      is_expected.to be_nil
+    end
+    it do
+      meter.manual!
+      is_expected.to eq(['must be equal to standard_profile'])
+    end
+    context 'edifact_measurement_method' do
+      subject { meter.invariant.errors[:edifact_measurement_method] }
+      it do
+        meter.manual!
+        is_expected.to eq(['must be equal to AMR'])
+      end
+      it do
+        meter.remote!
+        is_expected.to be_nil
+      end
+    end
+    context 'manufacturer_name' do
+      subject { meter.invariant.errors[:manufacturer_name] }
+      it do
+        meter.other!
+        is_expected.to eq(['must be equal to easy_meter'])
+      end
+      it do
+        meter.easy_meter!
+        is_expected.to be_nil
+      end
+    end
+  end
+  context 'standard_profile' do
+    entity(:meter) { create(:meter, :real, group: nil) }
+    subject { meter.invariant.errors[:datasource] }
+    before { meter.standard_profile! }
+    it do
+      meter.easy_meter!
+      is_expected.to be_nil
+      meter.other!
+      is_expected.to be_nil
+    end
+    context 'edifact_measurement_method' do
+      subject { meter.invariant.errors[:edifact_measurement_method] }
+      it do
+        meter.remote!
+        is_expected.to eq(['must be equal to MMR'])
+      end
+      it do
+        meter.manual!
+        is_expected.to be_nil
+      end
+    end
+    context 'manufacturer_name' do
+      subject { meter.invariant.errors[:manufacturer_name] }
+      it do
+        meter.easy_meter!
+        is_expected.to be_nil
+        meter.other!
+        is_expected.to be_nil
+      end
+    end
+  end
 
   subject { meter.invariant.errors[:group] }
+
+  entity(:localpool) { create(:localpool) }
 
   context 'no meter group' do
     entity(:meter) { create(:meter, :real, group: nil) }
