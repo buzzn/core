35264ec2370e44feebea1d0beb2812c0accbbeaa
Christian Meier <meier.kristian@gmail.com>
2018-05-17 12:55:42 +0200

    remove obsolete charts code

--- lib/buzzn/operations/group_chart.rb
+++
@@ -1,9 +0,0 @@
-require_relative 'chart'
-
-class Operations::GroupChart < Operations::Chart
-
-  def call(input, group)
-    Success(charts.for_group(group, interval(input)))
-  end
-
-end
--- lib/buzzn/operations/register_chart.rb
+++
@@ -1,9 +0,0 @@
-require_relative 'chart'
-
-class Operations::RegisterChart < Operations::Chart
-
-  def call(input, register)
-    Success(charts.for_register(register, interval(input)))
-  end
-
-end
