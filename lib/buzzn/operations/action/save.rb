f058a50ac451b94ea956f4041abfb2cc140595a1
Christian Meier <meier.kristian@gmail.com>
2018-05-17 09:12:00 +0200

    split update operation into assign/save to allow injecting in between

---
+++ lib/buzzn/operations/action/save.rb
@@ -0,0 +1,21 @@
+require_relative '../action'
+
+class Operations::Action::Save
+
+  include Dry::Transaction::Operation
+
+  def call(resource)
+    persist(resource.object)
+    Success(resource)
+  end
+
+  private
+
+  def persist(object)
+    if object.changed?
+      object.touch
+      object.save!
+    end
+  end
+
+end
--- lib/buzzn/operations/action/update.rb
+++ lib/buzzn/operations/action/update.rb
@@ -4,46 +4,11 @@ class Operations::Action::Update
 
   include Dry::Transaction::Operation
 
+  include Import['operations.action.save', 'operations.action.assign']
+
   def call(input, resource = nil)
-    raise ArgumentError.new('missing resource') unless resource
-
-    # we deliver only millis to client and have to nil the nanos
-    if (resource.object.updated_at.to_f * 1000).to_i != (input.delete(:updated_at).to_f * 1000).to_i
-      # TODO better a Left Monad and handle on roda
-      raise Buzzn::StaleEntity.new(resource.object)
-    else
-      update(input, resource)
-    end
-  end
-
-  def update(input, resource)
-    resource.object.class.transaction do
-      resource.object.attributes = input
-      result = check_invariant(resource.object)
-      if result.success?
-        persist(resource.object)
-        Success(resource)
-      else
-        resource.object.reload
-        Failure(result)
-      end
-    end
-  end
-
-  def persist(object)
-    if object.changed?
-      object.touch
-      object.save!
-    end
-  end
-
-  ALWAYS_SUCCESS = OpenStruct.new(success?: true)
-
-  def check_invariant(object)
-    if invariant = object.invariant
-      invariant
-    else
-      ALWAYS_SUCCESS
+    assign.call(input, resource).bind do |assigned_resource|
+      save.call(assigned_resource)
     end
   end
 
