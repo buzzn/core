02f0f67f9ca7ebc651dd79ddd032c622082815a3
Christian Meier <meier.kristian@gmail.com>
2018-05-17 17:27:01 +0200

    rubocop

--- lib/buzzn/transactions/step_adapters.rb
+++ lib/buzzn/transactions/step_adapters.rb
@@ -1,20 +1,18 @@
 class Transactions::StepAdapters < Dry::Transaction::StepAdapters
+
   extend Dry::Monads::Result::Mixin
 
-  register :validate, -> operation, options, args {
+  register :validate, ->(operation, options, args) {
     schema = operation.()
     unless schema.is_a? Dry::Validation::Schema
       raise ArgumentError.new("step +#{options[:step_name]}+ needs operation which returns a Dry::Validation::Schema instance")
     end
     kwargs = args[0]
-#    unless kwargs[:params]
-#      raise ArgumentError.new("step +#{options[:step_name]}+ requires input provided via +params:+")
-    #    end
     # FIXME help with transition to more intiutive transaction setup
     params = kwargs[:params] || kwargs
     result = schema.call(params)
     if result.success?
-     # FIXME help with transition to more intiutive transaction setup
+      # FIXME help with transition to more intiutive transaction setup
       if kwargs.key?(:params)
         kwargs[:params] = result.output
         Success(kwargs)
