cc52abe84cdc2b19b9fd73330fcb188f507360da
Christian Meier <meier.kristian@gmail.com>
2018-06-04 12:44:58 +0200

    fix startup require

--- lib/buzzn/schemas/transactions/admin/register/update_virtual.rb
+++ lib/buzzn/schemas/transactions/admin/register/update_virtual.rb
@@ -1,4 +1,5 @@
 require_relative '../register'
+require_relative '../../../../schemas/constraints/register/common'
 
 Schemas::Transactions::Admin::Register::UpdateVirtual = Schemas::Support.Form(Schemas::Constraints::Register::Common) do
   optional(:direction).value(included_in?: Register::Base.directions.values)
