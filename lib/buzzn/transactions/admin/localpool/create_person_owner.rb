567039cea3dd0220d7999da37b9b2acbe747d5d4
Christian Meier <meier.kristian@gmail.com>
2018-06-05 11:00:22 +0200

    remove obosolte code

--- lib/buzzn/transactions/admin/localpool/create_person_owner.rb
+++ lib/buzzn/transactions/admin/localpool/create_person_owner.rb
@@ -3,14 +3,6 @@ require_relative 'owner_base'
 module Transactions::Admin::Localpool
   class CreatePersonOwner < OwnerBase
 
-    def self.for(localpool)
-      new.with_step_args(
-        authorize: [localpool, localpool.permissions.owner.create],
-        create: [localpool],
-        assign_owner: [localpool]
-      )
-    end
-
     validate :schema
     authorize :allowed_roles
     around :db_transaction
