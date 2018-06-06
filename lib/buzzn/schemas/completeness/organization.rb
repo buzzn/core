938c9b211c21921e3b2934d8695c9ab4fbf9a488
Christian Meier <meier.kristian@gmail.com>
2018-06-06 10:14:18 +0200

    Revert "skip nested incompleteness errors for the time being"
    This reverts commit 6c423215d29dd03ea8f95c0e519639e775323cff.

--- lib/buzzn/schemas/completeness/organization.rb
+++ lib/buzzn/schemas/completeness/organization.rb
@@ -3,6 +3,11 @@ require_relative '../completeness'
 module Schemas::Completeness
 
   Organization = Schemas::Support.Schema do
+    required(:legal_representation).filled
+    required(:contact).filled
+    # this might be to general here as not all Organizations need to have an Address only Organizations
+    # as Owner of Localpool do
+    required(:address).filled
   end
 
 end
