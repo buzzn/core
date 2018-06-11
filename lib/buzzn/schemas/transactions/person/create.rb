3295e24e070ba4182a811a9789d3cedd0b7a89f5
Christian Meier <meier.kristian@gmail.com>
2018-06-11 11:49:25 +0200

    rubocop fiddling to obey it

--- lib/buzzn/schemas/transactions/person/create.rb
+++ lib/buzzn/schemas/transactions/person/create.rb
@@ -2,7 +2,6 @@ require_relative '../../constraints/person'
 require_relative '../../constraints/address'
 require_relative '../person'
 
-
 Schemas::Transactions::Person::Create = Schemas::Support.Form(Schemas::Constraints::Person) do
 
   optional(:address).schema(Schemas::Constraints::Address)
--- spec/transactions/shared_nested_address.rb
+++ spec/transactions/shared_nested_address.rb
@@ -1,25 +1,25 @@
 shared_examples 'create without address' do |transaction, expected_class, params|
 
-    entity(:result) do
-      transaction.(params: params, resource: resource).value
-    end
+  entity(:result) do
+    transaction.(params: params, resource: resource).value
+  end
 
-    it { expect(result.address).to be nil }
-    it { expect(result).to be_a expected_class }
+  it { expect(result.address).to be nil }
+  it { expect(result).to be_a expected_class }
 
 end
 
 shared_examples 'create with address' do |transaction, expected_class, params|
 
-    entity(:address) { {street: 'wallstreet', zip: '666', city: 'atlantis', country: 'IT'} }
+  entity(:address) { {street: 'wallstreet', zip: '666', city: 'atlantis', country: 'IT'} }
 
-    entity(:result) do
-      transaction.(params: params.merge(address: address),
-                   resource: resource).value
-    end
+  entity(:result) do
+    transaction.(params: params.merge(address: address),
+                 resource: resource).value
+  end
 
-    it { expect(result).to be_a expected_class }
-    it { expect(result.address).to be_a AddressResource }
+  it { expect(result).to be_a expected_class }
+  it { expect(result.address).to be_a AddressResource }
 
 end
 
@@ -49,7 +49,7 @@ shared_examples 'update without address' do |transaction, resource, params|
     end
 
     it { expect(result2).to be_a resource.class }
-#      it { expect(result2.name).to eq 'takakari' }
+    #      it { expect(result2.name).to eq 'takakari' }
     it { expect(result2.address).to be_a AddressResource }
 
   end
