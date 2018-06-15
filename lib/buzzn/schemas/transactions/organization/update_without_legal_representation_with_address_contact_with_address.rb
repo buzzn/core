84a47f0f0c5483647bd68e149d1bf9048eb64f64
Christian Meier <meier.kristian@gmail.com>
2018-06-15 11:50:38 +0100

    create/update organization owner

---
+++ lib/buzzn/schemas/transactions/organization/update_without_legal_representation_with_address_contact_with_address.rb
@@ -0,0 +1,12 @@
+require_relative 'update'
+require_relative '../address/update'
+
+module Schemas::Transactions::Organization
+
+  UpdateWithAddressLegalRepresentationContactWithAddress = Schemas::Support.Form(Update) do
+    optional(:address).schema(Schemas::Transactions::Address::Update)
+    optional(:contract).schema(Schemas::Transactions::Person::UpdateWithAddress)
+    optional(:legal_representation).schema(Schemas::Transactions::Person::Create)
+  end
+
+end
