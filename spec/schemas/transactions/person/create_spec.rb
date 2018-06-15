dde8308f626909b23b26fb1c62074c3ca3159c41
Christian Meier <meier.kristian@gmail.com>
2018-06-15 11:49:48 +0100

    create/update owner person bits

--- spec/schemas/transactions/person/create_spec.rb
+++ spec/schemas/transactions/person/create_spec.rb
@@ -3,7 +3,7 @@ require_relative '../shared_nested_address'
 
 describe 'Schemas::Transactions::Person::Create' do
 
-  subject { Schemas::Transactions::Person::Create }
+  subject { Schemas::Transactions::Person::CreateWithAddress }
 
   it_behaves_like 'create with nested address', prefix: 'M', first_name: 'Frank', last_name: 'Zappa', preferred_language: 'de'
 
