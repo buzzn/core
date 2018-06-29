a615324708b410c11806ae89b81b1a3bd668a31f
Maximilian <maximilian@buzzn.net>
2018-06-29 16:37:59 +0200

    services: add sidekiq services and specs

---
+++ lib/buzzn/services/sidekiq_server.rb
@@ -0,0 +1,25 @@
+require_relative '../services'
+require 'sidekiq'
+
+class Services::SidekiqServer
+
+  include Import[redis: 'services.redis_sidekiq']
+
+  def create
+    redis_conn = proc {
+      redis
+    }
+    Sidekiq.configure_server do |config|
+      config.redis = ConnectionPool.new(size: 25, &redis_conn)
+    end
+  end
+
+  def self.new
+    @instance ||= super().create
+  end
+
+  def self.current
+    self.new
+  end
+
+end
