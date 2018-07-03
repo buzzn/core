1ac5f0c13a47f55b0367a4dadd52822683a82fb1
Maximilian <maximilian@buzzn.net>
2018-07-03 11:11:25 +0200

    introduce MailService with Stdout and Mailgun Backend
    reduce MailWorker to two lines

--- lib/buzzn/workers/mail_worker.rb
+++ lib/buzzn/workers/mail_worker.rb
@@ -1,60 +1,14 @@
-require 'net/http'
-
 module Buzzn
-
-  class MailSendError < StandardError
-  end
-
   module Workers
     class MailWorker
 
       include Sidekiq::Worker
 
-      def perform(from, to, subject, text, bcc, html)
-        case Import.global('config.mail_backend')
-        when 'smtp'
-          # not implemented yet
-          nil
-        when 'mailgun'
-          api_key = Import.global('config.mailgun_api_key')
-          mailgun_domain = Import.global('config.mailgun_domain')
-          form_data = {
-            'from' => from,
-            'to' => to,
-            'subject' => subject,
-            'text' => text,
-          }
-          unless bcc.nil?
-            form_data['bcc'] = bcc
-          end
-          unless html.nil?
-            form_data['html'] = html
-          end
-
-          uri = URI.parse("https://api.mailgun.net/v3/#{mailgun_domain}/messages")
-          req = Net::HTTP::Post.new(uri)
-          req.basic_auth 'api', api_key
-          req.set_form_data(form_data)
-          opts = { use_ssl: uri.scheme == 'https'}
-          res = Net::HTTP.start(uri.hostname, uri.port, opts) do |http|
-            http.use_ssl = true
-            http.request(req)
-          end
-
-          case res
-          when Net::HTTPSuccess, Net::HTTPRedirection
-            # done
-            nil
-          else
-            raise MailSendError.new res.body
-          end
-        when 'noop'
-          # do nothing
-          nil
-        end
+      def perform(message = {})
+        mail_service = Import.global('services.mail_service')
+        mail_service.deliver(message)
       end
 
     end
   end
-
 end
