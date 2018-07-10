eacc4e2742f7f5e7c3fecf83bd0ccefc6cbc0e9c
Christian Meier <meier.kristian@gmail.com>
2018-07-10 11:08:08 +0200

    move Import.global to auto_inject

--- lib/buzzn/workers/mail_worker.rb
+++ lib/buzzn/workers/mail_worker.rb
@@ -1,11 +1,13 @@
+require 'sidekiq/worker'
+
 module Buzzn
   module Workers
     class MailWorker
 
+      include Import['services.mail_service']
       include Sidekiq::Worker
 
       def perform(message = {})
-        mail_service = Import.global('services.mail_service')
         mail_service.deliver(message)
       end
 
