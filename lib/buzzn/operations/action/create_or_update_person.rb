065f4abcbcbf1e9be9a87ecee63a81224ca2e6a0
Christian Meier <meier.kristian@gmail.com>
2018-07-13 10:56:43 +0200

    fix to allow assign existing person to created organziation owner

--- lib/buzzn/operations/action/create_or_update_person.rb
+++ lib/buzzn/operations/action/create_or_update_person.rb
@@ -6,13 +6,17 @@ module Operations::Action
 
     def call(params:, resource:, method:, **)
       if (person_resource = resource&.send(method)) && params.key?(method)
-        if params.size == 1 && params.key?('id')
-          params[method] = Person.find(params['id'])
+        if params.size == 1 && params.key?(:id)
+          params[method] = Person.find(params[:id])
         else
           super(params: params.delete(method), resource: person_resource)
         end
       elsif person_params = params[method]
-        params[method] = Person.create(person_params)
+        if person_params.size == 1 && person_params.key?(:id)
+          params[method] = Person.find(person_params[:id])
+        else
+          params[method] = Person.create(person_params)
+        end
       end
     end
 
