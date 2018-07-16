ccba264a42973fdaa860fbf332045154c4d4a7d7
Maximilian <maximilian@buzzn.net>
2018-07-16 17:44:05 +0200

    instance_class -> class
    This fails for Collections. Needs investigation.

--- lib/buzzn/operations/authorization/create.rb
+++ lib/buzzn/operations/authorization/create.rb
@@ -4,7 +4,7 @@ class Operations::Authorization::Create
 
   def call(resource:, **)
     unless resource.createable?
-      raise Buzzn::PermissionDenied.new(resource.instance_class, :create, resource.security_context.current_user)
+      raise Buzzn::PermissionDenied.new(resource.class, :create, resource.security_context.current_user)
       # TODO better a Left Monad and handle on roda
     end
     true
