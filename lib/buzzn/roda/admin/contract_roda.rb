a9d90ea57237087993bb5ec98ea68f0d3ec4d011
Maximilian <maximilian@buzzn.net>
2018-07-23 23:17:55 +0200

    add POST endpoint for contracts
    started to implement for LocalpoolProcessing Contract

--- lib/buzzn/roda/admin/contract_roda.rb
+++ lib/buzzn/roda/admin/contract_roda.rb
@@ -4,20 +4,19 @@ module Admin
   class ContractRoda < BaseRoda
 
     include Import.args[:env,
-                        'transactions.admin.contract.document'
+                        'transactions.admin.contract.document',
+                        'transactions.admin.contract.create_localpool_processing',
                        ]
 
     plugin :shared_vars
+    plugin :param_matchers
 
     PARENT = :contract
 
     route do |r|
 
       contracts = shared[LocalpoolRoda::PARENT].contracts
-
-      r.get! do
-        contracts
-      end
+      localpool_processing_contracts = shared[LocalpoolRoda::PARENT].localpool_processing_contracts
 
       r.on :id do |id|
 
@@ -45,6 +44,20 @@ module Admin
         end
 
       end
+
+      r.get! do
+        contracts
+      end
+
+      r.post!(:param=>'type') do |type|
+        case type.to_s
+        when 'contract_localpool_processing'
+          create_localpool_processing.(resource: localpool_processing_contracts, params: r.params)
+        else
+          r.response.status 400
+        end
+      end
+
     end
 
   end
---
+++ lib/buzzn/schemas/transactions/admin/contract/localpool_processing/create.rb
@@ -0,0 +1,4 @@
+require_relative '../localpool_processing'
+
+Schemas::Transactions::Admin::Contract::LocalpoolProcessing::Create = Schemas::Support.Form(Schemas::Constraints::Contract::Base) do
+end
