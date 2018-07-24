475dc55e7d15891db93b2d9d19ef8e62a6a98adb
Maximilian <maximilian@buzzn.net>
2018-07-24 15:08:02 +0200

    spec: add specs for LocalpoolProcessing

--- lib/buzzn/schemas/transactions/admin/contract/localpool_processing/create.rb
+++ lib/buzzn/schemas/transactions/admin/contract/localpool_processing/create.rb
@@ -2,4 +2,5 @@ require_relative '../localpool_processing'
 require_relative '../../../../constraints/contract/base'
 
 Schemas::Transactions::Admin::Contract::LocalpoolProcessing::Create = Schemas::Support.Form(Schemas::Constraints::Contract::Base) do
+  required(:tax_number).filled(:str?, max_size?: 64)
 end
