ff3bc9f805774eb00fe857e99403b5e729b1b02e
Maximilian <maximilian@buzzn.net>
2018-07-25 23:25:30 +0200

    add PreConditions check before rendering

--- lib/buzzn/transactions/admin/contract/document.rb
+++ lib/buzzn/transactions/admin/contract/document.rb
@@ -1,12 +1,26 @@
 require_relative '../contract'
+require_relative '../../../schemas/pre_conditions/contract/document_localpool_processing_contract'
 
 class Transactions::Admin::Contract::Document < Transactions::Base
 
   check :authorize, with: :'operations.authorization.document'
+  tee :contract_schema
+  around :db_transaction
   add :generator
   add :generate_document
   add :contract_document
   map :result
+
+  def contract_schema(resource:, **)
+    case resource
+    when Contract::LocalpoolProcessingResource
+      subject = Schemas::Support::ActiveRecordValidator.new(resource.object)
+      result = Schemas::PreConditions::Contract::DocumentLocalpoolProcessingContract.call(subject)
+      unless result.success?
+        raise Buzzn::ValidationError.new(result.errors)
+      end
+    end
+  end
 
   def generator(resource:, params:)
     resource.pdf_generator.new(resource)
@@ -18,7 +32,10 @@ class Transactions::Admin::Contract::Document < Transactions::Base
 
   def contract_document(resource:, params:, generator:, generate_document:)
     doc = generate_document.document
-    resource.object.documents << doc
+    # check if it already exists
+    unless resource.object.documents.where(:id => doc.id).any?
+      resource.object.documents << doc
+    end
     doc
   end
 
