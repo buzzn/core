d563d8d087c3e55aec0fe16096a7051ab63dbb63
Maximilian <maximilian@buzzn.net>
2018-07-25 23:24:03 +0200

    add allowed_actions with DocumentLocalpoolProcessingContract

--- lib/buzzn/resources/contract/localpool_processing_resource.rb
+++ lib/buzzn/resources/contract/localpool_processing_resource.rb
@@ -5,11 +5,25 @@ module Contract
 
     model LocalpoolProcessing
 
-    attributes :begin_date
-    attributes :tax_number
+    attributes :begin_date,
+               :tax_number,
+               :allowed_actions
 
     has_one :contractor
     has_one :customer
+
+    def allowed_actions
+      allowed = {}
+      if allowed?(permissions.document)
+        allowed[:document_localpool_processing_contract] = document_localpool_processing_contract.success? || document_localpool_processing_contract.errors
+      end
+      allowed
+    end
+
+    def document_localpool_processing_contract
+      subject = Schemas::Support::ActiveRecordValidator.new(self.object)
+      Schemas::PreConditions::Contract::DocumentLocalpoolProcessingContract.call(subject)
+    end
 
   end
 end
