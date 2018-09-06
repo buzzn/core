67e87cc996b9d20657ffe5dd08af1e9b7452ce7a
Maximilian <maximilian@buzzn.net>
2018-09-06 14:00:24 +0200

    devices: remove two_way_meter* and electricy_supplier

--- spec/transactions/admin/device_spec.rb
+++ spec/transactions/admin/device_spec.rb
@@ -5,8 +5,6 @@ describe Transactions::Admin::Device do
   entity(:input) do
     {
       law: 'free',
-      two_way_meter: 'yes',
-      two_way_meter_used: 'yes',
       primary_energy: 'bio_mass',
       commissioning: Date.today.as_json,
       kw_peak: 1.250,
@@ -36,18 +34,7 @@ describe Transactions::Admin::Device do
     it { expect(result.value!).to be_a Admin::DeviceResource }
     it { expect(result.value!.watt_peak).to eq(1250) }
     it { expect(result.value!.watt_hour_pa).to eq(23400) }
-    it { expect(result.value!.electricity_supplier).to be_nil }
 
-    context 'wtih electricity supplier' do
-
-      entity(:supplier) { create(:organization, :electricity_supplier) }
-      entity(:result2) do
-        transaction.(params: input.merge(electricity_supplier: {id: supplier.id}),
-                     resource: resource)
-      end
-
-      it { expect(result2.value!.electricity_supplier.id).to eq supplier.id }
-    end
   end
 
   describe 'Update' do
@@ -73,16 +60,5 @@ describe Transactions::Admin::Device do
     it { expect(result.value!.watt_peak).to eq(1250) }
     it { expect(result.value!.watt_hour_pa).to eq(23400) }
 
-    context 'wtih electricity supplier' do
-
-      entity(:supplier) { create(:organization, :electricity_supplier) }
-      entity(:result2) do
-        transaction.(params: input.merge(updated_at: resource.updated_at.as_json,
-                                         electricity_supplier: {id: supplier.id}),
-                     resource: resource)
-      end
-
-      it { expect(result2.value!.electricity_supplier.id).to eq supplier.id }
-    end
   end
 end
