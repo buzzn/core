ff4bf143250146c21411941bbeff4b42bce5f67b
Maximilian <maximilian@buzzn.net>
2018-09-17 11:46:40 +0200

    pdfs: updated separator

--- app/pdfs/02_abwicklungsvertrag.rb
+++ app/pdfs/02_abwicklungsvertrag.rb
@@ -64,7 +64,7 @@ module Pdf
       names = [name(legal_partner(customer))]
       if customer == Organization::GeneralResource || customer == Organization::Base
         if !customer.additional_legal_representation.nil? || !customer.additional_legal_representation.empty?
-          names += customer.additional_legal_representation.split(',').map {|x| x.strip }
+          names += customer.additional_legal_representation.split('$#$').map {|x| x.strip }
         end
       end
       if names.size > 1
