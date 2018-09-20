2ded41656b7b8efb1e6d339f758e506743cfba89
Maximilian <maximilian@buzzn.net>
2018-09-20 14:19:44 +0200

    organization: make most fields nullable through maybe

--- lib/buzzn/schemas/constraints/organization/base.rb
+++ lib/buzzn/schemas/constraints/organization/base.rb
@@ -2,10 +2,10 @@ require_relative '../organization'
 
 Schemas::Constraints::Organization::Base = Schemas::Support.Form do
   required(:name).filled(:str?, max_size?: 64, min_size?: 4)
-  optional(:description).filled(:str?, max_size?: 256)
-  optional(:email).filled(:str?, :email?, max_size?: 64)
-  optional(:phone).filled(:str?, :phone_number?, max_size?: 64)
-  optional(:fax).filled(:str?, :phone_number?, max_size?: 64)
-  optional(:website).filled(:str?, :url?, max_size?: 64)
-  optional(:additional_legal_representation).filled(:str?, max_size?: 256)
+  optional(:description).maybe(:str?, max_size?: 256)
+  optional(:email).maybe(:str?, :email?, max_size?: 64)
+  optional(:phone).maybe(:str?, :phone_number?, max_size?: 64)
+  optional(:fax).maybe(:str?, :phone_number?, max_size?: 64)
+  optional(:website).maybe(:str?, :url?, max_size?: 64)
+  optional(:additional_legal_representation).maybe(:str?, max_size?: 256)
 end
