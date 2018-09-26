90fcf24c94285dba4107b0f32465e26364b8140e
Maximilian <maximilian@buzzn.net>
2018-09-26 17:57:36 +0200

    metering_location_id: fix tests

--- spec/requests/admin/real_meter_spec.rb
+++ spec/requests/admin/real_meter_spec.rb
@@ -50,6 +50,7 @@ describe Admin::LocalpoolRoda, :request_helper do
         'edifact_cycle_interval'=>meter.attributes['edifact_cycle_interval'],
         'edifact_data_logging'=>meter.attributes['edifact_data_logging'],
         'sent_data_dso'=>meter.sent_data_dso.to_s,
+        'metering_location_id'=>meter.metering_location_id
       }
     end
 
@@ -139,7 +140,8 @@ describe Admin::LocalpoolRoda, :request_helper do
           'edifact_voltage_level'=>'E04',
           'edifact_cycle_interval'=>'QUARTERLY',
           'edifact_data_logging'=>'Z04',
-          'sent_data_dso'=>'2010-01-01'
+          'sent_data_dso'=>'2010-01-01',
+          'metering_location_id'=>nil
         }
       end
 
