43782a73a3ee458dcc80b5d2368ee07703a56ac4
Maximilian <maximilian@buzzn.net>
2018-11-09 13:01:01 +0100

    invariants: remove conflicting attributes

--- lib/buzzn/schemas/invariants/register/base.rb
+++ lib/buzzn/schemas/invariants/register/base.rb
@@ -7,10 +7,6 @@ module Schemas
 
       Meta = Schemas::Support.Form(Schemas::Constraints::Register::Meta) do
 
-        required(:observer_enabled).maybe
-        required(:observer_min_threshold).maybe
-        required(:observer_max_threshold).maybe
-
         rule(observer_enabled: [:observer_enabled, :observer_min_threshold, :observer_max_threshold]) do |observer_enabled, observer_min_threshold, observer_max_threshold|
           observer_enabled.true?.then(observer_max_threshold.filled?.and(observer_min_threshold.filled?).and(observer_max_threshold.gteq?(value(:observer_min_threshold))))
         end
