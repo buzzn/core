43e177c2375846317cb7534c1e5c424e378079dc
Maximilian <maximilian@buzzn.net>
2018-11-29 17:23:33 +0100

    end_date: last_date can be nil

--- lib/buzzn/operations/end_date.rb
+++ lib/buzzn/operations/end_date.rb
@@ -3,7 +3,13 @@ require_relative '../operations'
 class Operations::EndDate
 
   def call(params:, **)
-    params[:end_date] = params.delete(:last_date) + 1.day if params.key?(:last_date)
+    if params.key?(:last_date)
+      if params[:last_date].nil?
+        params.delete(:last_date)
+      else
+        params[:end_date] = params.delete(:last_date) + 1.day
+      end
+    end
   end
 
 end
