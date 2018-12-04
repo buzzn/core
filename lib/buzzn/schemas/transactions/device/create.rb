09318c674d3f8be9858aa212bd490a59331d3844
Maximilian <maximilian@buzzn.net>
2018-12-04 16:10:23 +0100

    predicates: introduce bigint support, add to device

--- lib/buzzn/schemas/transactions/device/create.rb
+++ lib/buzzn/schemas/transactions/device/create.rb
@@ -4,7 +4,7 @@ require_relative '../device'
 require_relative '../id'
 
 Schemas::Transactions::Device::Create = Schemas::Support.Form(Schemas::Constraints::DeviceCommon) do
-  required(:kw_peak).filled(:float?)
-  required(:kwh_per_annum).filled(:float?)
+  required(:kw_peak).filled(:float?, lt?: 10000)
+  required(:kwh_per_annum).filled(:float?, lt?: 366*24*10000)
   optional(:electricity_supplier).schema(Schemas::Transactions::Id)
 end
