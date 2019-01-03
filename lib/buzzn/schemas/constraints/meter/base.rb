0829641fa7f36e49923cf72de1d79695eec0ccb2
Maximilian <maximilian@buzzn.net>
2019-01-03 13:37:56 +0100

    meter: make direction_number mandatory

--- lib/buzzn/schemas/constraints/meter/base.rb
+++ lib/buzzn/schemas/constraints/meter/base.rb
@@ -2,6 +2,8 @@ require_relative 'common'
 require_relative '../../../../../app/models/meter/real'
 
 Schemas::Constraints::Meter::Base = Schemas::Support.Form(Schemas::Constraints::Meter::Common) do
+  required(:direction_number).maybe(included_in?: Meter::Real.direction_numbers.values)
+
   optional(:datasource).maybe(included_in?: Meter::Real.datasources.values)
   optional(:product_name).maybe(:str?, max_size?: 64)
   optional(:manufacturer_name).maybe(included_in?: Meter::Real.manufacturer_names.values)
@@ -12,7 +14,6 @@ Schemas::Constraints::Meter::Base = Schemas::Support.Form(Schemas::Constraints::
   optional(:sent_data_dso).maybe(:date?)
   optional(:converter_constant).maybe(:int?)
   optional(:calibrated_until).maybe(:date?)
-  optional(:direction_number).maybe(included_in?: Meter::Real.direction_numbers.values)
   optional(:edifact_metering_type).maybe(included_in?: Meter::Real.edifact_metering_types.values)
   optional(:edifact_meter_size).maybe(included_in?: Meter::Real.edifact_meter_sizes.values)
   optional(:edifact_measurement_method).maybe(included_in?: Meter::Real.edifact_measurement_methods.values)
