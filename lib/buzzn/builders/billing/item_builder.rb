9d90fe72591fbdaebc9c2eaf57b11217ee24f446
Maximilian <maximilian@buzzn.net>
2019-01-15 23:51:33 +0100

    Billings: don't allow single day ranges, fail silently for some errors

--- lib/buzzn/builders/billing/item_builder.rb
+++ lib/buzzn/builders/billing/item_builder.rb
@@ -5,15 +5,14 @@ module Builders::Billing
 
     class << self
 
-
-      def from_contract(contract, max_date_range, tariff)
+      def from_contract(contract, max_date_range, tariff, fail_silent: true)
         date_range = date_range(contract, max_date_range)
         attrs = {
           contract_type:   contract_type(contract),
           date_range:      date_range,
           tariff:          tariff,
-          begin_reading:   reading_close_to(contract, date_range.first),
-          end_reading:     reading_close_to(contract, date_range.last),
+          begin_reading:   reading_close_to(contract, date_range.first, fail_silent: fail_silent),
+          end_reading:     reading_close_to(contract, date_range.last,  fail_silent: fail_silent),
           register:        contract.register_meta.register
         }
         BillingItem.new(attrs)
@@ -50,9 +49,17 @@ module Builders::Billing
         end
       end
 
-      def reading_close_to(contract, date)
+      def reading_close_to(contract, date, fail_silent: true)
         reading_service = Import.global('services.reading_service')
-        readings = reading_service.get(contract.register_meta.register, date, :precision => 2.days)
+        begin
+          readings = reading_service.get(contract.register_meta.register, date, :precision => 2.days)
+        rescue Buzzn::DataSourceError => error
+          if fail_silent
+            readings = []
+          else
+            raise error
+          end
+        end
         readings.to_a.max_by(&:value) # if there's more than one reading, take the highest one.
       end
 
