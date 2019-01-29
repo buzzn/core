142a37cc2efad3a87e236ecdd0db0f14cb063df2
Maximilian <maximilian@buzzn.net>
2019-01-29 14:56:31 +0100

    contraints: don't allow localpool contracts before its inception

--- lib/buzzn/schemas/pre_conditions/localpool/create_metering_point_operator_contract.rb
+++ lib/buzzn/schemas/pre_conditions/localpool/create_metering_point_operator_contract.rb
@@ -8,6 +8,7 @@ module Schemas::PreConditions::Localpool
 
   CreateMeteringPointOperatorContract = Schemas::Support.Schema do
 
+    required(:start_date).value(:filled?)
     required(:address).filled
     required(:owner) do
       filled?.and(type?(Organization).then(schema(Schemas::PreConditions::Organization)).and(type?(Person).then(schema(Schemas::PreConditions::Person))))
