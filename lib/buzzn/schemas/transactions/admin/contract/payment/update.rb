3c3d979c32e0c3af4ae6507ad573d3ac9d4255e0
Maximilian <maximilian@buzzn.net>
2019-01-31 18:38:22 +0100

    payments: don't allow huge values

--- lib/buzzn/schemas/transactions/admin/contract/payment/update.rb
+++ lib/buzzn/schemas/transactions/admin/contract/payment/update.rb
@@ -2,7 +2,7 @@ require_relative '../payment'
 
 Schemas::Transactions::Admin::Contract::Payment::Update = Schemas::Support.Form(Schemas::Transactions::Update) do
   optional(:begin_date).filled(:date?)
-  optional(:price_cents).filled(:int?, gteq?: 0)
-  optional(:energy_consumption_kwh_pa).filled(:int?, gteq?: 0)
+  optional(:price_cents).filled(:int?, gteq?: 0, lt?: (2**32)/2)
+  optional(:energy_consumption_kwh_pa).filled(:int?, gteq?: 0, lt?: (2**32)/2)
   optional(:cycle).value(included_in?: Contract::Payment.cycles.values)
 end
