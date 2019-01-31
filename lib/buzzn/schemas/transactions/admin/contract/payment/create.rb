84f6059406d1816b796a65b43c86c9193c842cbc
Maximilian <maximilian@buzzn.net>
2019-01-31 14:45:50 +0100

    payments: make all values gteq 0

--- lib/buzzn/schemas/transactions/admin/contract/payment/create.rb
+++ lib/buzzn/schemas/transactions/admin/contract/payment/create.rb
@@ -2,11 +2,11 @@ require_relative '../payment'
 
 Schemas::Transactions::Admin::Contract::Payment::Create = Schemas::Support.Form do
   required(:begin_date).filled(:date?)
-  required(:energy_consumption_kwh_pa).filled(:int?, gt?: 0)
+  required(:energy_consumption_kwh_pa).filled(:int?, gteq?: 0)
   required(:cycle).value(included_in?: Contract::Payment.cycles.values)
   # tariff that should be used to calculate the price
   optional(:tariff_id).filled(:int?)
-  optional(:price_cents).maybe(:int?, gt?: 0)
+  optional(:price_cents).maybe(:int?, gteq?: 0)
 
   rule(price_cents: [:tariff_id, :price_cents]) do |tariff_id, price_cents|
     tariff_id.filled?.not.then(price_cents.filled?)#.and(price_cents.value(:int?, gt?: 0)))
