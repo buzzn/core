21a92f0517f8ee97ebfcbd07daf382a32ad44a2e
Maximilian <maximilian@buzzn.net>
2019-01-31 14:46:14 +0100

    wicked_pdf: fix dpi, no idea why this happend

--- lib/buzzn/services/pdf_html_generator.rb
+++ lib/buzzn/services/pdf_html_generator.rb
@@ -24,7 +24,7 @@ class Services::PdfHtmlGenerator
   end
 
   def generate_pdf(name_or_template, struct)
-    WickedPdf.new(find_wkhtmltopdf_binary_path).pdf_from_string(render_html(name_or_template, struct), javascript_delay: 0, dpi: '380', extra: '--enable-forms')
+    WickedPdf.new(find_wkhtmltopdf_binary_path).pdf_from_string(render_html(name_or_template, struct), javascript_delay: 0, dpi: 75, extra: '--enable-forms')
   end
 
   def find_wkhtmltopdf_binary_path
