2064f88550aef261b762a477a1b5d425c8fb41aa
Maximilian <maximilian@buzzn.net>
2019-01-31 19:13:03 +0100

    accounting: define proper 64-bit bounds

--- lib/buzzn/schemas/transactions/accouting/book.rb
+++ lib/buzzn/schemas/transactions/accouting/book.rb
@@ -2,7 +2,7 @@
 module Schemas::Transactions::Accounting
 
   Book = Schemas::Support.Form do
-    required(:amount).filled(:bigint?)
+    required(:amount).filled(:bigint?, gt?: -(2**64)/2, lt?: (2**64)/2)
     optional(:external_reference).maybe(:str?, max_size?: 256)
     optional(:external_settled_at).maybe(:date?)
     optional(:comment).maybe(:str?, max_size?: 256)
