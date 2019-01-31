b280cb616cb904213a661947513bea47f11dfd4c
Maximilian <maximilian@buzzn.net>
2019-01-31 14:45:50 +0100

    payments: make all values gteq 0

--- lib/buzzn/schemas/transactions/admin/contract/payment/update.rb
+++ lib/buzzn/schemas/transactions/admin/contract/payment/update.rb
@@ -2,7 +2,7 @@ require_relative '../payment'
 
 Schemas::Transactions::Admin::Contract::Payment::Update = Schemas::Support.Form(Schemas::Transactions::Update) do
   optional(:begin_date).filled(:date?)
-  optional(:price_cents).filled(:int?, gt?: 0)
-  optional(:energy_consumption_kwh_pa).filled(:int?, gt?: 0)
+  optional(:price_cents).filled(:int?, gteq?: 0)
+  optional(:energy_consumption_kwh_pa).filled(:int?, gteq?: 0)
   optional(:cycle).value(included_in?: Contract::Payment.cycles.values)
 end
