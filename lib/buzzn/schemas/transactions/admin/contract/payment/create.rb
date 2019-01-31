bf3f25f75c2479970675aef61d4a84a0a553d8c8
Maximilian <maximilian@buzzn.net>
2019-01-31 18:38:22 +0100

    payments: don't allow huge values

--- lib/buzzn/schemas/transactions/admin/contract/payment/create.rb
+++ lib/buzzn/schemas/transactions/admin/contract/payment/create.rb
@@ -2,11 +2,11 @@ require_relative '../payment'
 
 Schemas::Transactions::Admin::Contract::Payment::Create = Schemas::Support.Form do
   required(:begin_date).filled(:date?)
-  required(:energy_consumption_kwh_pa).filled(:int?, gteq?: 0)
+  required(:energy_consumption_kwh_pa).filled(:int?, gteq?: 0, lt?: (2**32)/2)
   required(:cycle).value(included_in?: Contract::Payment.cycles.values)
   # tariff that should be used to calculate the price
   optional(:tariff_id).filled(:int?)
-  optional(:price_cents).maybe(:int?, gteq?: 0)
+  optional(:price_cents).maybe(:int?, gteq?: 0, lt?: (2**32)/2)
 
   rule(price_cents: [:tariff_id, :price_cents]) do |tariff_id, price_cents|
     tariff_id.filled?.not.then(price_cents.filled?)#.and(price_cents.value(:int?, gt?: 0)))
