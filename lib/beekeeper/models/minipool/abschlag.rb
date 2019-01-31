a7b7d23b35da88aa6fd8425e7c7c3b8595760d63
Maximilian <maximilian@buzzn.net>
2019-01-31 14:45:30 +0100

    importer: import Abschläge / payments

--- lib/beekeeper/models/minipool/abschlag.rb
+++ lib/beekeeper/models/minipool/abschlag.rb
@@ -16,4 +16,21 @@ class Beekeeper::Minipool::Abschlag < Beekeeper::Minipool::BaseRecord
 
   self.table_name = 'minipooldb.abschlag'
 
+  def converted_attributes
+    {
+      price_cents: abschlag*100,
+      energy_consumption_kwh_pa: arbeit,
+      begin_date: date_parsed,
+      cycle: 'monthly'
+    }
+  end
+
+  def date_parsed
+    begin
+      Date.parse(datum)
+    rescue ArgumentError
+      raise "invalid date. please fix #{self.to_json}"
+    end
+  end
+
 end
