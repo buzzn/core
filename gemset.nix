72242431b63de63f17d32466033dbeeb528dac7e
Maximilian <maximilian@buzzn.net>
2019-02-01 12:24:22 +0100

    gems: replace wkhtmltopdf-binary-edge with -heroku

--- gemset.nix
+++ gemset.nix
@@ -1369,12 +1369,12 @@
     };
     version = "1.1.0";
   };
-  wkhtmltopdf-binary-edge = {
+  wkhtmltopdf-heroku = {
     source = {
       remotes = ["https://rubygems.org"];
-      sha256 = "0y2w09yynnqj5vzbivjpkrxfkilhw4zn7yrc49sac54315bb2nhr";
+      sha256 = "1grwyypq2r2w3vcah6sd7b6m6l9wi1qq882svissxq1m31rz9w76";
       type = "gem";
     };
-    version = "0.12.4.0";
+    version = "2.12.4.0";
   };
 }
