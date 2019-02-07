d38910d0b00d8ae85f4319697961d9e8fbb4ae0c
Maximilian <maximilian@buzzn.net>
2019-02-07 18:23:13 +0100

    gap_contract_customer: add create transactions

---
+++ spec/transactions/admin/localpool/create_organization_gap_contract_customer.rb
@@ -0,0 +1,27 @@
+require_relative '../../shared_nested_address'
+require_relative '../../shared_nested_person'
+require 'buzzn/transactions/admin/localpool/create_organization_gap_contract_customer'
+
+describe Transactions::Admin::Localpool::CreateOrganizationGapContractCustomer do
+
+  entity!(:localpool) { create(:group, :localpool) }
+
+  entity(:operator) { create(:account, :buzzn_operator) }
+
+  entity(:resource) { Admin::LocalpoolResource.all(operator).first }
+
+  it_behaves_like 'create without address', Transactions::Admin::Localpool::CreateOrganizationOwner.new, Organization::GeneralResource, name: 'Zappa-For-President'
+  it_behaves_like 'create with address', Transactions::Admin::Localpool::CreateOrganizationOwner.new, Organization::GeneralResource, name: 'Elvis-Lives'
+
+  context 'contact' do
+    it_behaves_like 'create without person', Transactions::Admin::Localpool::CreateOrganizationOwner.new, Organization::GeneralResource, :contact, name: 'Zappa-For-President-Forever'
+    it_behaves_like 'create with person without address', Transactions::Admin::Localpool::CreateOrganizationOwner.new, Organization::GeneralResource, :contact, name: 'Elvis-Lives-Forever'
+    it_behaves_like 'create with person with address', Transactions::Admin::Localpool::CreateOrganizationOwner.new, Organization::GeneralResource, :contact, name: 'Mamas-and-Papas'
+  end
+
+  context 'legal_representation' do
+    it_behaves_like 'create without person', Transactions::Admin::Localpool::CreateOrganizationOwner.new, Organization::GeneralResource, :legal_representation, name: 'Zappa-For-President-Again-And-Again'
+
+    it_behaves_like 'create with person without address', Transactions::Admin::Localpool::CreateOrganizationOwner.new, Organization::GeneralResource, :legal_representation, name: 'Elvis-Lives-Again-And-Again'
+  end
+end
