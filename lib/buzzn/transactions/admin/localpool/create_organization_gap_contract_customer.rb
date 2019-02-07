420346d01078507738ee6eb5d82528e4de3e1a6d
Maximilian <maximilian@buzzn.net>
2019-02-07 18:23:13 +0100

    gap_contract_customer: add create transactions

--- lib/buzzn/transactions/admin/localpool/create_organization_gap_contract_customer.rb
+++ lib/buzzn/transactions/admin/localpool/create_organization_gap_contract_customer.rb
@@ -1,8 +1,8 @@
-require_relative 'owner_base'
+require_relative 'gap_contract_customer_base'
 require_relative 'create_organization_base'
 
 module Transactions::Admin::Localpool
-  class CreateOrganizationOwner < OwnerBase
+  class CreateOrganizationGapContractCustomer < GapContractCustomerBase
 
     include CreateOrganizationBase
 
@@ -14,14 +14,14 @@ module Transactions::Admin::Localpool
     tee :create_contact, with: :'operations.action.create_person'
     tee :create_legal_representation, with: :'operations.action.create_person'
     add :new_organization, with: :'operations.action.create_item'
-    map :assign_owner # from super-class
+    map :assign_gap_contract_customer # from super-class
 
     def allowed_roles(permission_context:)
       permission_context.owner.create
     end
 
-    def assign_owner(new_organization:, resource:, **)
-      super(new_owner: new_organization, resource: resource)
+    def assign_gap_contract_customer(new_organization:, resource:, **)
+      super(new_customer: new_organization, resource: resource)
     end
 
   end
