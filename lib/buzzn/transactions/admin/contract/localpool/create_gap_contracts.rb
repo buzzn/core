018edae479354e3c19636282fbe59934841802e1
Maximilian <maximilian@buzzn.net>
2019-02-12 19:14:46 +0100

    create_gap_contracts: don't create them outside of range

--- lib/buzzn/transactions/admin/contract/localpool/create_gap_contracts.rb
+++ lib/buzzn/transactions/admin/contract/localpool/create_gap_contracts.rb
@@ -36,47 +36,47 @@ module Transactions::Admin::Contract::Localpool
         installed_at     = (register_meta.registers.collect { |r| r.installed_at     }.reject(&:nil?).sort_by { |x| x.date }.first)&.date
         decomissioned_at = (register_meta.registers.collect { |r| r.decomissioned_at }.reject(&:nil?).sort_by { |x| x.date }.last)&.date
 
-        date_range = ([params[:begin_date], installed_at || params[:begin_date]].max)..([params[:end_date], decomissioned_at || params[:end_date]]).min
+        if installed_at > params[:begin_date] || (!decomissioned_at.nil? && decomissioned_at < params[:end_date])
+          ranges = [ ]
+        else
+          date_range = ([params[:begin_date], installed_at || params[:begin_date]].max)..([params[:end_date], decomissioned_at || params[:end_date]]).min
 
-        ranges = [ date_range ]
-        register_meta.contracts.order(:begin_date).each do |contract|
-          new_ranges = []
-          ranges.each_with_index do |range, idx|
-            #   [ range ]
-            # [ contract ]
-            if contract.begin_date <= range.first && (contract.end_date.nil? || contract.end_date >= range.last)
-              ranges[idx] = range.first..range.first
-              next
+          ranges = [ date_range ]
+          register_meta.contracts.order(:begin_date).each do |contract|
+            new_ranges = []
+            ranges.each_with_index do |range, idx|
+              #   [ range ]
+              # [ contract ]
+              if contract.begin_date <= range.first && (contract.end_date.nil? || contract.end_date >= range.last)
+                ranges[idx] = range.first..range.first
+                next
+              end
+              # [ range ]
+              #     [   c   ]
+              # [ r ]
+              if contract.begin_date >= range.first && contract.begin_date < range.last && (contract.end_date.nil? || contract.end_date >= range.last)
+                ranges[idx] = range.first..contract.begin_date
+                next
+              end
+              #    [ range ]
+              # [   c  ]
+              #        [ r ]
+              if contract.begin_date <= range.first && (contract.end_date.nil? || (contract.end_date > range.first && contract.end_date <= range.last))
+                ranges[idx] = contract.end_date..range.last
+                next
+              end
+              # [    range     ]
+              #    [   c   ]
+              if range.first <= contract.begin_date && (!contract.end_date.nil? && (range.last > contract.end_date))
+                new_range = contract.end_date..range.last
+                ranges[idx] = range.first..contract.begin_date
+                new_ranges << new_range
+                next
+              end
             end
-            # [ range ]
-            #     [   c   ]
-            # [ r ]
-            begin
-            if contract.begin_date >= range.first && contract.begin_date < range.last && (contract.end_date.nil? || contract.end_date >= range.last)
-              ranges[idx] = range.first..contract.begin_date
-              next
-            end
-            #    [ range ]
-            # [   c  ]
-            #        [ r ]
-            if contract.begin_date <= range.first && (contract.end_date.nil? || (contract.end_date > range.first && contract.end_date <= range.last))
-              ranges[idx] = contract.end_date..range.last
-              next
-            end
-            # [    range     ]
-            #    [   c   ]
-            if range.first <= contract.begin_date && (!contract.end_date.nil? && (range.last > contract.end_date))
-              new_range = contract.end_date..range.last
-              ranges[idx] = range.first..contract.begin_date
-              new_ranges << new_range
-              next
-            end
-            rescue Exception => e
-              byebug.byebug
-            end
+            ranges = ranges + new_ranges
+            ranges.delete_if { |r| (r.last-r.first).zero? }
           end
-          ranges = ranges + new_ranges
-          ranges.delete_if { |r| (r.last-r.first).zero? }
         end
         {
           register_meta: register_meta,
