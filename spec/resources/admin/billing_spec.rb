2d82ac1bb2ce9ff98432b1760b645a578a94e545
Maximilian <maximilian@buzzn.net>
2019-02-15 21:22:55 +0100

    specs: fix for billing

--- spec/resources/admin/billing_spec.rb
+++ spec/resources/admin/billing_spec.rb
@@ -35,7 +35,7 @@ describe Admin::BillingResource do
       expect(subject['allowed_actions']['update']['status']['void']).to eql true
       expect(subject['allowed_actions']['update']['status']['calculated']).to eql true
       expect(subject['allowed_actions']['update']['status']['documented']).not_to eql true
-      expect(subject['allowed_actions']['update']['status']['documented'].to_s).to eql '{"contract"=>{"current_payment"=>["must be filled"]}}'
+      expect(subject['allowed_actions']['update']['status']['documented'].to_s).to eql '{"contract"=>{"current_payment"=>["must be filled"], "localpool"=>{"fake_stats"=>["must be filled"]}}}'
     end
 
   end
