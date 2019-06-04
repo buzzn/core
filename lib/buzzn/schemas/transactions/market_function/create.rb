bbca8a885c469e2120ae4dcaa205605c76a84cd8
Maximilian <maximilian@buzzn.net>
2019-06-04 15:57:04 +0200

    market_function: add create transaction

--- lib/buzzn/schemas/transactions/market_function/create.rb
+++ lib/buzzn/schemas/transactions/market_function/create.rb
@@ -5,16 +5,31 @@ require_relative '../../../../../app/models/organization/market_function'
 
 module Schemas::Transactions::MarketFunction
 
-  Create = Schemas::Support.Schema do
+  CreateBase = Schemas::Support.Schema do
     required(:market_partner_id).filled(:str?, max_size?: 64)
     required(:edifact_email).filled(:str?, :email?, max_size?: 64)
-    required(:function).value(included_in?: ::Organization::MarketFunction.functions.values)
 
     optional(:contact_person) do
       id?.not.then(schema(Schemas::Transactions::Person::AssignOrCreateWithAddress))
     end
 
     optional(:address).schema(Schemas::Transactions::Address::Create)
+  end
+
+  Create = Schemas::Support.Form(CreateBase) do
+    required(:function).value(included_in?: ::Organization::MarketFunction.functions.values)
+  end
+
+  class << self
+
+    def create_for(organization)
+      functions = organization.market_functions.collect { |x| x.function }
+      possible_functions = ::Organization::MarketFunction.functions.values - functions
+      Schemas::Support.Form(CreateBase) do
+        required(:function).value(included_in?: possible_functions)
+      end
+    end
+
   end
 
 end
---
+++ lib/buzzn/transactions/admin/market_function/create.rb
@@ -0,0 +1,38 @@
+require_relative '../market_function'
+require_relative '../../../schemas/transactions/market_function/create'
+
+module Transactions::Admin::MarketFunction
+  class Create < Transactions::Base
+
+    validate :schema
+    authorize :allowed_roles
+    around :db_transaction
+    tee :create_contact_person_address, with: 'operations.action.create_address'
+    tee :create_contact_person, with: 'operations.action.create_person'
+    tee :create_address, with: 'operations.action.create_address'
+    map :wrap_up, with: 'operations.action.create_item'
+
+    def schema(resource:, organization:, **)
+      Schemas::Transactions::MarketFunction.create_for(organization)
+    end
+
+    def allowed_roles(permission_context:)
+      permission_context.create
+    end
+
+    def create_contact_person_address(params:, **)
+      super(params: params[:contact_person] || {})
+    end
+
+    def create_contact_person(params:, **)
+      super(params: params, method: :contact_person)
+    end
+
+    def wrap_up(params:, resource:, **)
+      Organization::MarketFunctionResource.new(
+        *super(resource, params)
+      )
+    end
+
+  end
+end
