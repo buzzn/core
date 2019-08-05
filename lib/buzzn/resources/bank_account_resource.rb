a8ea8f55137ced80ca5c970d95c6f0ea0be18220
Clemens Manert <Clemens@Manert.de>
2019-08-05 15:30:23 +0200

    Adds sepa and account number. Fixes some minor code issues.

--- lib/buzzn/resources/bank_account_resource.rb
+++ lib/buzzn/resources/bank_account_resource.rb
@@ -9,21 +9,4 @@ class BankAccountResource < Buzzn::Resource::Entity
               :direct_debit,
               :updatable, :deletable
 
-  # @note This methods only works on german bank accounts.
-  # @return [String] The national bank code. NOT bic!
-  def bank_code()
-    iban[4..11]
-  end
-
-  # @note This methods only works on german bank accounts.
-  # @return [String] The bank account number. NOT bic!
-  def bank_account_number
-    iban[12..21]
-  end
-
-  # @note This methods only works on german bank accounts.
-  # @return [String] Letters used to identify the bank's country.
-  def country_code
-    iban[0..1]
-  end
 end
