f011062f462ba60267e88ae2bbaaf2497753e347
Maximilian <maximilian@buzzn.net>
2019-08-14 10:28:50 +0200

    meta: all values are maybe
    closes BPSD-27

--- lib/buzzn/schemas/transactions/admin/register/create_meta.rb
+++ lib/buzzn/schemas/transactions/admin/register/create_meta.rb
@@ -11,9 +11,9 @@ Schemas::Transactions::Admin::Register::CreateMetaLoose = Schemas::Support.Form 
   optional(:name).value(:str?, max_size?: 64)
   optional(:label).value(included_in?: Register::Meta.labels.values)
   optional(:observer_enabled).value(:bool?)
-  optional(:observer_min_threshold).value(:int?, gteq?: 0)
-  optional(:observer_max_threshold).value(:int?, gteq?: 0)
+  optional(:observer_min_threshold).maybe(:int?, gteq?: 0)
+  optional(:observer_max_threshold).maybe(:int?, gteq?: 0)
   optional(:observer_offline_monitoring).value(:bool?)
-  optional(:market_location_id).value(:str?, size?: 11)
+  optional(:market_location_id).maybe(:str?, size?: 11)
 
 end
