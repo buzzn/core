8006985cf2a94a73503a3670341413101a0c8ffb
Clemens Manert <Clemens@Manert.de>
2019-10-14 15:16:16 +0200

    Adds letter tariff change.

---
+++ app/pdfs/13_tariff_change_letter.rb
@@ -0,0 +1,34 @@
+require_relative 'a0x_general'
+
+module Pdf
+  class TariffChangeLetter < A0xGeneral
+
+    protected
+
+    def title
+      "#{Buzzn::Utils::Chronos.now.strftime('%Y-%m-%d-%H-%M-%S')}-Auftragseingangsbestätigung-#{contract.localpool.base_slug}-#{contract.contract_number}-#{contract.contract_number_addition}"
+    end
+
+    def build_struct
+      now = Date.today
+      upcoming = nil
+      Service::Tariffs.data(@contract.localpool.tariffs).each do |tariff|
+        if tariff.begin_date >= now
+          upcoming = tariff.tariff
+          break
+        end
+      end
+
+      super.tap do |h|
+        h[:is_pre_contract] = true
+        h[:document_name] = 'Auftragseingangsbestätigung'
+        h[:upcoming_tariff] = build_tariff(upcoming)
+      end
+    end
+
+    def template_name
+      '13_tariff_change_letter.slim'
+    end
+
+  end
+end
