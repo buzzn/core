ea0a133204fe37a870d7813112cc2ea3f981e3de
Clemens Manert <Clemens@Manert.de>
2019-10-30 20:17:42 +0100

    Permits tariffs for nothing

--- lib/buzzn/schemas/constraints/contract/tariff.rb
+++ lib/buzzn/schemas/constraints/contract/tariff.rb
@@ -3,6 +3,6 @@ require_relative '../contract'
 Schemas::Constraints::Contract::Tariff = Schemas::Support.Form do
   required(:name).filled(:str?, max_size?: 64)
   required(:begin_date).filled(:date?)
-  required(:energyprice_cents_per_kwh).filled(:float?, gt?: 0)
-  required(:baseprice_cents_per_month).filled(:float?, gt?: 0)
+  required(:energyprice_cents_per_kwh).filled(:float?, gteq?: 0)
+  required(:baseprice_cents_per_month).filled(:float?, gteq?: 0)
 end
