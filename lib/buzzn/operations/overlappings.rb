07f5e08ba70975731ef072d5f90d82d377da429a
KatharinaGrett <katharina-grett@web.de>
2020-06-29 15:19:36 +0200

    added checking if contract is still open

--- lib/buzzn/operations/overlappings.rb
+++ lib/buzzn/operations/overlappings.rb
@@ -6,10 +6,18 @@ class Operations::Overlappings
     unless params[:last_date].nil?
       resource.register_meta.registers.each  do |r| 
         if r.contracts.any?
-          r.contracts.each do |c|  
-            unless c.begin_date.nil? || c.end_date.nil?
-              if params[:last_date] > c.begin_date && params[:last_date] < c.end_date
-                  raise Buzzn::ValidationError.new(:no_other_contract_in_range=>["there is already another contract in that time range present"])
+          r.contracts.each do |c| 
+            unless c.id == resource.id
+              unless c.begin_date.nil? 
+                if c.end_date.nil?
+                  if params[:last_date] > c.begin_date
+                    raise Buzzn::ValidationError.new(:no_other_contract_in_range=>["there is already another contract in that time range present"])
+                  end
+                else
+                  if params[:last_date] > c.begin_date && params[:last_date] < c.end_date
+                      raise Buzzn::ValidationError.new(:no_other_contract_in_range=>["there is already another contract in that time range present"])
+                  end
+                end
               end
             end
           end
