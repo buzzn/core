41931284e7a0b4ed29839023e3f719f270f1cdad
Maximilian <maximilian@buzzn.net>
2020-07-12 20:55:11 +0200

    check whether Rake exists

--- lib/buzzn/db.rb
+++ lib/buzzn/db.rb
@@ -7,7 +7,7 @@ module Buzzn
 
   DB = Sequel.connect(ActiveRecord::Base.connection_config.dup.tap do |c|
       c[:user] = c.delete(:username)
-      if Rake.application.top_level_tasks.include?("db:create")
+      if !Rake.nil? && Rake.application.top_level_tasks.include?("db:create")
         # do not connect to the database when trying to create it
         c[:test] = false
       end
