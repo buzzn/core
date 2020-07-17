9fa42e00411cc1838d10a533fba28eda221ef9ad
Maximilian <maximilian@buzzn.net>
2020-07-17 14:00:12 +0200

    active_record: enable logger in development mode

--- config/initializers/active_record.rb
+++ config/initializers/active_record.rb
@@ -2,3 +2,8 @@ require 'active_record'
 
 ActiveRecord::Base.schema_format = :sql
 ActiveRecord::Base.raise_in_transactional_callbacks = true
+
+if (ENV['RACK_ENV'] || 'development') == 'development'
+    ActiveRecord::Base.logger = ActiveSupport::Logger.new(STDOUT)
+    ActiveRecord::Base.logger.level = ::Logger::DEBUG
+end
