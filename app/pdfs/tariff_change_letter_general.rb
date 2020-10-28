b4ed85e38005f31487ae58dbf278928873181ae0
Clemens Manert <Clemens@Manert.de>
2020-10-28 11:53:27 +0100

    Adds various tariff change letters

---
+++ app/pdfs/tariff_change_letter_general.rb
@@ -0,0 +1,35 @@
+require_relative 'a0x_general'
+
+module Pdf
+  class TariffChangeLetterGeneral < A0xGeneral
+
+    protected
+
+    def title
+      "Strompreisanpassung zum 1.1.2020"
+    end
+
+    def build_struct
+      now = Date.today
+      upcoming = nil
+      Service::Tariffs.data(@contract.localpool.tariffs).each do |tariff|
+        if tariff.begin_date >= now
+          upcoming = tariff.tariff
+          break
+        end
+      end
+
+      super.tap do |h|
+        h[:is_pre_contract] = true
+        h[:document_name] = 'Strompreisanpassung zum 01.02.2020'
+        h[:upcoming_tariff] = build_tariff(upcoming)
+      end
+    end
+
+    def template_name
+      '13_tariff_change_letter.slim'
+    end
+
+
+  end
+end
---
+++ lib/buzzn/schemas/pre_conditions/contract/document_localpool_power_taker_contract_tariff_change.rb
@@ -0,0 +1,40 @@
+require_relative '../person'
+require_relative '../organization'
+require_relative '../contract'
+require './app/models/organization/general.rb'
+require './app/models/person.rb'
+
+module Schemas::PreConditions::Contract
+
+  DocumentLocalpoolPowerTakerContractTariffChange = Schemas::Support.Schema do
+
+    configure do
+      def upcomping_tariff_present?(localpool)
+        localpool.tariffs.select {|t| t.begin_date > Date.today}.any?
+      end
+    end
+
+    required(:customer) do
+      mtype?(Organization::General).then(schema(Schemas::PreConditions::Organization))
+                                   .and(mtype?(Person).then(schema(Schemas::PreConditions::Person)))
+    end
+
+    required(:contractor) do
+      mtype?(Organization::General).then(schema(Schemas::PreConditions::Organization))
+                                   .and(mtype?(Person).then(schema(Schemas::PreConditions::Person)))
+    end
+
+    required(:localpool).schema do
+      required(:address).filled
+    end
+
+    required(:contractor_bank_account).filled
+
+    required(:current_tariff).filled
+    required(:current_payment).filled
+
+    required(:localpool, &:upcomping_tariff_present?)
+
+  end
+
+end
