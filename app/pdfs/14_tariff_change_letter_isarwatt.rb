4580dc36a007180d60514ef8441a9fae47d3dc22
Clemens Manert <Clemens@Manert.de>
2020-10-29 15:29:08 +0100

    Update tariff change letter buzzn

--- app/pdfs/14_tariff_change_letter_isarwatt.rb
+++ app/pdfs/14_tariff_change_letter_isarwatt.rb
@@ -6,5 +6,15 @@ module Pdf
     def template_name
       '14_tariff_change_letter_isarwatt.slim'
     end
+
+    def preis_sentence(subject, previous, next_one, currency)
+      if previous < next_one
+        "erhöhen wir den #{subject} von derzeit #{german_div(previous)} #{currency}/Monat auf #{german_div(next_one)} #{currency}/Monat."
+      elsif previous > next_one
+        "senken wir den #{subject} von derzeit #{german_div(previous)} #{currency}/Monat auf #{german_div(next_one)} #{currency}/Monat."
+      else
+        "Der #{subject} bleibt bei #{next_one}  #{currency}."
+      end
+    end
   end
 end
