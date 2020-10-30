e870406f062e11511eebb9f963e4700f8c47729b
Clemens Manert <Clemens@Manert.de>
2020-10-30 13:30:38 +0100

    Deliver tariff   change letter to powergiver if powertaker has no email present.

--- lib/buzzn/services/deliver_tarrif_change_letter_service.rb
+++ lib/buzzn/services/deliver_tarrif_change_letter_service.rb
@@ -8,6 +8,47 @@ class Services::DeliverTarrifChangeLetterService
   end
 
   def deliver_tariff_change_letter(localpool, contract, document_id)
+    if contract.email.nil?
+      deliver_tariff_change_letter_powergiver(localpool, contract, document_id)
+    else
+      deliver_tariff_change_letter_powertaker(localpool, contract, document_id)
+    end
+  end
+
+  def deliver_tariff_change_letter_powergiver(localpool, contract, document_id)
+    target = Document.find(document_id)
+
+    contact = localpool.contact
+
+    salute = "Hallo #{contact.first_name} #{contact.last_name}"
+
+    if contact.prefix == 'M'
+      salute = "Sehr geehrter Herr #{contact.last_name}"
+    elsif contact.prefix == 'F'
+      salute = "Sehr geehrte Frau #{contact.last_name}"
+    end
+
+    message = <<~MSG
+      #{salute},
+
+      ihr Strohmnehmer #{contract.contact.first_name}  #{contract.contact.last_name}  hat in unserem System
+      leider keine Emailadresse hinterlegt, wir bitten Sie, das angehängte Schreiben zuzustellen.
+
+      Mit freundlichen Grüßen,
+
+      Buzzn Team
+    MSG
+
+    mail_service.deliver_later({:from => 'team@buzzn.net',
+                                :to => contact.email,
+                                :from_person_id => localpool.owner.contact.id,
+                                :bcc => 'team@localpool.de',
+                                :subject => "Preisanpassungsschreiben Energiegruppe #{localpool.name}",
+                                :text => message,
+                                :document_id => target.id})
+  end
+
+  def deliver_tariff_change_letter_powertaker(localpool, contract, document_id)
     target = Document.find(document_id)
 
     sender = if localpool.owner.is_a?(Organization::GeneralResource)
