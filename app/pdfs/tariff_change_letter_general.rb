fd779d4d6a511593f2db1c4f81a7a85bd3eeb74b
Clemens Manert <Clemens@Manert.de>
2020-10-30 16:17:16 +0100

    Fix date

--- app/pdfs/tariff_change_letter_general.rb
+++ app/pdfs/tariff_change_letter_general.rb
@@ -6,7 +6,7 @@ module Pdf
     protected
 
     def title
-      'Strompreisanpassung zum 1.1.2020'
+      'Strompreisanpassung zum 1.1.2021'
     end
 
     def preis_sentence(subject, previous, next_one, currency)
@@ -30,7 +30,7 @@ module Pdf
       end
       super.tap do |h|
         h[:is_pre_contract] = true
-        h[:document_name] = 'Strompreisanpassung zum 01.02.2020'
+        h[:document_name] = 'Strompreisanpassung zum 01.02.2021'
         h[:upcoming_tariff] = build_tariff(upcoming)
         h[:baseprice_sentence] = preis_sentence('Arbeitspreis', @contract.current_tariff.baseprice_cents_per_month_after_taxes, upcoming.baseprice_cents_per_month_after_taxes, 'Cent/kWh')
         h[:energyprice_sentence] = preis_sentence('Arbeitspreis', @contract.current_tariff.energyprice_cents_per_kwh_after_taxes, upcoming.energyprice_cents_per_kwh_after_taxes, 'Cent/kWh')
