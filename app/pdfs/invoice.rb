a1ab6dd3933c6dac9b7abba171f671a37fe0a700
Waleed <waleed.gatosorriso@gmail.com>
2021-02-24 14:42:57 +0100

    DEV-580 removing start from brutto price in T01 invoice

--- app/pdfs/invoice.rb
+++ app/pdfs/invoice.rb
@@ -333,7 +333,7 @@ module Pdf
 
       vat = billing_hash[:vat]
       has_bank_and_direct_debit = @billing.contract.customer_bank_account&.direct_debit
-      payment_amounts_to = "Abschlag beträgt #{abschlag[:amount_euro_netto]} € netto +  #{abschlag[:amount_euro_vat]} € USt (#{((Vat.current.amount - 1)*100).to_i} %) = <strong>#{abschlag[:amount_euro]} € brutto&ast;</strong>"
+      payment_amounts_to = "Abschlag beträgt #{abschlag[:amount_euro_netto]} € netto +  #{abschlag[:amount_euro_vat]} € USt (#{((Vat.current.amount - 1)*100).to_i} %) = <strong>#{abschlag[:amount_euro]} € brutto</strong>"
       abschlag_begin_date = to_date(abschlag[:begin_date])
       # negative means it's disabled for this powertaker
       abschlag[:satz] = if abschlag[:disabled]
