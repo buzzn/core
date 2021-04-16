f6e3786a4f3b92aaeaf1b208ffefdd5e5e8e0819
KatharinaGrett <katharina-grett@web.de>
2021-04-16 13:46:31 +0200

    Dev 539 created global report architecture

---
+++ lib/buzzn/roda/admin/global_report_roda.rb
@@ -0,0 +1,34 @@
+require_relative '../admin_roda'
+require 'buzzn/utils/file'
+
+module Admin
+  class GlobalReportRoda < BaseRoda
+
+    include Import.args[:env,
+                        generate_meter_report: 'transactions.admin.global_report.generate_meter_report',
+                        return_report: 'transactions.admin.global_report.return_report'
+                      ]
+
+    plugin :shared_vars
+
+    route do |r|
+
+        r.on 'meter_report_id' do
+          r.get! do
+            generate_meter_report
+          end
+        end
+
+        r.on 'report' do
+          r.post! do
+            report = return_report.(params: r.params)
+            filename = Buzzn::Utils::File.sanitize_filename('report.csv')
+            r.response.headers['Content-Type'] = 'text/csv;charset=ISO-8859'
+            r.response.headers['Content-Disposition'] = "inline; filename=\"#{filename}\""
+            r.response.write(report.value!)
+          end
+        end
+
+    end
+  end
+end
