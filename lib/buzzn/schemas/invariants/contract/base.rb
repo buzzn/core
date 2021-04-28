4f25c2d0748761793517727ea4a0525cfdc6fb0f
KatharinaGrett <katharina-grett@web.de>
2021-04-28 13:21:59 +0200

    Dev-475 set termination_date and end_date as optional

--- lib/buzzn/schemas/invariants/contract/base.rb
+++ lib/buzzn/schemas/invariants/contract/base.rb
@@ -4,8 +4,8 @@ module Schemas
     module Contract
 
       Base = Schemas::Support.Form(Schemas::Constraints::Contract::Base) do
-        required(:termination_date).maybe
-        required(:end_date).maybe
+        optional(:termination_date).maybe
+        optional(:end_date).maybe
 
         rule(begin_date: [:begin_date, :termination_date, :end_date]) do |begin_date, termination_date, end_date|
           end_date.filled?.or(termination_date.filled?).then begin_date.filled?
