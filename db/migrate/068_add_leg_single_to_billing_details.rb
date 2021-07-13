2263de567e8f9a133c72e2746c20a08bd2713840
Stefanie <stefanie@buzzn.net>
2021-07-13 11:33:04 +0200

    DEV-637-leg_single added leg_single migration

---
+++ db/migrate/068_add_leg_single_to_billing_details.rb
@@ -0,0 +1,11 @@
+class AddLegSingleToBillingDetails < ActiveRecord::Migration
+
+    def up
+      add_column :billing_details, :leg_single, :boolean
+    end
+  
+    def down
+      remove_column :billing_details, :leg_single
+    end
+  
+end 
