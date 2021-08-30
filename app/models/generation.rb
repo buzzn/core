5520cdc4cce3c1a7305102fe384292caf1c228ff
Stefanie <stefanie@buzzn.net>
2021-08-30 12:40:16 +0200

    DEV-643 enhanced rake task to add generation to groups via csv

--- app/models/generation.rb
+++ app/models/generation.rb
@@ -1,10 +1,12 @@
-require 'csv'
-require 'buzzn/utils/file'
+# generation.csv displays database
+# i.e. all groups that should have a generation-value must be included. For groups that are not included generation is going to be set nil
 
 class Groups < ActiveRecord::Base
 
     def self.import_from_csv(file)
         data = Buzzn::Utils::File.read(file)
+        check = []
+
         CSV.parse(data.gsub(/\r\n?/, "\n"), col_sep: ';', headers: true) do |row|
             gen = row['Generation']
             group = Group::Localpool.find(Hash[row.to_a]['ID'])
@@ -13,20 +15,32 @@ class Groups < ActiveRecord::Base
                 # logger.info "id not found: #{row}"
                 group.not_found("id not found: '#{row}'")
             else
-                if row['Generation'].match?(/\A-?\d+\Z/) == false
-                    logger.info "generation = 0 as not numeric"
-                elsif "#{group.generation}" != gen
-                    logger.info "row: #{row} generation changed from #{group['generation']} to #{gen}"
-                end
-                group.update(generation: gen)
-            end
 
+                if row['Generation'].nil?
+                    not_found("generation for group #{row['ID']}")
+                elsif row['Generation'].match?(/\A-?\d+\Z/) == false
+                    logger.info "generation for group #{row['ID']} = 0 as not numeric"
+                elsif "#{group.generation}" != gen
+                    logger.info "generation for group #{row['ID']} changed from #{group.generation} to #{gen}"
+                end
+                check.push(row['ID'])
+                group.update(generation: gen)
+
+            end
         end
+
+        Group::Localpool.all.each do |group|
+            if !check.include?("#{group.id}") && !group.generation.nil?
+                group.update(generation: nil)
+                logger.info "generation for group #{group.id} deleted"
+            end
+        end
+
     end
 
 
     private
-    
+
     def self.logger; @_logger ||= Buzzn::Logger.new(self); end
 
     def self.not_found(msg)
